a00a6d37611fecd6917456aae7164e631f402ac3
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-01-18 16:02:34 +0000

    Stripped all email files into plugin
    git-svn-id: svn://php-wax.com/svn/plugins/cms/trunk@1714 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- migrate/PageTableMigrate.php
+++ migrate/PageTableMigrate.php
@@ -5,7 +5,6 @@ class PageTableMigrate extends WXMigrate {
   public function up() {
     $this->create_column("title", "string");
     $this->create_column("url", "string");
-    $this->create_column("excerpt", "text");
     $this->create_column("content", "text");
     $this->create_column("author_id", "integer");
     $this->create_column("date_created", "DATETIME");
