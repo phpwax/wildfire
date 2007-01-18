8509a8101ef9e6079db51c45ba8fe5207684a9cd
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-01-18 16:54:52 +0000

    bug fix
    git-svn-id: svn://php-wax.com/svn/plugins/cms/trunk@1716 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- resources/app/controller/admin/AdminArticlesController.php
+++ resources/app/controller/admin/AdminArticlesController.php
@@ -1,13 +1,5 @@
 <?php
 class AdminArticlesController extends CMSAdminArticleController {
-  public $model = "cms_article";
-  public $model_class = "CmsArticle";
-  public $scaffold_columns = array(
-    "title"   =>array(),
-		"author"   =>array(),
-    "page_status" => array()
-  );
-  public $filter_columns = array("title", "url");
   
   
 }
