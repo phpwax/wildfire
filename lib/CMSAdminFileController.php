9745cfa926187be545d20d352069b490209b5569
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-01-20 23:00:29 +0000

    continued cleanup of core cms
    git-svn-id: svn://php-wax.com/svn/plugins/cms/trunk@1746 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- lib/CMSAdminFileController.php
+++ lib/CMSAdminFileController.php
@@ -10,25 +10,8 @@ class CMSAdminFileController extends CMSAdminComponent {
 		
   public $model;
 	public $model_class="CmsFile";
-	public $db_table;
 	public $display_name = "Files and Images";
-	public $tag_model = "CmsTag";
 	
-	public function create() {
-		if($this->tag_model) {
-	    $tag_model = new $this->tag_model;
-  	  $this->tags = $tag_model->find_all();
-	  }
-		parent::create();
-	}
-	
-	public function edit() {
-	  if($this->tag_model) {
-	    $tag_model = new $this->tag_model;
-  	  $this->tags = $tag_model->find_all();
-	  }
-		parent::edit();
-	}
 	
 	public function file_info() {
 	  $this->use_layout=false;
@@ -38,22 +21,27 @@ class CMSAdminFileController extends CMSAdminComponent {
 	}
 	
 	public function show_image() {
-	  $this->use_layout=false;
-	  $this->accept_routes=2;
-	  if(!isset($this->route_array[1])) $size=170;
-	   else $size = $this->route_array[1];
-	  $this->show_image = new $this->model_class($this->route_array[0]);
-	  if($this->image = File::render_temp_image($this->show_image->path.$this->show_image->filename, $size) ) {
-	    return true;
-	  } else {
-	    $this->image = File::display_image(PUBLIC_DIR."images/filesystem/generic.png");
-	  }
-	}
+  	$this->use_layout=false;
+  	$this->use_view = false;
+  	
+  	if(!isset($this->route_array[1])) $size=110;
+  	 else $size = $this->route_array[1];
+  	$this->show_image = new CmsFile($this->route_array[0]);
+
+    $source = $this->show_image->path.$this->show_image->filename;
+    $file = CACHE_DIR.$this->route_array[0]."_".$this->route_array[1];
+    if(!is_readable($file)) {
+      File::resize_image($source, $file, $size);
+    }
+		if($this->image = File::display_image($file) ) {
+			return true;
+		}
+    return false;
+  }
 	
 	public function download_file() {
 	  $this->use_layout=false;
-	  $this->accept_routes=1;
-	  $this->get_file = new $this->model_class($this->route_array[0]);
+	  $this->get_file = new $this->model_class($this->param("id"));
 	  File::stream_file($this->get_file->path.$this->get_file->filename);
 	}
 		
