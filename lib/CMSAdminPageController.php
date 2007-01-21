33e5ca94d4fbe7359239e9bafdf698765cc19f87
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-01-21 17:55:08 +0000

    work on image browser
    git-svn-id: svn://php-wax.com/svn/plugins/cms/trunk@1873 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- lib/CMSAdminPageController.php
+++ lib/CMSAdminPageController.php
@@ -24,7 +24,7 @@ class CMSAdminPageController extends CMSAdminComponent{
 		$this->use_layout=false;
 		$page = new CmsPage($this->param("id"));
 		$page->add_images($_POST['id'], $this->param("order"));
-		$this->image = $page->fetch_images($_POST['id']);
+		$this->image = $page->find_images($_POST['id']);
 	}
 	
 	public function remove_image() {
