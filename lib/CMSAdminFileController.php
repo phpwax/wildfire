af54e357e0cd250f66c54d869a9d01c4cfe82387
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-01-21 09:09:45 +0000

    bug fix
    git-svn-id: svn://php-wax.com/svn/plugins/cms/trunk@1762 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- lib/CMSAdminFileController.php
+++ lib/CMSAdminFileController.php
@@ -45,13 +45,13 @@ class CMSAdminFileController extends CMSAdminComponent {
 	}
 	
 	public function browse_images() {
-  	$this->all_images = ($image = new Image) ? $image->find_all_images() : array();
+  	$this->all_images = ($image = new CmsFile) ? $image->find_all_images() : array();
 		$this->all_images_partial = $this->render_partial("list_all_images");
 	}
 	
 	public function image_filter() {
     $this->use_layout=false;
-    $images = new Image;
+    $images = new CmsFile;
     $this->all_images = $images->find_filter_images($_POST['filter']);
     $this->all_images_partial = $this->render_partial("list_all_images");  
   }
