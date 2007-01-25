ec785547e639b580e133d508831bc5ff31210eb8
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-01-25 14:12:01 +0000

    bug fix
    git-svn-id: svn://php-wax.com/svn/plugins/cms/trunk@1940 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- lib/CMSAdminSectionController.php
+++ lib/CMSAdminSectionController.php
@@ -6,8 +6,7 @@ class CMSAdminSectionController extends CMSAdminComponent {
 	public $model_name = "cms_section";													
 	public $display_name = "Site Sections";
 	public $scaffold_columns = array(
-    "title"   =>array(),
-    "page_status" => array()
+    "title"   =>array()
   );
   public $filter_columns = array("title");
 
