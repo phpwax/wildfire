d5b9cfcf0256814bff7b5ba732f6aa33d631e1c3
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-01-25 11:40:28 +0000

    work on cms sections
    git-svn-id: svn://php-wax.com/svn/plugins/cms/trunk@1937 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- lib/CMSAdminSectionController.php
+++ lib/CMSAdminSectionController.php
@@ -18,7 +18,7 @@ class CMSAdminSectionController extends CMSAdminComponent {
 	public function make_tree_array($level=0) {
 		$sections = new CmsSection;
 		foreach(new RecursiveIteratorIterator($sections) as $node) {
-			$tree_array[]=$node;
+			$this->tree_array[]=$node;
 		}
 	}
 
