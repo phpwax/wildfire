3eff46af3ebbf1d050adf33b0ddf20cc6270dfc3
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-01-25 22:43:55 +0000

    debugging
    git-svn-id: svn://php-wax.com/svn/plugins/cms/trunk@1995 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- lib/CMSAdminPageController.php
+++ lib/CMSAdminPageController.php
@@ -25,7 +25,6 @@ class CMSAdminPageController extends CMSAdminComponent {
 		$this->use_view="index";
 		$section = new CmsSection;
 		$this->all_rows = $this->model->find_all_by_cms_section_id($section->find_by_url($this->original_action)->id);
-		print_r($this->all_rows); exit;
 		$this->filter_block_partial .= $this->render_partial("section_filter");
 		$this->list = $this->render_partial("list");
 	}
