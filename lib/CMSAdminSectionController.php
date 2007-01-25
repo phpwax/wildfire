233bee0f6657d6a42c88b7efa8f826fcb88c483a
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-01-25 17:48:12 +0000

    work on cms sections
    git-svn-id: svn://php-wax.com/svn/plugins/cms/trunk@1958 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- lib/CMSAdminSectionController.php
+++ lib/CMSAdminSectionController.php
@@ -11,34 +11,15 @@ class CMSAdminSectionController extends CMSAdminComponent {
   public $filter_columns = array("title");
 
 	public function controller_global() {
-		$sections = $this->model->find_roots();
-		$this->traverse_tree($sections);
-		$this->tree_collection = $this->create_section_tree();
+		$this->tree_collection = $this->model->sections_as_collection();
 	}
 	
 	public function index() {
-		$this->all_rows = $this->final_array;
+		$this->all_rows = $this->model->find_ordered_sections();
 		$this->list = $this->render_partial("list");
 	}
 	
-	protected function traverse_tree($object_array) {
-		foreach($object_array as $node) {
-			$this->final_array[] = $node;
-			if($node->has_children()) {
-				$this->traverse_tree($node->get_children("title ASC"));
-			} 
-		}
-	}
 	
-	protected function create_section_tree() {
-		$collection["0"]="Default";
-		foreach($this->final_array as $item) {
-	  	$value = str_pad($item->title, strlen($item->title) + $item->get_level(), "^", STR_PAD_LEFT);
-			$value = str_replace("^", "&nbsp;", $value);
-			$collection["{$item->id}"] = $value;
-		}
-		return $collection;
-	}
 	
 	
 
--- lib/CmsSection.php
+++ lib/CmsSection.php
@@ -3,10 +3,38 @@
 class CmsSection extends WXTreeRecord {
   
   public $type_options = array("0"=>"Page-Style Content", "1"=>"News Article-Style Content");
+	public $tree_array = array();
 
  	public function section_type() {
  	  return $this->type_options[$this->type];
  	}
+
+	protected function traverse_tree($object_array) {
+		foreach($object_array as $node) {
+			$this->tree_array[] = $node;
+			if($node->has_children()) {
+				$this->traverse_tree($node->get_children("title ASC"));
+			} 
+		}
+	}
+	
+	public function sections_as_collection() {	
+		$this->traverse_tree($this->find_roots());
+		$collection["0"]="Default";
+		foreach($this->tree_array as $item) {
+	  	$value = str_pad($item->title, strlen($item->title) + $item->get_level(), "^", STR_PAD_LEFT);
+			$value = str_replace("^", "&nbsp;", $value);
+			$collection["{$item->id}"] = $value;
+		}
+		return $collection;
+	}
+	
+	public function find_ordered_sections() {
+		$this->traverse_tree($this->find_roots());
+		return $this->tree_array();
+	}
+	
+	
  	
 	
 }
