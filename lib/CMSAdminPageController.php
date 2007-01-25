a342a558bf54e8e8586a23b5ae5a1c86a13248ae
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-01-25 23:00:54 +0000

    work on cms sections
    git-svn-id: svn://php-wax.com/svn/plugins/cms/trunk@1999 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- lib/CMSAdminPageController.php
+++ lib/CMSAdminPageController.php
@@ -25,7 +25,7 @@ class CMSAdminPageController extends CMSAdminComponent {
 		$this->use_view="index";
 		$section = new CmsSection;
 		$this->all_rows = $this->model->find_all_by_cms_section_id($section->find_by_url($this->original_action)->id);
-		$this->filter_block_partial .= $this->render_partial("section_filter");
+		$this->filter_block_partial = $this->filter_block_partial.$this->render_partial("section_filter");
 		$this->list = $this->render_partial("list");
 	}
 
