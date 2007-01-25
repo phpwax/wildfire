d36e5b4f15464ccb5af34df36604342fb0e8552b
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-01-25 21:12:50 +0000

    debugging
    git-svn-id: svn://php-wax.com/svn/plugins/cms/trunk@1980 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- lib/CmsSection.php
+++ lib/CmsSection.php
@@ -38,6 +38,7 @@ class CmsSection extends WXTreeRecord {
 	public function filtered_sections($id) {
 		if(!$this->tree_array) $this->traverse_tree($this->find_roots());
 		$array = $this->tree_array;
+		print_r($array); exit;
 		foreach($array as $key=>$node) {
 			if($node->section_type != $id) unset($array[$key]);
 		}
