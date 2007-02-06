8945be0d491161eda6ae513c1d3111224f3a70ff
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-02-06 12:32:52 +0000

    Filtered default section from editable list
    git-svn-id: svn://php-wax.com/svn/plugins/cms/trunk@2254 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- lib/CmsSection.php
+++ lib/CmsSection.php
@@ -38,7 +38,8 @@ class CmsSection extends WXTreeRecord {
 	}
 	
 	public function find_ordered_sections($start_level = "0") {
-		if(!$this->tree_array) $this->traverse_tree($this->find_roots($start_level), $this->order_field, $this->order_direction);
+    $this->tree_array=array();
+		$this->traverse_tree($this->find_roots($start_level), $this->order_field, $this->order_direction);
 		return $this->tree_array;
 	}
 	
