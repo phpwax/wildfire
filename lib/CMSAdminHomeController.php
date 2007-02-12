b23dda8a6c5978d33722c80fff13cdfb890d2109
charles <charles@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-02-12 15:09:46 +0000

    new configuration values - address & google key
    git-svn-id: svn://php-wax.com/svn/plugins/cms/trunk@2288 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- lib/CMSAdminHomeController.php
+++ lib/CMSAdminHomeController.php
@@ -52,6 +52,20 @@ class CMSAdminHomeController extends CMSAdminComponent {
 	  $this->search_module = $this->render_partial("stat_search");
 	  $this->dash_module = $this->render_partial("stat_dash");
 	  
+		 //address module
+			$this->address_details = CmsConfiguration::get('address');
+			$address_details = unserialize($this->address_details);		
+			$this->postcode = $address_details['postcode']; 
+			$this->name = $address_details['name'];
+			$this->address = $address_details['address']; 
+
+			$this->email = $address_details['email_address'];
+			$this->phone = $address_details['phone_number'];
+			$this->fax	 = $address_details['fax_number'];
+			$this->address_module = $this->render_partial("stat_address");
+			//key
+			$this->google_key = CmsConfiguration::get('google_key');
+			$this->google_module = $this->render_partial("stat_google");
 	}
 	
 	public function support() { $this->display_action_name = 'Support'; }
@@ -71,6 +85,7 @@ class CMSAdminHomeController extends CMSAdminComponent {
     if($_POST['stat_link_url']) CmsConfiguration::set("stat_link_url", $_POST['stat_link_url']);
     if($_POST['stat_search_url']) CmsConfiguration::set("stat_search_url", $_POST['stat_search_url']);
     if($_POST['stat_dash_url']) CmsConfiguration::set("stat_dash_url", $_POST['stat_dash_url']);
+		if($_POST['address']) CmsConfiguration::set("address", serialize($_POST['address'])	);
   }
 
 
