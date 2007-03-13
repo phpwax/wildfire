ee67035bacc9bf68960c3474f7989dbbf7234fe0
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-03-13 11:46:55 +0000

    debugging change
    git-svn-id: svn://php-wax.com/svn/plugins/cms/trunk@2474 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- lib/CMSAdminHomeController.php
+++ lib/CMSAdminHomeController.php
@@ -6,7 +6,7 @@
 
 class CMSAdminHomeController extends CMSAdminComponent {
 	public $module_name = "home";												
-  	public $model;
+  public $model;
 	public $model_name = "cms_user";
 	public $model_class = "CmsUser";
 	public $display_name = "Home";
@@ -20,6 +20,7 @@ class CMSAdminHomeController extends CMSAdminComponent {
 	protected function process_login() {
 		$auth = new WXDBAuthenticate(array("db_table"=>$this->model_name));
 		if( $auth->verify($_POST['username'], $_POST['password'])){
+		  print_r($this); exit;
 		  if($this->authorised_redirect) return $this->authorised_redirect;		  
 			else return 'index';
 		}
