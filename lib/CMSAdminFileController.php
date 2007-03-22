f1c37e81d5ed5f240509bc870dffffa56569a6cd
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-03-22 09:24:26 +0000

    pagination to image browse
    git-svn-id: svn://php-wax.com/svn/plugins/cms/trunk@2569 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- lib/CMSAdminFileController.php
+++ lib/CMSAdminFileController.php
@@ -54,11 +54,11 @@ class CMSAdminFileController extends CMSAdminComponent {
       $offset = ($this->param("id") -1) * 10; 
       $count = 10;
     }
+    $this->id = $this->param("id");
 		$this->use_layout=false;
   	$this->all_images = ($image = new CmsFile) ? $image->find_all_images() : array();
-  	if($count > 0 && count($this->all_images > $offset + $count)) $this->more =true;
-  	if($count > 0 && $offset > 0) $this->previous = true;
-  	$this->id = $this->param("id");
+  	if($count > 0 && count($this->all_images > $offset + $count)) $this->more = $this->id+1;
+  	if($count > 0 && $offset > 0) $this->previous = $this->id -1;
   	$this->all_images = new LimitIterator(new ArrayIterator($this->all_images), $offset, $count);
     $this->all_images_partial = $this->render_partial("list_all_images");  
 	}
