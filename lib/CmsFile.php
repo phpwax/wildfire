9e22e9ee27326924920fe5dff2ebafe6d8c83d36
charles <charles@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-03-23 09:59:28 +0000

    change to filter on caption and filename
    git-svn-id: svn://php-wax.com/svn/plugins/cms/trunk@2601 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- lib/CmsFile.php
+++ lib/CmsFile.php
@@ -6,7 +6,7 @@ class CmsFile extends WXFileActiveRecord {
 	}
 	
 	public function find_filter_images($filter, $limit = false) {
-    $params = array("conditions"=>"type LIKE '%image%' AND filename LIKE '%$filter%'");
+    $params = array("conditions"=>"type LIKE '%image%' AND (filename LIKE '%$filter%' OR caption LIKE '%$filter%')");
     if($limit) $params['limit']=$limit;
 	  return $this->find_all($params);
 	}
