0c1e583241dd57b9cb43b37041b79d71c7b2d28b
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-03-29 17:40:25 +0000

    moved cms to support asset server serving
    git-svn-id: svn://php-wax.com/svn/plugins/cms/trunk@2670 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- lib/CMSHelper.php
+++ lib/CMSHelper.php
@@ -4,13 +4,11 @@ class CMSHelper extends WXHelpers {
   public function simple_wysiwyg($object, $attribute, $options=array(), $with_label=true) {
     if($options["class"]) $options["class"].=" widgEditor";
     else $options["class"] = "widgEditor";
-		print stylesheet_link_tag('/javascripts/editors/simple/css/widget-content.css');
     return text_area($object, $attribute, $options, $with_label, "Put your content here");
   }
   public function simple_wysiwyg_tag($name, $value="", $options=array(), $with_label=true) {
     if($options["class"]) $options["class"].=" widgEditor";
     else $options["class"] = "widgEditor";
-		print stylesheet_link_tag('/javascripts/editors/simple/css/widget-content.css');
     return text_area_tag($name, $value, $options, $with_label, "Put your content here");
   }
 	
