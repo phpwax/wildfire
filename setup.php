1475214677075e0edc68910916b3433460ba439b
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-04-11 10:46:38 +0000

    installer changes
    git-svn-id: svn://php-wax.com/svn/plugins/cms/trunk@2710 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- setup.php
+++ setup.php
@@ -10,5 +10,5 @@ CMSApplication::register_module("categories", array("display_name"=>"Categories"
 CMSApplication::register_module("pages", array("display_name"=>"Site Pages", "link"=>"/admin/pages/"));
 CMSApplication::register_module("articles", array("display_name"=>"Articles", "link"=>"/admin/articles/"));
 CMSApplication::register_module("files", array("display_name"=>"Files", "link"=>"/admin/files/"));
-
+WXConfiguration::set("assets", "static.webxpress.com");
 ?>
