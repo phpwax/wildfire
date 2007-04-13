9ea5bb394c50a642906738f5420e47dd1390d882
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-04-13 12:12:11 +0000

    bug fix
    git-svn-id: svn://php-wax.com/svn/plugins/cms/trunk@2769 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- lib/CmsContent.php
+++ lib/CmsContent.php
@@ -39,17 +39,16 @@ class CmsContent extends WXActiveRecord {
 	}
 	
 	public function published_content($url, $section, $params=array()) {
-	  error_log($url.":".$section);
 	  $condition = "`status`=1 AND (DATE_FORMAT(`published`, '%y%m%d') <=  DATE_FORMAT(NOW(),'%y%m%d'))";
 	  if($params['conditions']) $params['conditions'].=" AND ".$condition;
 	  else $params['conditions'] = $condition;
-	  if(strlen($url)>1) return $this->find_by_url_and_cms_section_id($url, $section, $params);
+	  if(strlen($url)>1 && $res = $this->find_by_url_and_cms_section_id($url, $section, $params)) return $res;
 	  elseif($res = $this->find_all_by_cms_section_id($section, $params)) return $res;
 	  return array();
 	}
 	
 	public function all_content($url, $section, $params=array()) {
-	  if(strlen($url)>1) return $this->find_by_url_and_cms_section_id($url, $section, $params);
+	  if(strlen($url)>1 && $res = $this->find_by_url_and_cms_section_id($url, $section, $params)) return $res;
 	  elseif($res = $this->find_all_by_cms_section_id($section, $params)) return $res;
 	  return array();
   }
