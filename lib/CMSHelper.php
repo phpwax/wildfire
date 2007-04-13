fc9687e6834209650639ae6cc500c8cb970c7bec
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-04-13 09:10:58 +0000

    smart nav switcher
    git-svn-id: svn://php-wax.com/svn/plugins/cms/trunk@2749 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- lib/CMSHelper.php
+++ lib/CMSHelper.php
@@ -41,6 +41,7 @@ class CMSHelper extends WXHelpers {
   }
   
   public function smart_nav($url, $display, $current, $selected_id) {
+    echo substr($current, 1); exit;
     if(substr($current, 1) == $url) return content_tag("li", content_tag("a", $display, array("href"=>$url)), array("id"=>$selected_id));
     return content_tag("li", content_tag("a", $display, array("href"=>$url)));
   }
