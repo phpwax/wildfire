e8ff93bfbc1f6c8ded29d0f4771115708bd60075
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-06-01 11:10:01 +0000

    bug fix
    git-svn-id: svn://php-wax.com/svn/plugins/cms/trunk@2960 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- lib/CMSHelper.php
+++ lib/CMSHelper.php
@@ -16,6 +16,7 @@ class CMSHelper extends WXHelpers {
     $section = new CmsSection;
     $sec = $section->find_by_title($section_title);
     if(!$sec) $sec = $section->find_by_url($section_title);
+    if(!$sec) $sec = $section->find($section_title);
     $content = new CmsContent;
     return $content->published_content($sec->url, $sec->id, $params);
   }
