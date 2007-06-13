c1cbcccc1e62bd7e6dbcd27bde3efc07db3b639c
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-06-13 14:48:17 +0000

    Bug fix: Only set author id if not already set
    git-svn-id: svn://php-wax.com/svn/plugins/cms/trunk@3053 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- lib/CMSAdminComponent.php
+++ lib/CMSAdminComponent.php
@@ -150,8 +150,7 @@ class CMSAdminComponent extends WXControllerBase {
 	*/
 	protected function save($model, $redirect_to=false, $success = "Successfully Saved") {
 		if( $model->is_posted() ) {
-			$model->author_id = $this->current_user->id;
-			
+			if(!$model->author_id) $model->author_id = $this->current_user->id;			
 			if($id = $model->update_attributes($_POST[$this->model_name]) ) {
 			  if($redirect_to =="edit") $redirect_to = "edit/".$id;
 			  elseif(!$redirect_to) $redirect_to="index";
