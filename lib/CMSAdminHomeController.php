1a8310be1cae3c6262a9d71d2f3f34e2e61b40cd
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-06-15 12:15:29 +0000

    added start of settings module
    git-svn-id: svn://php-wax.com/svn/plugins/cms/trunk@3080 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- lib/CMSAdminHomeController.php
+++ lib/CMSAdminHomeController.php
@@ -52,20 +52,7 @@ class CMSAdminHomeController extends CMSAdminComponent {
 	  $this->search_module = $this->render_partial("stat_search");
 	  $this->dash_module = $this->render_partial("stat_dash");
 	  
-		 //address module
-			$this->address_details = CmsConfiguration::get('address');
-			$address_details = unserialize($this->address_details);		
-			$this->postcode = $address_details['postcode']; 
-			$this->name = $address_details['name'];
-			$this->address = $address_details['address']; 
-
-			$this->email = $address_details['email_address'];
-			$this->phone = $address_details['phone_number'];
-			$this->fax	 = $address_details['fax_number'];
-			$this->address_module = $this->render_partial("stat_address");
-			//key
-			$this->google_key = CmsConfiguration::get('google_key');
-			$this->google_module = $this->render_partial("stat_google");
+		 
 	}
 	
 	public function support() { $this->display_action_name = 'Support'; }
@@ -93,13 +80,7 @@ class CMSAdminHomeController extends CMSAdminComponent {
     return $simple;
   }
   
-  protected function stat_setup() {
-    if($_POST['stat_link_url']) CmsConfiguration::set("stat_link_url", $_POST['stat_link_url']);
-    if($_POST['stat_search_url']) CmsConfiguration::set("stat_search_url", $_POST['stat_search_url']);
-    if($_POST['stat_dash_url']) CmsConfiguration::set("stat_dash_url", $_POST['stat_dash_url']);
-		if($_POST['address']) CmsConfiguration::set("address", serialize($_POST['address'])	);
-		if($_POST['google_key']) CmsConfiguration::set("google_key", $_POST['google_key']	);
-  }
+  
 
 
 
--- lib/CMSAdminSettingsController.php
+++ lib/CMSAdminSettingsController.php
@@ -12,10 +12,34 @@ class CMSAdminSettingsController extends CMSAdminComponent {
 	public $display_name = "Settings";
 	
 	public function index() {
-	  
+	  $this->stat_setup();
 	}
 	
+  
+  protected function stat_setup() {
+    if($_POST['stat_link_url']) CmsConfiguration::set("stat_link_url", $_POST['stat_link_url']);
+    if($_POST['stat_search_url']) CmsConfiguration::set("stat_search_url", $_POST['stat_search_url']);
+    if($_POST['stat_dash_url']) CmsConfiguration::set("stat_dash_url", $_POST['stat_dash_url']);
+		if($_POST['address']) CmsConfiguration::set("address", serialize($_POST['address'])	);
+		if($_POST['google_key']) CmsConfiguration::set("google_key", $_POST['google_key']	);
+		$this->stat_visit = CmsConfiguration::get("stat_dash_url");
+		$this->stat_refer = CmsConfiguration::get("stat_link_url");
+		$this->stat_search = CmsConfiguration::get("stat_search_url");
+  }
 
+  
+  
+  protected function address_setup() {
+		$this->address_details = CmsConfiguration::get('address');
+		$address_details = unserialize($this->address_details);		
+		$this->postcode = $address_details['postcode']; 
+		$this->name = $address_details['name'];
+		$this->address = $address_details['address']; 
+		$this->email = $address_details['email_address'];
+		$this->phone = $address_details['phone_number'];
+		$this->fax	 = $address_details['fax_number'];
+		$this->google_key = CmsConfiguration::get('google_key');
+  }
 
 
 
