1cc4e2a1be8a3877b6edf6a02a4da9490f9ec654
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-06-15 11:51:35 +0000

    added start of settings module
    git-svn-id: svn://php-wax.com/svn/plugins/cms/trunk@3078 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

---
+++ lib/CMSAdminSettingsController.php
@@ -0,0 +1,24 @@
+<?php
+/**
+* Home page controller
+* @package PHP-WAX CMS
+*/
+
+class CMSAdminSettingsController extends CMSAdminComponent {
+	public $module_name = "settings";												
+  public $model;
+	public $model_name = "cms_configuration";
+	public $model_class = "CmsConfiguration";
+	public $display_name = "Settings";
+	
+	public function index() {
+	  
+	}
+	
+
+
+
+
+}
+
+?>
--- setup.php
+++ setup.php
@@ -5,6 +5,7 @@
 
 define("CMS_VERSION", "0.1.5");
 CMSApplication::register_module("home", array("display_name"=>"Admin Home", "link"=>"/admin/home/"));
+CMSApplication::register_module("settings", array("display_name"=>"Settings", "link"=>"/admin/settings/"));
 CMSApplication::register_module("sections", array("display_name"=>"Site Sections", "link"=>"/admin/sections/"));
 CMSApplication::register_module("categories", array("display_name"=>"Categories", "link"=>"/admin/categories/"));
 CMSApplication::register_module("content", array("display_name"=>"Content", "link"=>"/admin/content/"));
