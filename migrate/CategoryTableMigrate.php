0c94db3758038f4300eb5ed4cd2094c4cd12b787
joet <joet@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-06-16 19:58:11 +0000

    bugfix which was killing the parent category dropdown. default parent_id is now 0
    git-svn-id: svn://php-wax.com/svn/plugins/cms/trunk@3111 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- migrate/CategoryTableMigrate.php
+++ migrate/CategoryTableMigrate.php
@@ -4,7 +4,7 @@ class CategoryTableMigrate extends WXMigrate {
   
   public function up() {
     $this->create_column("name", "string");
-    $this->create_column("parent_id", "integer");
+    $this->create_column("parent_id", "integer",2,false,0);
     $this->create_table("cms_category");
   }
   
