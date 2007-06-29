feb06c1cabb8fe9f1ec7e9f5c062b0b175451713
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-06-29 14:19:13 +0000

    Work on overall cms improvements
    git-svn-id: svn://php-wax.com/svn/plugins/cms/trunk@3215 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- lib/CMSAdminContentController.php
+++ lib/CMSAdminContentController.php
@@ -19,16 +19,17 @@ class CMSAdminContentController extends CMSAdminComponent {
 	public $allowed_images = 3;
 
 	public function method_missing() {
+	  if(!$page = url("page")) $page=1;
 		$this->use_view="index";
 		$section = new CmsSection;
-		$this->all_rows = $this->model->find_all_by_cms_section_id($section->find_by_url($this->action)->id);
+		$options = array("order"=>"published DESC", "page"=>$page, "per_page"=>10);
+		$this->all_rows = $this->model->find_all_by_cms_section_id($section->find_by_url($this->action)->id, $options);
 		$this->filter_block_partial = $this->render_partial("filter_block");
 		$this->list = $this->render_partial("list");
 	}
 	
 	public function index() {
 	  if(!$page = url("page")) $page=1;
-		$this->set_order();
 		$this->display_action_name = 'List Items';
 	  $options = array("order"=>"published DESC", "page"=>$page, "per_page"=>10);
 		$this->all_rows = $this->model->find_all($options);
