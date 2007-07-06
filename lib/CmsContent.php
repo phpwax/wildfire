ed917e36cdd6df05d753e78ec1639b838991b51e
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-07-06 11:22:24 +0000

    debug
    git-svn-id: svn://php-wax.com/svn/plugins/cms/trunk@3263 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- lib/CmsContent.php
+++ lib/CmsContent.php
@@ -94,10 +94,24 @@ class CmsContent extends WXActiveRecord {
     exit;
   }
   
-  public function uniord($u) {
-      $c = unpack("N", mb_convert_encoding($u, 'UCS-4BE', 'UTF-8'));
-      return $c[1];
-    }
+  public function uniord($c) {
+    $ud = 0;
+    if (ord($c{0})>=0 && ord($c{0})<=127)
+     $ud = ord($c{0});
+    if (ord($c{0})>=192 && ord($c{0})<=223)
+     $ud = (ord($c{0})-192)*64 + (ord($c{1})-128);
+    if (ord($c{0})>=224 && ord($c{0})<=239)
+     $ud = (ord($c{0})-224)*4096 + (ord($c{1})-128)*64 + (ord($c{2})-128);
+    if (ord($c{0})>=240 && ord($c{0})<=247)
+     $ud = (ord($c{0})-240)*262144 + (ord($c{1})-128)*4096 + (ord($c{2})-128)*64 + (ord($c{3})-128);
+    if (ord($c{0})>=248 && ord($c{0})<=251)
+     $ud = (ord($c{0})-248)*16777216 + (ord($c{1})-128)*262144 + (ord($c{2})-128)*4096 + (ord($c{3})-128)*64 + (ord($c{4})-128);
+    if (ord($c{0})>=252 && ord($c{0})<=253)
+     $ud = (ord($c{0})-252)*1073741824 + (ord($c{1})-128)*16777216 + (ord($c{2})-128)*262144 + (ord($c{3})-128)*4096 + (ord($c{4})-128)*64 + (ord($c{5})-128);
+    if (ord($c{0})>=254 && ord($c{0})<=255) //error
+     $ud = false;
+   return $ud;
+  }
 	
 }
 
