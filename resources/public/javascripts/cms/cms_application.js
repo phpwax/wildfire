7d1d7f74e164638565373d2c0f5b2dc29bce0625
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-07-24 09:39:24 +0000

    started major cleanup of javascript
    git-svn-id: svn://php-wax.com/svn/plugins/cms/trunk@3430 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- resources/public/javascripts/cms/cms_application.js
+++ resources/public/javascripts/cms/cms_application.js
@@ -2,9 +2,9 @@
 
 function setup_preview(permalink, id, preview_pane) {
   $(document).ready(function() {
-      $(preview_pane).jqm({trigger: id, onShow: function(hash) {
+      $('#'+preview_pane).jqm({trigger: id, onShow: function(hash) {
         hash.w.show();
-        $(preview_pane).html("<iframe src='"+permalink+"' width='100%' height='100%' border='0' />");
+        $('#'+preview_pane).html("<iframe src='"+permalink+"' width='100%' height='100%' border='0' />");
       }});
       
   });
