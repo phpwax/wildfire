e99fa782b5888bbbe28d740d034cb0b4b86ea605
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-07-24 09:51:43 +0000

    started major cleanup of javascript
    git-svn-id: svn://php-wax.com/svn/plugins/cms/trunk@3432 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- resources/public/javascripts/cms/cms_application.js
+++ resources/public/javascripts/cms/cms_application.js
@@ -1,8 +1,8 @@
 /**** Add application wide javascripts below this point  ******/
 
-function setup_preview(permalink, id, preview_pane) {
+function setup_preview(permalink, trigger_id, preview_pane) {
   $(document).ready(function() {
-      $('#'+preview_pane).jqm({trigger: id, onShow: function(hash) {
+      $('#'+preview_pane).jqm({trigger: trigger_id, onShow: function(hash) {
         hash.w.show();
         $('#'+preview_pane).html("<iframe src='"+permalink+"' width='100%' height='100%' border='0' />");
       }});
