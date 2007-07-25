11fd1c90d1d85bb2580bbd880944b88897a5f0cc
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-07-25 15:47:29 +0000

    More work on major presentation/behaviour separation
    git-svn-id: svn://php-wax.com/svn/plugins/cms/trunk@3485 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- resources/public/javascripts/cms/cms_content.js
+++ resources/public/javascripts/cms/cms_content.js
@@ -52,7 +52,7 @@ $(document).ready(function(event) {
   });
   
   /*** Load in the first page of images via ajax ***/
-  $.get("/admin/files/browse_images/1/", function(response){$("#image_list").html(response.responseText);})
+  $.get("/admin/files/browse_images/1/", function(response){$("#image_list").html(response);})
   
 });
 
