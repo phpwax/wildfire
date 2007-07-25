a75a83603969ba27369c51e643444bb980602163
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-07-25 15:32:41 +0000

    More work on major presentation/behaviour separation
    git-svn-id: svn://php-wax.com/svn/plugins/cms/trunk@3483 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- resources/public/javascripts/cms/cms_content.js
+++ resources/public/javascripts/cms/cms_content.js
@@ -3,5 +3,28 @@ $(document).ready(function() {
     $("#page_tab_title").html($("#cms_content_title").val());
     $("#cms_content_title").keyup(function() {
       $("#page_tab_title").html($("#cms_content_title").val());
-    });    
+    });
+    $("#new_cat_create").click(function() {
+      $.ajax({ url: "/admin/content/new_category/?cat="+$("#new_cat").val(), 
+        complete: function(response){$("#category_list").html(response.responseText); initialise_draggables();}
+      });
+      return false;
+    });   
+    initialise_draggables();
 });
+
+function initialise_draggables() {
+  $(".category_tag").Draggable({ ghosting:	true, opacity: 	0.4, revert:true});
+  $("#cat_drop").Droppable(
+  	{ accept:     'category_tag', hoverclass:	'dropzone_active', tolerance:	'pointer',
+  		ondrop:	function(drag) {
+  		  $.post("/admin/content/add_category/"+content_page_id,{id: drag.id},
+  		  function(response){  $("#cat_dropzone").html(response); initialise_draggables();  });
+  	}
+  });
+  $(".category_trash_button").click(function(){
+    $.get("/admin/content/remove_category/"+content_page_id+"?cat="+this.id.substr(22),function(response){
+      $("#cat_dropzone").html(response); initialise_draggables();
+    })
+  });
+}
