de9b2f7ae84918573631965f3337073907a8585d
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-07-26 09:50:11 +0000

    More work on javascript separation
    git-svn-id: svn://php-wax.com/svn/plugins/cms/trunk@3494 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- resources/public/javascripts/cms/cms_content.js
+++ resources/public/javascripts/cms/cms_content.js
@@ -44,23 +44,21 @@ $(document).ready(function(event) {
   	{
   	  accept: 'drag_image', hoverclass: 'dropzone_active', tolerance: 'pointer',
   		ondrop:	function (drag) {
-  			$.post("/admin/content/add_image/"+content_page_id+"?order="+$(this).attr("id").substr(9), 
+  			$.post("/admin/content/add_image/"+content_page_id+"?order="+$(this).attr("id").substr(8), 
 				  {id: drag.id},
-          function(response){$("#"+this.id).html(response);}
+          function(response) {
+            $("#dropzone"+get_query_var(this.url,'order')).html(response);
+            initialise_images();
+          }
         )
   		}
   });
   
-  $(".image_trash_icon").click(function(){
-    $.get("/admin/content/remove_image/"+content_page_id+"?image="+this.id.substr(14),function(response){
-      this.parent().html(response);
-    })
-  });
+  
   
   /*** Load in the first page of images via ajax ***/
   $.get("/admin/files/browse_images/1/", function(response){
     $("#image_list").html(response);
-    initialise_images();
   });
   
   $(".paginate_images").click(function(){
@@ -69,10 +67,43 @@ $(document).ready(function(event) {
       initialise_images();
     })
   });
-  
+  initialise_images();
 });
 
 function initialise_images() {
   $(".drag_image").Draggable({ ghosting:	true, opacity: 	0.4, revert:true});
-  
+  $(".image_trash_icon").click(function(){
+    $.get("/admin/content/remove_image/"+content_page_id+"?image="+this.id.substr(13)+"&order="+this.parentNode.id.substr(8),function(response){
+      $("#dropzone"+get_query_var(this.url,'order')).html(response);
+      initialise_images();
+    })
+  });
+}
+
+
+function get_query_var(query, variable) {
+  var query=query.substring((query.indexOf("?")+1))
+  var vars = query.split("&");
+  for (var i=0;i<vars.length;i++) {
+    var pair = vars[i].split("=");
+    if (pair[0] == variable) {
+      return pair[1];
+    }
+  }
+}
+
+/******* Setup for the link modal window *******/
+
+$(document).ready(function() {
+  $('#link_dialog').jqm();
+});
+
+function cms_insert_url(type) {
+  if(type=='web') {
+    var theURL = prompt("Enter the URL for this link:", "http://");
+  } else var theURL = type;
+  if (theURL != null) {			
+		theIframe.contentWindow.document.execCommand("CreateLink", false, theURL);
+		theWidgEditor.theToolbar.setState("Link", "on");
+	}
 }
