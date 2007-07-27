4afcd65e74342c5ae64cc76cc71c6ab7ba368576
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-07-27 14:57:08 +0000

    Improvement to email sending interface
    git-svn-id: svn://php-wax.com/svn/plugins/cms/trunk@3542 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- lib/CMSSubscription.php
+++ lib/CMSSubscription.php
@@ -1,9 +1,5 @@
 <?php
 interface CMSSubscription {
-  
-  public $from_email;
-  public $from_name;
-  public $email_subject;
     
   public function get_email_content($handle);
   public function fetch_emails();
