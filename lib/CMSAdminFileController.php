bec1167d87c2234ce42aca9c9d3d571c5df99a84
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-08-02 13:15:42 +0000

    Started work on new file tree
    git-svn-id: svn://php-wax.com/svn/plugins/cms/trunk@3612 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- lib/CMSAdminFileController.php
+++ lib/CMSAdminFileController.php
@@ -71,6 +71,7 @@ class CMSAdminFileController extends CMSAdminComponent {
 	  $folder = $_POST["folder"];
 	  if(strpos("_", $folder)) $folder = str_replace("_", "/", $folder);
 	  $folder = PUBLIC_DIR.$folder;
+	  die($folder);
 	  $this->all_rows = $this->model->find_by_path($folder);
 	  $this->list = $this->render_partial("list");
 	}
