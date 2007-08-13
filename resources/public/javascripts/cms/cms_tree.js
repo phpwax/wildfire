4e1b51b299d04d644008cf01b6208a927160af3a
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-08-13 11:42:58 +0000

    Improvements to file system
    git-svn-id: svn://php-wax.com/svn/plugins/cms/trunk@3680 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- resources/public/javascripts/cms/cms_tree.js
+++ resources/public/javascripts/cms/cms_tree.js
@@ -142,7 +142,7 @@ $(document).ready(function() {
     
     $("#create_new_folder").click(function(){
       $.post("/admin/files/new_folder",
-       {folder: $("#new_folder_name").val()},
+       {folder: $("#new_folder_name").val(), parent: $("#cms_file_new_folder").val()},
        function(response) { $("#file_tree").html(response);}
       );
     });
