c7bf5def18edd30d3c60e62487c0ca14c3d2c2b0
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-08-16 13:40:29 +0000

    debugging meta content
    git-svn-id: svn://php-wax.com/svn/plugins/cms/trunk@3769 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- lib/CmsExtraContent.php
+++ lib/CmsExtraContent.php
@@ -3,7 +3,14 @@
 class CmsExtraContent extends WXActiveRecord {
   
 	public function __get($name) {
+	  $this->check_extra($name);
 	  parent::__get($name);
+	}
+	
+	public function check_extra($name) {
+	  $model = new CmsExtraContent;
+	  $result = $model->find_all_by_name($name);
+	  if($result) {print_r($result); exit;}
 	}
 }
 
