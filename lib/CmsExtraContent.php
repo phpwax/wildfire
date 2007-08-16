97dff5cf2431119f4a433cd9e6a10e91075235b7
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-08-16 13:53:30 +0000

    debugging meta content
    git-svn-id: svn://php-wax.com/svn/plugins/cms/trunk@3779 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- lib/CmsExtraContent.php
+++ lib/CmsExtraContent.php
@@ -8,7 +8,7 @@ class CmsExtraContent extends WXActiveRecord {
 	}
 	
 	public function check_extra($name) {
-	  if(!$this->cms_content_id) return false;
+	  if(!$this->id) return false;
 	  $sql = "SELECT value FROM cms_extra_content WHERE cms_content_id = ".$this->cms_content_id." AND `name` = $name";
 	  die($sql);
 	}
