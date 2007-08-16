4d93846c282b7fa960076e866c141a8700b04971
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-08-16 13:21:25 +0000

    debugging meta content
    git-svn-id: svn://php-wax.com/svn/plugins/cms/trunk@3761 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- lib/CMSAdminContentController.php
+++ lib/CMSAdminContentController.php
@@ -66,8 +66,9 @@ class CMSAdminContentController extends CMSAdminComponent {
 		$files = new CmsFile();
 		$this->all_links = $files->find_all_files();
 		$this->link_partial = $this->render_partial("apply_links");
-		$this->extra_content_partial = $this->render_partial("extra_content");
 		parent::edit();
+		$this->extra_content_partial = $this->render_partial("extra_content");
+		$this->form = $this->render_partial("form");
 	}
 	
 	public function create() {
