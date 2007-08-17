58e7c136efecbb1a00b8c1113e540a767da9a8da
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-08-17 13:25:57 +0000

    a very useful find_with_extra_content methhod
    git-svn-id: svn://php-wax.com/svn/plugins/cms/trunk@3815 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- lib/CmsContent.php
+++ lib/CmsContent.php
@@ -148,10 +148,7 @@ class CmsContent extends WXActiveRecord {
   public function find_with_extra_content($name, $params=array()) {
     $params["join"]=array("table"=>"cms_extra_content", "lhs"=>"id", "rhs"=>"cms_content_id");
     if($params["conditions"]) $params["conditions"].="AND name='$name'";
-    else $params["conditions"]= "name = $name";
-    if(!empty($params['join'])) {
-      print_r($params);exit;
-    }
+    else $params["conditions"]= "name = '$name'";
     return $this->find_all($params);
   }
   
