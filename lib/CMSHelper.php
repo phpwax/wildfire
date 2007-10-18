e8560cf30d9b39ca44f20753941b87f8f9ea5685
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-10-18 09:58:49 +0000

    Image randomiser helper
    git-svn-id: svn://php-wax.com/svn/plugins/cms/trunk@4038 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- lib/CMSHelper.php
+++ lib/CMSHelper.php
@@ -143,6 +143,7 @@ class CMSHelper extends WXHelpers {
 	
 	public function random_image($directory="images", $alt="") {
 	  $images = File::list_images_recursive(PUBLIC_DIR.$directory);
+	  print_r($images); exit;
 	  $image = $images[rand(0, count($images))];
 	  return "<img src='/".$directory."/".$image."' alt='".$alt."' />";
 	}
