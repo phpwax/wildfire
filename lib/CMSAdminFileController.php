7c2fcdb332549fc1df3c539abb9f7077db594b2d
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-11-15 17:13:42 +0000

    Ross: Started working on draggable files
    git-svn-id: svn://php-wax.com/svn/plugins/cms/trunk@4069 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- lib/CMSAdminFileController.php
+++ lib/CMSAdminFileController.php
@@ -145,7 +145,7 @@ class CMSAdminFileController extends CMSAdminComponent {
   	// Generates a valid XHTML list of all directories, sub-directories, and files in $directory
   	// Remove trailing slash
   	if( substr($directory, -1) == "/" ) $directory = substr($directory, 0, strlen($directory) - 1);
-  	$code = "<ul id='php-file-tree'><li class='pft-directory'>";
+  	$code = "<ul id='php-file-tree'><li class='pft-directory selected_folder'>";
   	$code .= "<a href='#' id='".$this->unslashify($directory)."' class='tree_folder'>Your Folder</a>";
   	$code .= $this->file_tree_dir($directory, $return_link, $extensions);
   	$code .= "</li></ul>";
