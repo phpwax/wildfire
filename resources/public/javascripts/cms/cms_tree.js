77f70c8053ede14bddfa1e58d1e21bf07ad5b752
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-11-16 14:44:26 +0000

    Ross: Work on improvements to filesystem
    git-svn-id: svn://php-wax.com/svn/plugins/cms/trunk@4122 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- resources/public/javascripts/cms/cms_tree.js
+++ resources/public/javascripts/cms/cms_tree.js
@@ -106,9 +106,6 @@ function droppable_folders() {
     }
   });
   $(".tree_folder span").editInPlace({
-      url: "/admin/files/rename_folder/"+$(this).parent().id,
-      params: "ajax=yes",
-      save_button: "",
-      cancel_button: ""
+      url: "/admin/files/rename_folder/"+$(this).parent().id
     });
 }
