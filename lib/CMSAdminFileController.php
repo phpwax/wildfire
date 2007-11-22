e52cea517f8db6ff846846a00ee4cb16f39b486b
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-11-22 11:54:43 +0000

    Ross: Work on improvements to filesystem
    git-svn-id: svn://php-wax.com/svn/plugins/cms/trunk@4144 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- lib/CMSAdminFileController.php
+++ lib/CMSAdminFileController.php
@@ -234,7 +234,7 @@ class CMSAdminFileController extends CMSAdminComponent {
 	public function rename_folder() {
 	  $this->use_layout = false;
 	  $orig = str_replace("_","/",$_POST["old_name"]);
-	  die(strrpos($orig, "/"));
+	  die(strpos($orig, "/"));
 	  
 	  $new = substr(0, strrpos($orig, "/"), $orig);
 	  $new = $new.$_POST['new_name'];
