24c21d42287c7543b83f62177155758db72a1768
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-11-22 16:25:19 +0000

    Ross: Work on improvements to filesystem
    git-svn-id: svn://php-wax.com/svn/plugins/cms/trunk@4158 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- resources/public/javascripts/cms/cms_tree.js
+++ resources/public/javascripts/cms/cms_tree.js
@@ -1,50 +1,6 @@
 $(document).ready(function() {
-		tree = $('#php-file-tree');
-		$('li', tree.get(0)).each(
-			function()
-			{
-				subbranch = $('ul', this);
-				if (subbranch.size() > 0) {
-					if (subbranch.eq(0).css('display') == 'none') {
-						$(this).prepend('<img src="/images/cms/bullet_toggle_plus.gif" width="9" height="9" class="expandImage" />');
-					} else {
-						$(this).prepend('<img src="/images/cms/bullet_toggle_minus.gif" width="9" height="9" class="expandImage" />');
-					}
-				} else {
-					$(this).prepend('<img src="/images/cms/spacer.gif" width="9" height="9" class="expandImage" />');
-				}
-			}
-		);
-		$('img.expandImage', tree.get(0)).click(
-			function()
-			{
-				if (this.src.indexOf('spacer') == -1) {
-					subbranch = $('ul', this.parentNode).eq(0);
-					if (subbranch.css('display') == 'none') {
-						subbranch.show();
-						this.src = '/images/cms/bullet_toggle_minus.gif';
-					} else {
-						subbranch.hide();
-						this.src = '/images/cms/bullet_toggle_plus.gif';
-					}
-				}
-			}
-		);
 		
-		
-    $(".tree_folder").click(function(){ 
-      $.post("/admin/files/fetch_folder", 
-			  {folder: this.id},
-        function(response) {
-          $("#file_tree_files").html(response);
-          draggable_files();
-          droppable_folders();
-        }
-      );
-      $(".pft-directory").removeClass("selected_folder");
-      $(this).parent().addClass("selected_folder");
-    });
-    
+    initialise_tree();
     $("#create_new_folder").click(function(){
       $.post("/admin/files/new_folder",
        {folder: $("#new_folder_name").val(), parent: $("#cms_file_new_folder").val()},
@@ -62,6 +18,55 @@ $(document).ready(function() {
     draggable_files();
     droppable_folders();
 });
+
+
+function initialise_tree() {
+  tree = $('#php-file-tree');
+	$('li', tree.get(0)).each(
+		function()
+		{
+			subbranch = $('ul', this);
+			if (subbranch.size() > 0) {
+				if (subbranch.eq(0).css('display') == 'none') {
+					$(this).prepend('<img src="/images/cms/bullet_toggle_plus.gif" width="9" height="9" class="expandImage" />');
+				} else {
+					$(this).prepend('<img src="/images/cms/bullet_toggle_minus.gif" width="9" height="9" class="expandImage" />');
+				}
+			} else {
+				$(this).prepend('<img src="/images/cms/spacer.gif" width="9" height="9" class="expandImage" />');
+			}
+		}
+	);
+	$('img.expandImage', tree.get(0)).click(
+		function()
+		{
+			if (this.src.indexOf('spacer') == -1) {
+				subbranch = $('ul', this.parentNode).eq(0);
+				if (subbranch.css('display') == 'none') {
+					subbranch.show();
+					this.src = '/images/cms/bullet_toggle_minus.gif';
+				} else {
+					subbranch.hide();
+					this.src = '/images/cms/bullet_toggle_plus.gif';
+				}
+			}
+		}
+	);
+	
+	
+  $(".tree_folder").click(function(){ 
+    $.post("/admin/files/fetch_folder", 
+		  {folder: this.id},
+      function(response) {
+        $("#file_tree_files").html(response);
+        draggable_files();
+        droppable_folders();
+      }
+    );
+    $(".pft-directory").removeClass("selected_folder");
+    $(this).parent().addClass("selected_folder");
+  });
+}
 
 function draggable_files() {
   $(".file_preview").Draggable({
@@ -108,8 +113,7 @@ function droppable_folders() {
         			  {},
                 function(response) {
                   $("#file_tree").html(response);
-                  droppable_folders();
-                  draggable_files();
+                  initialise_tree();
                 }
               );
             }
