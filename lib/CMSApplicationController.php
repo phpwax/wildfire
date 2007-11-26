41304c6656c4698d79dc846a03778140c94222fe
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-11-26 10:34:36 +0000

    Latest site developments.... ready to go beta.
    git-svn-id: svn://php-wax.com/svn/plugins/cms/trunk@4187 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- lib/CMSApplicationController.php
+++ lib/CMSApplicationController.php
@@ -62,6 +62,8 @@ class CmsApplicationController extends WXControllerBase{
 	  
 	  $this->show_image = new CmsFile($this->route_array[0]);
     $source = $this->show_image->path.$this->show_image->filename;
+    $relative = strstr($source, "public/");
+    die($relative);
     $file = CACHE_DIR.$this->route_array[0]."_".$this->route_array[1];
 	  if(!is_readable($file)) File::resize_image($source, $file, $size, false, $this->force_image_width);	  
 	  if($this->image = File::display_image($file) ) return true;
