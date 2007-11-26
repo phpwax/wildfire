cb89f19261413df8e6fa3ffffa1c176e657dce69
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-11-26 11:23:19 +0000

    Ross: Work on improvements to filesystem
    git-svn-id: svn://php-wax.com/svn/plugins/cms/trunk@4193 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- lib/CMSApplicationController.php
+++ lib/CMSApplicationController.php
@@ -65,9 +65,9 @@ class CmsApplicationController extends WXControllerBase{
     $relative = strstr($source, "public/");
     $relative = str_replace("public/", "", $relative);
     $source = PUBLIC_DIR.$relative;
-    error_log($source);
     $file = CACHE_DIR.$this->route_array[0]."_".$this->route_array[1];
-	  if(!is_readable($file)) File::resize_image($source, $file, $size, false, $this->force_image_width);	  
+	  if(!is_readable($file)) File::resize_image($source, $file, $size, false, $this->force_image_width);
+	  error_log($file); 
 	  if($this->image = File::display_image($file) ) return true;
 	  return false;
 	}
