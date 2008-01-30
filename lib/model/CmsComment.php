2d365c107a27f2d7b73e28b4ff31c43921677dbf
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2008-01-30 22:27:53 +0000

    Continued work on comments system
    git-svn-id: svn://php-wax.com/svn/plugins/cms/trunk@4414 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- lib/model/CmsComment.php
+++ lib/model/CmsComment.php
@@ -5,7 +5,16 @@ class CmsComment extends WXActiveRecord {
 	public $status_options = array("0"=>"Unapproved", "1"=>"Approved", "2"=>"Spam"); 
 	
 
+  public function validations() {
+    $this->valid_required("author_name");
+    $this->valid_required("author_email");
+    $this->valid_format("author_email", "email");
+  }
   
+  
+  public function before_save() {
+    $this->author_ip = $_SERVER["REMOTE_ADDR"];
+  }
 
 
 }
