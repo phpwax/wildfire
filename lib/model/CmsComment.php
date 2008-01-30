b261238e1f187a3dcd87d72924d799e48dd51161
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2008-01-30 22:43:27 +0000

    Continued work on comments system
    git-svn-id: svn://php-wax.com/svn/plugins/cms/trunk@4416 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- lib/model/CmsComment.php
+++ lib/model/CmsComment.php
@@ -17,7 +17,18 @@ class CmsComment extends WXActiveRecord {
     $this->time = date("Y-m-d H:i:s");
   }
   
-  
+  function time_ago() {
+    $ts = time() - strtotime(str_replace("-","/",$this->time));
+    if($ts>31536000) $val = round($ts/31536000,0).' year';
+    else if($ts>2419200) $val = round($ts/2419200,0).' month';
+    else if($ts>604800) $val = round($ts/604800,0).' week';
+    else if($ts>86400) $val = round($ts/86400,0).' day';
+    else if($ts>3600) $val = round($ts/3600,0).' hour';
+    else if($ts>60) $val = round($ts/60,0).' minute';
+    else $val = $ts.' second';
+    if($val>1) $val .= 's';
+    return $val;
+  }
 
 
 }
