5e92cf7f8c77990c6cd31dee9a3c09a6c415a3cc
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2008-02-24 14:03:22 +0000

    Improved text conversion
    git-svn-id: svn://php-wax.com/svn/plugins/cms/trunk@4536 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- lib/filters/CmsTextFilter.php
+++ lib/filters/CmsTextFilter.php
@@ -10,7 +10,7 @@ class CmsTextFilter  {
   
   static public $filters = array(
     "before_save"=>array("clean_word", "strip_attributes", "correct_entities", "strip_slashes"),
-    "before_output"=> array("first_para_hook", "no_widows", "ampersand_hook", "strip_slashes", "nice_quotes", "yt_video", "nl2p")
+    "before_output"=> array("first_para_hook", "no_widows", "ampersand_hook", "strip_slashes", "nice_quotes", "yt_video")
   );
   
   static public function add_filter($trigger, $method) {
@@ -106,11 +106,41 @@ class CmsTextFilter  {
     return preg_replace("/<!--yt_video-->([a-zA-Z\-0-9]*)<!--\/yt_video-->/", $replace, $text);
   }
   
-  static public function nl2p($text) {
-    return "<p>" . str_replace("
+  static public function nl2p($pee) {
+    $pee = $pee . "\n"; // just to make things a little easier, pad the end
+  	$pee = preg_replace('|<br />\s*<br />|', "\n\n", $pee);
+  	// Space things out a little
+  	$allblocks = '(?:table|thead|tfoot|caption|colgroup|tbody|tr|td|th|div|dl|dd|dt|ul|ol|li|pre|select|form|map|area|blockquote|address|math|style|input|p|h[1-6]|hr)';
+  	$pee = preg_replace('!(<' . $allblocks . '[^>]*>)!', "\n$1", $pee);
+  	$pee = preg_replace('!(</' . $allblocks . '>)!', "$1\n\n", $pee);
+  	$pee = str_replace(array("\r\n", "\r"), "\n", $pee); // cross-platform newlines
+  	if ( strpos($pee, '<object') !== false ) {
+  		$pee = preg_replace('|\s*<param([^>]*)>\s*|', "<param$1>", $pee); // no pee inside object/embed
+  		$pee = preg_replace('|\s*</embed>\s*|', '</embed>', $pee);
+  	}
+  	$pee = preg_replace("/\n\n+/", "\n\n", $pee); // take care of duplicates
+  	$pee = preg_replace('/\n?(.+?)(?:\n\s*\n|\z)/s', "<p>$1</p>\n", $pee); // make paragraphs, including one at the end
+  	$pee = preg_replace('|<p>\s*?</p>|', '', $pee); // under certain strange conditions it could create a P of entirely whitespace
+  	$pee = preg_replace('!<p>([^<]+)\s*?(</(?:div|address|form)[^>]*>)!', "<p>$1</p>$2", $pee);
+  	$pee = preg_replace( '|<p>|', "$1<p>", $pee );
+  	$pee = preg_replace('!<p>\s*(</?' . $allblocks . '[^>]*>)\s*</p>!', "$1", $pee); // don't pee all over a tag
+  	$pee = preg_replace("|<p>(<li.+?)</p>|", "$1", $pee); // problem with nested lists
+  	$pee = preg_replace('|<p><blockquote([^>]*)>|i', "<blockquote$1><p>", $pee);
+  	$pee = str_replace('</blockquote></p>', '</p></blockquote>', $pee);
+  	$pee = preg_replace('!<p>\s*(</?' . $allblocks . '[^>]*>)!', "$1", $pee);
+  	$pee = preg_replace('!(</?' . $allblocks . '[^>]*>)\s*</p>!', "$1", $pee);
+  	if ($br) {
+  		$pee = preg_replace('/<(script|style).*?<\/\\1>/se', 'str_replace("\n", "<WPPreserveNewline />", "\\0")', $pee);
+  		$pee = preg_replace('|(?<!<br />)\s*\n|', "<br />\n", $pee); // optionally make line breaks
+  		$pee = str_replace('<WPPreserveNewline />', "\n", $pee);
+  	}
+  	$pee = preg_replace('!(</?' . $allblocks . '[^>]*>)\s*<br />!', "$1", $pee);
+  	$pee = preg_replace('!<br />(\s*</?(?:p|li|div|dl|dd|dt|th|pre|td|ul|ol)[^>]*>)!', '$1', $pee);
+  	if (strpos($pee, '<pre') !== false)
+  		$pee = preg_replace_callback('!(<pre.*?>)(.*?)</pre>!is', 'clean_pre', $pee );
+  	$pee = preg_replace( "|\n</p>$|", '</p>', $pee );
 
-  ", "</p>
-  <p>", $text) . "</p>";
+  	return $pee;
   }
 
 
