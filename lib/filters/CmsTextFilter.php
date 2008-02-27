afe0c9610890e6b7d19aee93fc06ad8a1127a419
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2008-02-27 01:21:25 +0000

    More filter goodness
    git-svn-id: svn://php-wax.com/svn/plugins/cms/trunk@4572 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- lib/filters/CmsTextFilter.php
+++ lib/filters/CmsTextFilter.php
@@ -9,7 +9,7 @@ class CmsTextFilter  {
   
   
   static public $filters = array(
-    "before_save"=>array("correct_entities", "clean_word", "strip_attributes", "strip_slashes"),
+    "before_save"=>array("correct_entities", "clean_word", "strip_attributes", "strip_slashes", "convert_chars"),
     "before_output"=> array("first_para_hook", "no_widows", "ampersand_hook", "strip_slashes", "nice_quotes", "yt_video")
   );
   
@@ -141,6 +141,57 @@ class CmsTextFilter  {
   	$pee = preg_replace( "|\n</p>$|", '</p>', $pee );
 
   	return $pee;
+  }
+  
+  public function convert_chars($content, $flag = 'obsolete') {
+  	// Translation of invalid Unicode references range to valid range
+  	$wp_htmltranswinuni = array(
+  	'&#128;' => '&#8364;', // the Euro sign
+  	'&#129;' => '',
+  	'&#130;' => '&#8218;', // these are Windows CP1252 specific characters
+  	'&#131;' => '&#402;',  // they would look weird on non-Windows browsers
+  	'&#132;' => '&#8222;',
+  	'&#133;' => '&#8230;',
+  	'&#134;' => '&#8224;',
+  	'&#135;' => '&#8225;',
+  	'&#136;' => '&#710;',
+  	'&#137;' => '&#8240;',
+  	'&#138;' => '&#352;',
+  	'&#139;' => '&#8249;',
+  	'&#140;' => '&#338;',
+  	'&#141;' => '',
+  	'&#142;' => '&#382;',
+  	'&#143;' => '',
+  	'&#144;' => '',
+  	'&#145;' => '&#8216;',
+  	'&#146;' => '&#8217;',
+  	'&#147;' => '&#8220;',
+  	'&#148;' => '&#8221;',
+  	'&#149;' => '&#8226;',
+  	'&#150;' => '&#8211;',
+  	'&#151;' => '&#8212;',
+  	'&#152;' => '&#732;',
+  	'&#153;' => '&#8482;',
+  	'&#154;' => '&#353;',
+  	'&#155;' => '&#8250;',
+  	'&#156;' => '&#339;',
+  	'&#157;' => '',
+  	'&#158;' => '',
+  	'&#159;' => '&#376;'
+  	);
+
+
+  	// Converts lone & characters into &#38; (a.k.a. &amp;)
+  	$content = preg_replace('/&([^#])(?![a-z1-4]{1,8};)/i', '&#038;$1', $content);
+
+  	// Fix Word pasting
+  	$content = strtr($content, $wp_htmltranswinuni);
+
+  	// Just a little XHTML help
+  	$content = str_replace('<br>', '<br />', $content);
+  	$content = str_replace('<hr>', '<hr />', $content);
+
+  	return $content;
   }
 
 
