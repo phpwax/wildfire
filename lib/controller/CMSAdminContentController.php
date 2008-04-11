10905de32a4b9fee88a3f135a6dad41ead23f993
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2008-04-11 11:31:33 +0000

    Removed dependency on deprecated JavascriptHelper
    git-svn-id: svn://php-wax.com/svn/plugins/cms/trunk@5283 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- lib/controller/CMSAdminContentController.php
+++ lib/controller/CMSAdminContentController.php
@@ -81,7 +81,7 @@ class CMSAdminContentController extends CMSAdminComponent {
 	
 	public function edit() {
 		$this->page = new $this->model_class($this->route_array[0]);
-		$this->attached_images = $this->page->images;
+		if(!$this->attached_images = $this->page->images) $this->attached_images=array();
 		if(!$this->attached_categories = $this->page->categories) $this->attached_categories= array();
 		$cat = new CmsCategory;
 		if(!$this->all_categories = $cat->find_all(array("order"=>"parent_id ASC, name ASC"))) $this->all_categories=array();
