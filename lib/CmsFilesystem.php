ea82f4cbc31d5ec63ef92f95fa2df9b557077070
Ross Riley <ross@oneblackbear.com>
2008-04-14 17:45:36 +0100

    Work on new filesystem

--- lib/CmsFilesystem.php
+++ lib/CmsFilesystem.php
@@ -176,7 +176,7 @@ class CmsFilesystem {
   	$output = '';
   	$this->jsonStart();
     if($path == '' || $path == '/'){
-  		$this->jsonAdd("\"displayname\":\"$defaultDisplay\",\"scheme\":\"Filestore\",\"type\": \"directory\", \"name\": \"filestore\", \"path\": \"/{$this->relativepath}\",\"virtual\":\"true\"");
+  		$this->jsonAdd("\"displayname\":\"$defaultDisplay\",\"scheme\":\"Filestore\",\"type\": \"directory\", \"name\": \"{$this->defaultDisplay}\", \"path\": \"/{$this->relativepath}\",\"virtual\":\"true\"");
   		$output .= $this->jsonReturn('getFolder');
   	} else {
 	
