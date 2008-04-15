6c77ba539b58d9e167cc0164fa469c439bdc34b7
Ross Riley <ross@oneblackbear.com>
2008-04-15 01:07:10 +0100

    Work on new filesystem

--- lib/CmsFilesystem.php
+++ lib/CmsFilesystem.php
@@ -453,8 +453,9 @@ class CmsFilesystem {
     	}
     }
     if(isset($files)){
-      error_log(print_r($files, 1));
       $ak = array_keys($files);
+      error_log(print_r($files, 1));
+      
   	  for($i=0;$i < sizeof($ak);$i++){
     	  $filename = $ak[$i];
     	  if($files[$filename][1]!='done'){
