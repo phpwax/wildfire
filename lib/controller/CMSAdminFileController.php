88cd7097fa7d74809b9c2ec0abbae10d770a92d1
Ross Riley <ross@oneblackbear.com>
2008-04-15 18:03:45 +0100

    Work on new image browsing

--- lib/controller/CMSAdminFileController.php
+++ lib/controller/CMSAdminFileController.php
@@ -69,6 +69,7 @@ class CMSAdminFileController extends CMSAdminComponent {
 				file_put_contents($source, $icon_file);
 			}
 		}
+		echo "About to resize $source  to  $file   at   $size";
     if(!is_file($file) || !is_readable($file)) {
       File::resize_image($source, $file, $size);
     }
