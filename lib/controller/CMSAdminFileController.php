58ac6d9d9019d1b09c4f6a8b47e62faffac8138f
Ross Riley <ross@oneblackbear.com>
2008-04-15 14:10:58 +0100

    Cleanup after filesystem changes

--- lib/controller/CMSAdminFileController.php
+++ lib/controller/CMSAdminFileController.php
@@ -27,53 +27,9 @@ class CMSAdminFileController extends CMSAdminComponent {
 		$this->sub_links = array();
 		$this->sub_links[""] = "Manage Your Files";
 		$this->sub_links["upload"]="Advanced File Upload";
-		$this->sub_links['synchronise'] = "Synchronise";
 	}
 	
-	public function synchronise(){
-		//directory to scan round
-		$directory = WAX_ROOT . $this->model->file_base;
-		//arrays
-		$hdd_files = array();
-		$db_files = array();
-		//find all files in the directory structure
-		$iter = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($directory), true);
-		foreach ( $iter as $file ) {
-			$path = $iter->getPath()."/".basename($file);
-			if(is_file($path) ) $hdd_files[] = $path;
-		}
-		//find all db file records
-		$files = $this->model->find_all();
-		foreach($files as $file) $db_files[] = $file->path . $file->filename;
-		//difference between the two
-		$difference = array_diff($hdd_files, $db_files);
-		//loop round the differences to see if its a file or not
-		foreach($difference as $diff){
-			if(is_file($diff)) $missing_from_db[] = $diff;
-			else $missing_from_hdd[] = $diff;
-		}
-		$saved = 0;
-		$failed = 0;	
-		//loop round each missing file and add it into db
-		foreach((array) $missing_from_db as $file){
-			$path = substr($file, 0, strrpos($file, "/")+1 );
-			$filename = substr($file, strrpos($file, "/")+1 );
-			$info = getimagesize($file);
-			$model = new $this->model_class;
-			$model->path = str_ireplace(WAX_DIR, "", $path);
-			$model->filename = $filename;
-			$model->type = $info['mime'];
-			if($model->save()) $saved ++;
-			else $failed ++;
-		}	
-		$total_missing = count($missing_from_db);
-		$msg = $total_missing . " files missing from DB";
-		if($saved == $total_missing) $msg .= " - all saved";
-		else $msg .= " - $saved saved";
-		if(count($missing_from_hdd)>0) $msg .= "<br />".count($missing_from_hdd)." files missing from hdd";
-		Session::add_message($msg);
-		$this->redirect_to("/admin/files/");
-	}
+	
 	
 	public function fs() {
 	  $this->use_layout=false;
@@ -83,27 +39,9 @@ class CMSAdminFileController extends CMSAdminComponent {
 	  $fs->dispatch($action, array_merge($_GET, $_POST));
 	}
 	
-	public function file_info() {
-	  $this->use_layout=false;
-	  $this->accept_routes=1;
-	  $this->show_file = new $this->model_class($this->route_array[0]);
-		/* CHANGED - now works with relative file paths */
-		$path = WAX_ROOT . $this->show_file->file_path . $this->show_file->filename;
-	  $this->file_size = 0;
-	  if(is_readable($path)) $this->file_size = floor( filesize($path) / 1024)." Kb";
-	}
 	
-	public function index() {
-	  parent::index();
-	  $this->all_rows=array();
-	  $folder = $this->model->base_dir."/";
-	  $sql = "SELECT * FROM cms_file WHERE SUBSTRING_INDEX(path, 'public/', -1) = '".$folder."' ORDER BY filename ASC";
-	  $this->all_rows = $this->model->find_by_sql($sql);
-	  $this->file_tree = $this->file_tree(PUBLIC_DIR."files/", "test");
-	  $this->list = $this->render_partial("list");
 	
-		$this->upload_to = $this->render_partial('upload_to');
-	}
+	
 	
 	
 	public function show_image() {
@@ -164,31 +102,6 @@ class CMSAdminFileController extends CMSAdminComponent {
 		parent::edit();
 	}
 	
-
-	public function fetch_folder() {
-	  $this->use_layout=false;
-	  $folder = $_POST["folder"] . "/";
-		if($folder == "/") $folder = $this->model->base_dir."/";
-	  if(strpos($folder,"~")) $folder = str_replace("~", "/", $folder);
-	  $sql = "SELECT * FROM cms_file WHERE path = 'public/".$folder."' ORDER BY filename ASC";
-	  $this->all_rows = $this->model->find_by_sql($sql);
-	  //$this->all_rows = $this->model->find_all_by_path($folder, array("order"=>"filename ASC"));
-	  $this->list = $this->render_partial("list");
-	}
-	
-	public function new_folder() {
-	  $this->use_layout=false;
-	 	if($_POST["parent"]) $parent = str_replace("~","/",$_POST["parent"]); else $parent = $this->model->base_dir;
-	  $folder = PUBLIC_DIR.$parent."/".$_POST["folder"];
-	  if(!is_dir($folder)) mkdir($folder);
-	  else $this->warning = "Folder already exists";
-    $this->file_tree = $this->file_tree(PUBLIC_DIR.$this->model->base_dir, "test",array(),false);
-	}
-	
-	public function upload_to(){
-		$this->use_layout = false;
-		$this->upload_to = $this->render_partial('upload_to');
-	}
 	
 	public function browse_images() {
 		$this->use_layout=false;
@@ -216,74 +129,6 @@ class CMSAdminFileController extends CMSAdminComponent {
     $this->image = new $this->model_class(url('id'));
   }
   
-  public function upload() {
-    if($_FILES) {
-      foreach($_FILES as $newfile) {
-        foreach($newfile as $k=>$val) {
-          $newfile_fix[$k]['filename']=$val;
-        }
-        $path = str_replace("files/", "", $_POST['cms_file']['folder']);
-        $path = str_replace("files", "", $path );
-        if(substr($path, -1) !="/") $path = $path."/";
-        $file = new CmsFile;
-        $file->filename = $newfile_fix;
-        $file->file_base.= $path;
-        $file->caption = $_POST["cms_file"]["caption"];
-        $file->save();
-      }
-      echo "SUCCESS"; exit;
-    }
-    
-  }
-  
-  public function file_tree($directory, $return_link, $extensions = array(), $collapse=true) {
-  	// Generates a valid XHTML list of all directories, sub-directories, and files in $directory
-  	// Remove trailing slash
-  	if( substr($directory, -1) == "/" ) $directory = substr($directory, 0, strlen($directory) - 1);
-  	$code = "<ul id='php-file-tree' class='filetree'><li class='tree_folder'>";
-  	$code .= "<span class='folder'><a href='#' id='".$this->unslashify($directory)."'>Your Folder</a></span>";
-  	$code .= $this->file_tree_dir($directory, $return_link, $extensions,true,$collapse);
-  	$code .= "</li></ul>";
-  	return $code;
-  }
-
-  public function file_tree_dir($directory, $return_link, $extensions = array(), $first_call = true, $collapse=true) {
-
-  	// Get and sort directories/files
-  	$file = scandir($directory);
-  	natcasesort($file);
-  	// Make directories first
-  	$files = $dirs = array();
-  	foreach($file as $this_file) {
-  	  if(substr($this_file,0,1)==".") continue;
-  		if( is_dir("$directory/$this_file" ) ) $dirs[] = $this_file; else $files[] = $this_file;
-  	}
-  	$file = array_merge($dirs, $files);
-
-  	// Filter unwanted extensions
-  	if( !empty($extensions) ) {
-  		foreach( array_keys($file) as $key ) {
-  			if( !is_dir("$directory/$file[$key]") ) {
-  				$ext = substr($file[$key], strrpos($file[$key], ".") + 1); 
-  				if( !in_array($ext, $extensions) ) unset($file[$key]);
-  			}
-  		}
-  	}
-  	if($collapse) $php_file_tree = "<ul class='collapsed'>";
-  	else $php_file_tree .= "<ul>";
-		foreach( $file as $this_file ) {
-			if( $this_file != "." && $this_file != ".." ) {
-				if( is_dir("$directory/$this_file") ) {
-					// Directory
-					$php_file_tree .= "<li class=\"folder\"><span class='folder'><a href=\"#\" id='".$this->unslashify($directory."/".$this_file)."' class='tree_folder'>" . htmlspecialchars($this_file) . "</a></span>";
-					$php_file_tree .= $this->file_tree_dir("$directory/$this_file", $return_link ,$extensions, $collapse);
-					$php_file_tree .= "</li>";
-				} 
-			}
-		}
-		$php_file_tree .= "</ul>";
-  	return $php_file_tree;
-  }
 
 	/**
 	* Save
@@ -309,64 +154,6 @@ class CMSAdminFileController extends CMSAdminComponent {
  		return false;
 	}
 	
-	protected function unslashify($path) {
-	  $path = str_replace(PUBLIC_DIR, "", $path);
-    return str_replace('/', '~', $path);
-	}
-	
-	public function move_file() {
-	  $this->use_view=false;
-	  $this->use_layout = false;
-	  $dest = "public/".str_replace("~","/",$_POST["folder"]);
-	  $file = new CmsFile($_POST["file_id"]);
-		$source = WAX_ROOT . $file->path. $file->filename;
-		$destination = WAX_ROOT. $dest."/".$file->filename;
-	  if(rename($source, $destination) ) {
-	    $file->path = $dest."/";
-	    $file->save();
-    }
-	}
-	
-	public function rename_folder() {
-	  $_POST['new_name'] = str_replace("~","-",$_POST['new_name']);
-	  $this->use_view = false;
-	  $this->use_layout = false;
-	  $orig = str_replace("~","/", $_POST["old_name"]);
-	  $directory_parts = explode("/", $orig);
-	  $oldname = array_pop($directory_parts);
-
-		if( ($oldname != $this->model->base_dir) && rename(PUBLIC_DIR.$orig."/", PUBLIC_DIR.implode("/",$directory_parts)."/".$_POST['new_name'] ) ) {
-	    $file = new CmsFile;
-	    $search = $orig;
-	    $files = $file->find_all(array("conditions"=>"path LIKE '%$search%'"));
-	    foreach($files as $file) {
-				$path = str_replace($orig, implode("/",$directory_parts)."/".$_POST['new_name'], $file->path);
-	      $file->path = $path;
-	      $file->save();
-	    }
-	  } 
-
-	}
-	
-	public function refresh_tree() {
-	  $this->use_layout = false;
-	  $this->all_rows = $this->model->find_all_by_path(PUBLIC_DIR.$this->model->base_dir."/", array("order"=>"filename ASC"));
-	  $this->file_tree = $this->file_tree(PUBLIC_DIR."files/", "test", array(), false);
-	}
-	
-	public function delete_folder() {
-	  $this->use_view = false;
-	  $this->use_layout = false;
-	  $folder = $orig = str_replace("~","/", $_POST["folder_name"]);		
-	  if(($folder != $this->model->base_dir) && File::recursively_delete(PUBLIC_DIR.$folder) ) {
-	    $search = PUBLIC_DIR.$folder;
-	    $file_model = new CmsFile;
-	    $files = $file_model->find_all(array("conditions"=>"path LIKE '%$search%'"));
-	    foreach($files as $file) {
-	      $file_model->delete($file->id);
-	    }
-	  }
-	}
 
 }
 ?>
