ae0b18782a414375634c26540fa11a23f9fa4cee
Ross Riley <ross@oneblackbear.com>
2008-04-15 17:58:36 +0100

    Work on new image browsing

--- lib/controller/CMSAdminFileController.php
+++ lib/controller/CMSAdminFileController.php
@@ -61,7 +61,8 @@ class CMSAdminFileController extends CMSAdminComponent {
 			if(!is_file($file) || !is_readable($file)) {
 				$icon_type = File::get_extension($img->filename);
 				$icon = PLUGIN_DIR."cms/resources/public/images/cms/"."cms-generic-icon-".strtolower($icon_type).".gif";
-				if(!$icon_file = file_get_contents($icon)) {
+				$icon_file = @file_get_contents($icon);
+				if(!$icon_file) {
 					$icon_file = PLUGIN_DIR."cms/resources/public/images/cms/"."cms-generic-icon.png";
 					$source = CACHE_DIR."cms-generic-icon.gif";
 				}
