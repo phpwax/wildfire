24ffc30d51fabdf20b9528446c8ed01ceadcae43
Ross Riley <ross@oneblackbear.com>
2008-05-19 23:52:39 +0100

    Work on FS Porting

--- lib/controller/CMSAdminFileController.php
+++ lib/controller/CMSAdminFileController.php
@@ -132,7 +132,8 @@ class CMSAdminFileController extends CMSAdminComponent {
     $files = $file->find_all();
     foreach($files as $file) {
       $new = new WildfireFile;
-      $new_file = $new->filter("filename = '{$file->filename}' AND rpath LIKE '%{$file->path}%'" )->first();
+      $s_path = str_replace("public/files/", "", $file->path);
+      $new_file = $new->filter("filename = '{$file->filename}' AND rpath LIKE '%{$s_path}%'" )->first();
       if($new_file) $new_file->oldid = $file->id;
       print_r($new_file); exit;
       //$new_file->save();
