a9c4380741947bf287332cbf1f3fee1e4457ae3d
Ross Riley <ross@oneblackbear.com>
2008-06-18 16:54:51 +0100

    Work on wysiwyg toolbar

--- resources/public/javascripts/cms/cms_content.js
+++ resources/public/javascripts/cms/cms_content.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
       return false;
     });   
     initialise_draggables();
-    $("#resizable_panel").resizable({ 
+    $(".widgIframe").resizable({ 
         handles: "s" 
     });
 });
