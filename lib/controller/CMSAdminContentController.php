f87a6e14d0fb5cac00f0345b5806ec95aef1f77c
Ross Riley <ross@oneblackbear.com>
2008-06-19 09:45:42 +0100

    debug test

--- lib/controller/CMSAdminContentController.php
+++ lib/controller/CMSAdminContentController.php
@@ -95,6 +95,7 @@ class CMSAdminContentController extends CMSAdminComponent {
 		$model = new CmsContent();
 		$model->status = 3;
 		$model->author_id = Session::get('loggedin_user');
+		$model->url = time();
 		print_r($model); exit;
 		$this->redirect_to("/admin/content/edit/".$model->save()->id);
 	}
