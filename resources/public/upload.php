33bcb38f70984ed9e0d0ea7f9e4a2c756e08d2e5
Ross Riley <ross@oneblackbear.com>
2008-06-20 18:11:25 +0100

    Work on advanced file upload

--- resources/public/upload.php
+++ resources/public/upload.php
@@ -6,9 +6,9 @@ if($_FILES) {
     $fs = new CmsFilesystem;
     $_FILES['upload'] = $_FILES["Filedata"];
     $fs->upload($path);
+    $fs->databaseSync($fs->defaultFileStore.$path, $path);
     $file = new WildfireFile;
-    $newfile = $file->filter(array("filename"=>$_FILES['upload']['name'], "rpath"=>$path));
-    
+    $newfile = $file->filter(array("filename"=>$_FILES['upload']['name'], "rpath"=>$path))->first();
     $newfile->description = $_POST["wildfire_file_description"];
     $newfile->save();
     echo "Uploaded";
