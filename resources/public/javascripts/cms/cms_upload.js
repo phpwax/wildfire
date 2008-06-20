886bb4df1a23eb46b0163ae80e513198d6a5bae5
Ross Riley <ross@oneblackbear.com>
2008-06-20 15:10:38 +0100

    Work on advanced file upload

--- resources/public/javascripts/cms/cms_upload.js
+++ resources/public/javascripts/cms/cms_upload.js
@@ -6,15 +6,16 @@ $(document).ready(function() {
 			upload_url: "/admin/files/upload",	// Relative to the SWF file
 			post_params: {"PHPSESSID" : "<?php echo session_id(); ?>"},
 			file_size_limit : "100 MB",
-			file_types : "*.*",
-			file_types_description : "All Files",
-			file_upload_limit : 100,
-			file_queue_limit : 0,
-			custom_settings : {
-				progressTarget : "fsUploadProgress",
-				cancelButtonId : "btnCancel"
-			},
-			debug: false,
+			file_size_limit : "100 MB",
+  		file_types : "*.*",
+  		file_types_description : "All Files",
+  		file_upload_limit : 100,
+  		file_queue_limit : 0,
+  		custom_settings : {
+  			progressTarget : "fsUploadProgress",
+  			cancelButtonId : "btnCancel"
+  		},
+  		debug: false,
 
 			// The event handler functions are defined in handlers.js
 			file_queued_handler : fileQueued,
