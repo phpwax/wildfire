10b7f97269608ff9a36ba45001d1933ed7bc291c
Ross Riley <ross@oneblackbear.com>
2008-06-20 15:13:24 +0100

    Work on advanced file upload

--- resources/public/javascripts/cms/cms_upload.js
+++ resources/public/javascripts/cms/cms_upload.js
@@ -1,12 +1,16 @@
 $(document).ready(function() {
   var swfu;
 
-		var settings = {
-			flash_url : "/image/cms/swfupload_f8.swf",
-			upload_url: "/admin/files/upload",	// Relative to the SWF file
-			post_params: {"PHPSESSID" : "<?php echo session_id(); ?>"},
-			file_size_limit : "100 MB",
-			file_size_limit : "100 MB",
+  SWFUpload.onload = function () {
+  	var settings = {
+  		flash_url : "/images/cms/swfupload_f8.swf",
+  		upload_url: "/admin/files/upload",	// Relative to the SWF file
+  		post_params: {
+  			"PHPSESSID" : "NONE",
+  			"HELLO-WORLD" : "Here I Am",
+  			".what" : "OKAY"
+  		},
+  		file_size_limit : "100 MB",
   		file_types : "*.*",
   		file_types_description : "All Files",
   		file_upload_limit : 100,
@@ -17,7 +21,7 @@ $(document).ready(function() {
   		},
   		debug: false,
 
-			// The event handler functions are defined in handlers.js
+  		// The event handler functions are defined in handlers.js
   		swfupload_loaded_handler : swfUploadLoaded,
   		file_queued_handler : fileQueued,
   		file_queue_error_handler : fileQueueError,
@@ -30,10 +34,11 @@ $(document).ready(function() {
   		queue_complete_handler : queueComplete,	// Queue plugin event
 
   		// SWFObject settings
-  		minimum_flash_version : "8",
+  		minimum_flash_version : "9.0.28",
   		swfupload_pre_load_handler : swfUploadPreLoad,
   		swfupload_load_failed_handler : swfUploadLoadFailed
-		};
+  	};
 
-		swfu = new SWFUpload(settings);
+  	swfu = new SWFUpload(settings);
+  }
 });
