4e67dd1af4e62f94ac423af3cb708b7b6ab92201
Ross Riley <ross@oneblackbear.com>
2008-06-23 17:48:27 +0100

    Fix to show_image

--- lib/controller/CMSApplicationController.php
+++ lib/controller/CMSApplicationController.php
@@ -117,8 +117,9 @@ class CmsApplicationController extends WXControllerBase{
   	$img = new WildfireFile($img_id);
 		/* CHANGED - allows for relative paths in db */
     $source = PUBLIC_DIR. $img->rpath.$img->filename;    
-    die($source);
+
 		$file = CACHE_DIR.$img_id."_".$img_size;
+		die($file);
 		$source=preg_replace("/[\s]/", "\ ", $source);
 		if(!File::is_image($source)){
 			if(!is_file($file) || !is_readable($file)) {
