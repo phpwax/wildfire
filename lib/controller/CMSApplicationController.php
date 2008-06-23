96f2c06b7cba9e529a27fca4a275600072113ca6
Ross Riley <ross@oneblackbear.com>
2008-06-23 20:06:34 +0100

    fix to loop in show_image

--- lib/controller/CMSApplicationController.php
+++ lib/controller/CMSApplicationController.php
@@ -104,6 +104,7 @@ class CmsApplicationController extends WXControllerBase{
 	
 	/* used by old and new */
 	public function show_image() {
+	  die("HERE");
 	  $options = WaxUrl::get_params();
 	  $img_id = WaxUrl::get("id");
 	  $img_size = $options["params"][0];
