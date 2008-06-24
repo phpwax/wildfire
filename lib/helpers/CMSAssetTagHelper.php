3ed0c1fa7e1342ad90759405945d9b81c2f0cf97
charles <charles@oneblackbear.com>
2008-06-24 17:42:18 +0100

    removed CMSAssetTagHelper functions - this is now defuncted

--- lib/helpers/CMSAssetTagHelper.php
+++ lib/helpers/CMSAssetTagHelper.php
@@ -1,31 +1,6 @@
 <?php
 /**
- *
- * @package PHP-Wax
- * @author Ross Riley
+ * NO LONGER USED - VERY VERY OLD SCHOOL
  **/
-
-/**
- *  Simple Helpers to create links to images/js/css
- */
-class CMSAssetTagHelper extends AssetTagHelper {
-
-    /**
-     *  @var string[]
-     */
-  public $javascript_default_sources = null;
-  static public $asset_server = false;
-
-
-  public function __construct() {
-    $this->javascript_default_sources =	array('prototype', 'builder','effects', 'dragdrop', 'controls', 'slider');
-    self::$asset_server = WXConfiguration::get("assets");
-  }
-  
-  public function cms_serve_asset($type, $namespace, $filename) {
-    if($server = self::$asset_server) $source .= "http://".$server;
-    $source .= "/$type/$namespace/$filename";
-    return $source;
-  }
-}
+class CMSAssetTagHelper extends AssetTagHelper {}
 ?>
