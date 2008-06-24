c2bfc7e19693372efd778711d5757e9527469faa
charles <charles@oneblackbear.com>
2008-06-24 17:37:56 +0100

    comments

--- lib/controller/CMSAdminCommentController.php
+++ lib/controller/CMSAdminCommentController.php
@@ -1,4 +1,7 @@
 <?php
+/**
+ * controller for comments
+ */
 
 class CMSAdminCommentController extends CMSAdminComponent {
 
@@ -15,11 +18,16 @@ class CMSAdminCommentController extends CMSAdminComponent {
   public $default_order = "time";
 	public $default_direction="DESC";
   
-
+	/**
+	 * add a new sub nav option
+	 */	
 	public function controller_global() {
     $this->sub_links["moderation"]="Comments in Moderation";
 	}
-	
+	/**
+	 * list page for this module 
+	 * shows only active comments (ie status =1) and paginates them
+	 **/
 	public function index( ) {
 		$this->set_order();
 		$this->display_action_name = 'List Comments';
@@ -28,7 +36,9 @@ class CMSAdminCommentController extends CMSAdminComponent {
 		$this->filter_block_partial = $this->render_partial("filter_block");
 		$this->list = $this->render_partial("list");
 	}
-	
+	/**
+	 * list page for all 'spam' comments (ie status=2)
+	 */	
 	public function moderation() {
 	  $this->use_view="index";
 	  $this->set_order();
@@ -38,7 +48,9 @@ class CMSAdminCommentController extends CMSAdminComponent {
 		$this->filter_block_partial = $this->render_partial("filter_block");
 		$this->list = $this->render_partial("list");
 	}
-	
+	/**
+	 * turn a comment into spam (no, not the meat product...)
+	 */	
 	public function spam() {
 	  $comment = new CmsComment(Resquest::get("id"));
 	  if($comment->update_attributes(array("status"=>"2")) ) {
@@ -46,7 +58,9 @@ class CMSAdminCommentController extends CMSAdminComponent {
 	  }
 	  $this->redirect_to(array("action"=>"index"));
 	}
-	
+	/**
+	 * approve a comment - so set its status to 1 
+	**/
 	public function approve() {
 	  $comment = new CmsComment(Resquest::get("id"));
 	  if($comment->update_attributes(array("status"=>"1")) ) {
