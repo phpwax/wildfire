f912bcea249ef4230237117a3b7bafbcdddd932b
charles <charles@oneblackbear.com>
2008-06-24 11:13:02 +0100

    more comments & removed order functions which are currently defunct

--- lib/controller/CMSAdminSectionController.php
+++ lib/controller/CMSAdminSectionController.php
@@ -13,33 +13,22 @@ class CMSAdminSectionController extends CMSAdminComponent {
   public $filter_columns = array("title");
 	public $order_by_columns = array("title","url");
 
+	/**
+	* create the tree structure used for the drop down section selection
+	**/
 	public function controller_global() {
 		$this->tree_collection = $this->model->sections_as_collection();
 	}
+	/**
+	 * index page - list of all sections
+	 * @return void
+	 */
 	
 	public function index() {
 		parent::index();
 		$this->all_rows = $this->model->find_ordered_sections();
 		$this->list = $this->render_partial("list");
 	}
-	
-	/*public function order() {
-		if($root_section = $this->model->find_by_parent_id(0)) {
-			$this->main_sections = $root_section->get_children("`order` ASC");
-		} else $this->main_sections = array();
-	}*/
-	
-	public function reorder() {
-		$this->use_layout=false;
-		$this->use_view=false;
-		foreach($_POST['sortable_sections'] as $order=>$id) {
-			if(!is_numeric($order) && !is_numeric($id)) return false;
-			$section = new $this->model_class($id);
-			$section->update_attributes(array("order"=>$order));
-		}
-		echo "ok";
-	}	
-	
 
 }
 
