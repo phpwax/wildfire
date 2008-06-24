7225623aac35744f905cfa1b5a94083d7d4eee6e
charles <charles@oneblackbear.com>
2008-06-24 17:33:18 +0100

    comments and tidying

--- lib/controller/CMSAdminCategoryController.php
+++ lib/controller/CMSAdminCategoryController.php
@@ -1,4 +1,7 @@
 <?php
+/**
+ * admin section for categories - inherits methods from admin component
+ */
 
 class CMSAdminCategoryController extends CMSAdminComponent {
 
@@ -10,20 +13,22 @@ class CMSAdminCategoryController extends CMSAdminComponent {
     "name"   =>array(),
   );
   public $filter_columns = array("name");
-
+	/**
+	 * set up the categories listing
+	 */	
 	public function controller_global() {
 	  $array = array(0 => 'No Parent');
 		$this->tree_collection = $this->model->categories_as_collection(null, $array);
 	}
-	
+	/**
+	 * ajax filter function - takes the incoming string, matches against columns 
+	 * and outputs view of the matching data
+	 */	
 	public function filters() {
 	  $this->use_layout=false;
 	  $cat = new CmsCategory;
-	  if(strlen($fil = $_POST['filter'])<1) {
-  	  $this->all_categories = $cat->order("parent_id ASC, name ASC")->all();
-  	} else {
-      $this->all_categories = $cat->filter("name LIKE '%$fil%'")->order("parent_id ASC, name ASC")->all();
-    }
+	  if(strlen($fil = $_POST['filter'])<1) $this->all_categories = $cat->order("parent_id ASC, name ASC")->all();
+  	else $this->all_categories = $cat->filter("name LIKE '%$fil%'")->order("parent_id ASC, name ASC")->all();
   	$this->cat_partial = $this->render_partial("cat_list");
 	}
 
