b1f9c0d12d6bf254e7d40cc8ad9f158f7bef6809
Ross Riley <ross@oneblackbear.com>
2008-06-30 00:09:54 +0100

    Debug of foreign key error

--- lib/model/CmsSection.php
+++ lib/model/CmsSection.php
@@ -38,8 +38,6 @@ class CmsSection extends WaxTreeModel {
 	}
 	
 	public function permalink() {
-	  return "";
-	  var_dump($this->path_to_root()); exit;
 		$path = array_reverse($this->path_to_root());
 		print_r($path); exit;
 		foreach($path as $object)
