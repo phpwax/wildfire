2e238f0b88b22bb71f4f28fa4565cf927df810a7
Ross Riley <ross@oneblackbear.com>
2008-06-30 16:50:29 +0100

    Testing eager loading

--- lib/model/CmsContent.php
+++ lib/model/CmsContent.php
@@ -18,7 +18,7 @@ class CmsContent extends WaxModel {
 		//used in conversion
 		$this->define("oldid", "IntegerField");
 		//images
-		$this->define("images", "ManyToManyField", array('target_model'=>"WildfireFile", 'editable'=>false, "load"=>"eager"));
+		$this->define("images", "ManyToManyField", array('target_model'=>"WildfireFile", 'editable'=>false, "load"=>"lazy"));
 		//section
 		$this->define("section", "ForeignKey", array('target_model'=>'CmsSection','editable'=>false));
 		//author
