7d9ae1398240169138ac82d2f5fd28113eb213c0
Ross Riley <ross@oneblackbear.com>
2008-06-30 10:06:12 +0100

    attempt to debug ManyToMany get recursion

--- lib/model/CmsContent.php
+++ lib/model/CmsContent.php
@@ -77,7 +77,8 @@ class CmsContent extends WaxModel {
 	}
 	
 	public function permalink() {
-	  print_r($this->section); exit;
+	  $sec = $this->section;
+	  echo $sec->permalink(); exit;
 		return $this->section->permalink()."/".$this->url;
 	}
 	public function date_published(){
