5833f13c80b3a0cd2b831693b3b8eeccaa47d8fe
Sheldon Els <sheldon@oneblackbear.com>
2008-07-01 16:19:05 +0100

    renamed loading type and changed it to a boolean

--- lib/model/CmsContent.php
+++ lib/model/CmsContent.php
@@ -18,17 +18,17 @@ class CmsContent extends WaxModel {
 		//used in conversion
 		$this->define("oldid", "IntegerField");
 		//images
-		$this->define("images", "ManyToManyField", array('target_model'=>"WildfireFile", 'editable'=>false, "load"=>"eager"));
+		$this->define("images", "ManyToManyField", array('target_model'=>"WildfireFile", 'editable'=>false, "eager_loading"=>true));
 		//section
 		$this->define("section", "ForeignKey", array('target_model'=>'CmsSection','editable'=>false));
 		//author
 		$this->define("author", "ForeignKey", array('target_model'=>'WildfireUser', 'col_name'=>"author_id",'editable'=>false));
 		//more_content <-> content
-		$this->define("more_content", "HasManyField", array('target_model'=>"CmsExtraContent", 'join_field'=>"cms_content_id",'editable'=>false, "load"=>"eager"));
+		$this->define("more_content", "HasManyField", array('target_model'=>"CmsExtraContent", 'join_field'=>"cms_content_id",'editable'=>false, "eager_loading"=>true));
 		//comments <-> attached_to
 		$this->define("comments", "HasManyField", array('target_model'=>"CmsComment", 'join_field'=>"attached_id",'editable'=>false));
 		//category <-> attached_to
-		$this->define("categories", "ManyToManyField", array('target_model'=>"CmsCategory",'editable'=>false, "load"=>"eager"));
+		$this->define("categories", "ManyToManyField", array('target_model'=>"CmsCategory",'editable'=>false, "eager_loading"=>true));
 	}
 	public function page_status() {
 		return $this->status_options[$this->status];
