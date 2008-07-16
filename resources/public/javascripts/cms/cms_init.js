81f20bc863de90a7d2bce8c7803d314ddf7a10f0
Ross Riley <ross@oneblackbear.com>
2008-07-16 16:09:51 +0100

    Fix to js error when container does not exist

--- resources/public/javascripts/cms/cms_init.js
+++ resources/public/javascripts/cms/cms_init.js
@@ -10,16 +10,6 @@ function setup_preview(permalink, trigger_id, preview_pane) {
 
 
 
-/* Tab setup and emailer setup for CMSGeneralEmailer */
-$(document).ready(function() {
-  if($("#container").length) $("#container").tabs();
-  $(".email_send_button").click(function(){
-    if($("#test_emails").val().length > 4) return confirm('This will be sent to TEST EMAILS ONLY!\n (Real email would be sent to '+this.id.substring(0,this.id.indexOf("_")) +' recipients)');
-    else return confirm('Are you sure you want to send this to '+this.id.substring(0,this.id.indexOf("_")) +' recipients?');
-  })
-});
-
-
 /* JS Table initialisation for index.html */
 $(document).ready(function() {
   if($("#item_list_container").length) {
