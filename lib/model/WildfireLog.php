a68a4353612ba7ca43d23b65e9703dccb8fdf44a
Ross Riley <ross@oneblackbear.com>
2008-09-06 10:57:34 +0100

    Improvements to wildfire_log

--- lib/model/WildfireLog.php
+++ lib/model/WildfireLog.php
@@ -6,9 +6,16 @@ class WildfireLog extends WaxModel {
   public function setup() {
     $this->define("action", "CharField");
     $this->define("user", "ForeignKey", array("target_model"=>"WildfireUser"));
-    $this->define("time", "DateTimeField");
+    $this->define("action_time", "DateTimeField");
   }
 
+  public function by() {
+    return $this->user->fullname;
+  }
+  
+  public function action_time(){
+		return date('d/m/Y @ H:i', strtotime($this->time));
+	}
 	
 }
 
