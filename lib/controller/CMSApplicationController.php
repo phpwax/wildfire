64707cb0a6eb6cb6741fac9f6c5cd493a991dd31
charles <charles@oneblackbear.com>
2008-09-08 10:38:56 +0100

    pass in the format var

--- lib/controller/CMSApplicationController.php
+++ lib/controller/CMSApplicationController.php
@@ -194,9 +194,9 @@ class CmsApplicationController extends WXControllerBase{
 	  $this->use_view="cms_".$type;		
 	  foreach($sections as $section) {
 			WaxLog::log("error", "[viewing] looking for page/cms_".$section."_".$type);
-	  	if($this->is_viewable("page/cms_".$section."_".$type)) $this->use_view = "cms_".$section."_".$type;
+	  	if($this->is_viewable("page/cms_".$section."_".$type, $this->use_format)) $this->use_view = "cms_".$section."_".$type;
 	  }
-		if($this->is_page() && $this->is_viewable("page/cms_".$type. "_".$this->cms_content->url) ) $this->use_view =  "cms_".$type. "_".$this->cms_content->url;			
+		if($this->is_page() && $this->is_viewable("page/cms_".$type. "_".$this->cms_content->url,$this->use_format) ) $this->use_view =  "cms_".$type. "_".$this->cms_content->url;			
 	}
 	
   /**
