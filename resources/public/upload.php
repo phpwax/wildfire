ae3e16ee182b5330a866d2888ff6d87b97c0e1f0
Ross Riley <ross@oneblackbear.com>
2008-09-10 12:51:22 +0100

    Fix to upload.php

--- resources/public/upload.php
+++ resources/public/upload.php
@@ -1,18 +1,16 @@
 <?php
 require_once dirname(__FILE__).'/../app/config/environment.php';
-$app=new WXApplication(true);
+$app=new WXApplication(false);
+
 if($_FILES) {
     $path = $_POST['wildfire_file_folder'];
     $fs = new CmsFilesystem;
-    error_log(print_r($_FILES, 1));
-    error_log(print_r($_POST, 1));
     $_FILES['upload'] = $_FILES["Filedata"];
     $fs->upload($path);
     $fs->databaseSync($fs->defaultFileStore.$path, $path);
     $file = new WildfireFile;
     $newfile = $file->filter(array("filename"=>$_FILES['upload']['name'], "rpath"=>$path))->first();
     $newfile->description = $_POST["wildfire_file_description"];
-    $newfile->flags = "normal";
     $newfile->save();
     echo "Uploaded";
 }
