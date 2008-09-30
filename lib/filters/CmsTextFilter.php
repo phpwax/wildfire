e08a64ea8c9823be4d1d6c5edd3c3cb5d74ca457
charles <charles@oneblackbear.com>
2008-09-30 15:29:04 +0100

    fix to filter

--- lib/filters/CmsTextFilter.php
+++ lib/filters/CmsTextFilter.php
@@ -100,11 +100,11 @@ class CmsTextFilter  {
 	static public function videos($text){
 		/*standard youtube*/
 		$youtube = '<object width="$2" height="$3">
-	    <param name="movie" value="http://www.youtube.com/v/$6"></param>
-	    <embed src="http://www.youtube.com/v/$6" type="application/x-shockwave-flash" width="$2" height="$3"></embed>
-	  </object>';
+		  <param name="movie" value="http://www.youtube.com/v/$6"></param>
+		  <embed src="http://www.youtube.com/v/$6" type="application/x-shockwave-flash" width="$2" height="$3"></embed>
+		</object>';
 
-		$text = preg_replace("/<a href=\"(.*)\" rel=\"([0-9]*px):([0-9]*px)\">(.*)youtube(.*)\?v=([a-zA-Z\-0-9_]*)([&]*)(.*)<\/a>/u", $youtube, $text);
+		$text = preg_replace("/<a href=\"(.*)\" rel=\"([0-9]*px):([0-9]*px)\">(.*)youtube(.*)\?v=([a-zA-Z\-0-9_\/]*)([&]*)(.*)<\/a>/u", $youtube, $text);
 
 		/*VIMEO*/
 		$vimeo ='<object width="$2" height="$3">
@@ -115,12 +115,12 @@ class CmsTextFilter  {
 							</embed>
 						</object>';
 
-		$text = preg_replace("/<a href=\"(.*)\" rel=\"([0-9]*px):([0-9]*px)\">(.*)vimeo(.*)\/([a-zA-Z\-0-9_]*)([&]*)(.*)<\/a>/u", $vimeo, $text);						
+		$text = preg_replace("/<a href=\"(.*)\" rel=\"([0-9]*px):([0-9]*px)\">(.*)vimeo(.*)\/([a-zA-Z\-0-9_\/]*)([&]*)(.*)<\/a>/u", $vimeo, $text);						
 
 		/*GOOGLE*/
 		$google = '<embed id="VideoPlayback" src="http://video.google.com/googleplayer.swf?docid=$6&hl=en&fs=true" width="$2" height="$3" allowFullScreen="true" allowScriptAccess="always" type="application/x-shockwave-flash"> </embed>';
 
-		$text = preg_replace("/<a href=\"(.*)\" rel=\"([0-9]*px):([0-9]*px)\">(.*)google(.*)\?docid=([a-zA-Z\-0-9_]*)([&]*)(.*)<\/a>/u", $google, $text);						
+		$text = preg_replace("/<a href=\"(.*)\" rel=\"([0-9]*px):([0-9]*px)\">(.*)google(.*)\?docid=([a-zA-Z\-0-9_\/]*)([&]*)(.*)<\/a>/u", $google, $text);						
 
 		/*LOCAL*/
 		$local ='<object width="$2" height="$3">
@@ -130,7 +130,7 @@ class CmsTextFilter  {
 							<embed src="$4" type="application/x-shockwave-flash" allowfullscreen="true" allowscriptaccess="always" width="$2" height="$3">
 							</embed>
 						</object>';
-		$text = preg_replace("/<a href=\"(.*)\" rel=\"([0-9]*px):([0-9]*px)\">LOCAL:([a-zA-Z\-0-9_]*)<\/a>/u", $local, $text);						
+		$text = preg_replace("/<a href=\"(.*)\" rel=\"([0-9]*px):([0-9]*px)\">LOCAL:([a-zA-Z\-0-9_\/]*)<\/a>/u", $local, $text);						
 		
 		
 		return $text;
