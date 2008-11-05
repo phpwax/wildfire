69f43f4ae2a9026fe1ebefab9848ba1a2b051402
Sheldon Els <sheldon@oneblackbear.com>
2008-11-05 13:09:50 +0000

    fix to index action on CmsSection to not render generic index view

--- lib/controller/CMSAdminSectionController.php
+++ lib/controller/CMSAdminSectionController.php
@@ -24,9 +24,12 @@ class CMSAdminSectionController extends CMSAdminComponent {
 	 * index page - list of all sections
 	 */	
 	public function index() {
-		parent::index();
+	  Session::set("list_refer", $_SERVER['REQUEST_URI']);
+		$this->set_order();
+		$this->display_action_name = 'List Items';
 		$this->all_rows = $this->model->find_ordered_sections();
 		if(!$this->all_rows) $this->all_rows = array();
+		$this->filter_block_partial = $this->render_partial("filter_block");
 		$this->list = $this->render_partial("list");
 	}
 
