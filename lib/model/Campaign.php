b34af45b92cbf72024d885d4d209ca3ebfd36c11
charles <charles@oneblackbear.com>
2009-01-13 09:34:59 +0000

    api intergration - setting the primary key after save

--- lib/model/Campaign.php
+++ lib/model/Campaign.php
@@ -84,8 +84,8 @@ class Campaign extends CampaignMonitorModel {
 	}
 	
 	public function after_soap($res){
+		print_r($res);exit;
 		$this->{$this->primary_key} = $res->{'Campaign.CreateResult'};
-		print_r($this);exit;
 	}
 }
 
