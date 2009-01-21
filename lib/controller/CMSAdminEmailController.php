a95efd95c4d26c772a2c41c95973494dc3303585
charles <charles@oneblackbear.com>
2009-01-21 17:19:24 +0000

    log prime

--- lib/controller/CMSAdminEmailController.php
+++ lib/controller/CMSAdminEmailController.php
@@ -87,6 +87,7 @@ class CMSAdminEmailController extends CMSAdminComponent {
 		if($this->model->is_posted()){
 			$this->model = $this->model->handle_post();
 			WaxLog::log('error', '[POST RESULT]'.print_r($this->model->row,1));
+			WaxLog::log('error', '[PRIME]'.print_r($this->model->primval(),1));
 			if($this->model->primval() > 0){
 				Session::add_message('Your campaign has been created!');
 				$this->redirect_to('/admin/email');
