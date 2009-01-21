72dcfa1ce36c0d0b84a1be7b198566c6c34a31e2
charles <charles@oneblackbear.com>
2009-01-21 15:12:36 +0000

    log error

--- lib/model/CampaignMonitorAdapter.php
+++ lib/model/CampaignMonitorAdapter.php
@@ -314,6 +314,7 @@ class CampaignMonitorAdapter extends WaxDbAdapter {
 		//call the client wsdl and then the soap function
 		$client = new SoapClient($this->soap_wsdl, array('trace'=>true));
 		$res = $client->__soapCall($method, array($this->soap_arguments) );
+		WaxLog::log('error', '[SOAP RESPONSE]'.print_r($client->__getLastResponse(), 1));
 		$model->after_soap($res);
 		return $res;
 	}
