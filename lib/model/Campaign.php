8bb2c7a82c438e18b3213b0e1fc6cf2fa575540f
charles <charles@oneblackbear.com>
2009-01-21 17:43:12 +0000

    seperate send function - as there seems to be a lag on the api

--- lib/model/Campaign.php
+++ lib/model/Campaign.php
@@ -90,17 +90,7 @@ class Campaign extends CampaignMonitorModel {
 	}
 	//error checking
 	public function after_soap($res){
-		if($errors = $res->{'Campaign.CreateResult'}->enc_value->Message){
-			$this->errors[$this->primary_key] = $errors;
-		}elseif(is_string($res->{'Campaign.CreateResult'})){
-			$this->CampaignID = $res->{'Campaign.CreateResult'};			
-			$model = new Campaign;
-			$model->ClientID = $this->ClientID;
-			$model->CampaignID = $this->CampaignID;
-			$model->SendDate = $this->SendDate;
-			$model->ConfirmationEmail = $this->ConfirmationEmail;
-			$res = $model->Send();	
-		}
+		if($errors = $res->{'Campaign.CreateResult'}->enc_value->Message) $this->errors[$this->primary_key] = $errors;
 	}
 }
 
