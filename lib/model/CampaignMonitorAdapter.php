96bb5ce3b32d089dff4f0dc04765c29a36a0a4ed
charles <charles@oneblackbear.com>
2009-01-21 13:03:36 +0000

    bug tracking

--- lib/model/CampaignMonitorAdapter.php
+++ lib/model/CampaignMonitorAdapter.php
@@ -255,7 +255,7 @@ class CampaignMonitorAdapter extends WaxDbAdapter {
 		WaxLog::log('error', '[API CALLING - '.$this->call_method.']'.$parse_func. " -- ".$this->cm_api_method);
 		if(method_exists($model, $func)) $res=$this->{$parse_func}($model->$func($this->url,$model),$model); //check if the model has an over riding function		
 		else $res = $this->{$parse_func}($this->$func($this->url, $model), $model); //otherwise call the default one
-		WaxLog::log('error', '[SOAP RES]'. print_r($res,1));
+		WaxLog::log('error', '['.$func.' RES]'. print_r($res,1));
 		$model->after_api_result_parsed($res);
 		return $res;
 	}
