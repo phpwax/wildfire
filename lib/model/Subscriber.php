775e51ca2bb8fa8d26d85cbd146067c522f08bb2
charles <charles@oneblackbear.com>
2009-01-29 19:28:25 +0000

    debug

--- lib/model/Subscriber.php
+++ lib/model/Subscriber.php
@@ -12,7 +12,7 @@ class Subscriber extends CampaignMonitorModel {
 	
 	public $primary_key="ListID";
   public $primary_type = "CharField";
-	public $save_action = array("Subscribers.AddSubscriberWithCustomFields" => "soap");
+	public $save_action = array("Subscribers.AddSubscriber" => "http");
 	public $select_action = array("Subscribers.GetActive" => "http");	
 	
 	public $get_action = array("Subscribers.GetActive", 
