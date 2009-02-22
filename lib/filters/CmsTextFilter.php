492c846aa7e8dd3f83c9b318da7df18b5805a94c
Ross Riley <ross@oneblackbear.com>
2009-02-22 11:43:57 +0000

    Fix to unicode bug content

--- lib/filters/CmsTextFilter.php
+++ lib/filters/CmsTextFilter.php
@@ -10,7 +10,7 @@ class CmsTextFilter  {
   
   static public $filters = array(
     "before_save"=>array("convert_chars", "correct_entities", "strip_attributes", "strip_slashes", "inline_images"),
-    "before_output"=> array("first_para_hook")
+    "before_output"=> array("first_para_hook", "clean_word")
   );
   
   static public function add_filter($trigger, $method) {
