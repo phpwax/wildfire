94c874af0758a1dff0b33e394eca802aaef5a69c
Ross Riley <ross@oneblackbear.com>
2009-02-22 15:52:11 +0000

    Fix to unicode bug content

--- lib/filters/CmsTextFilter.php
+++ lib/filters/CmsTextFilter.php
@@ -10,7 +10,7 @@ class CmsTextFilter  {
   
   static public $filters = array(
     "before_save"=>array("convert_chars", "correct_entities", "strip_attributes", "strip_slashes", "inline_images"),
-    "before_output"=> array("clean_word", "first_para_hook", "ampersand_hook", "strip_slashes", "yt_video", "videos")
+    "before_output"=> array("reverse_entities", "first_para_hook", "ampersand_hook", "strip_slashes", "yt_video", "videos")
   );
   
   static public function add_filter($trigger, $method) {
@@ -43,6 +43,12 @@ class CmsTextFilter  {
   static public function correct_entities($text) {
 		$modified = str_replace("£", "&pound;", $text);
 		$modified = str_replace("€", "&euro;", $modified);
+    return $modified;
+  }
+  
+  static public function reverse_entities($text) {
+		$modified = str_replace( "&pound;","£", $text);
+		$modified = str_replace("&euro;","€", $modified);
     return $modified;
   }
   
