a1664562432260185a09a19def4e33d5669799c9
charles <charles@oneblackbear.com>
2009-03-18 09:24:11 +0000

    WARNING! Older versions of the framework (pre 68db6c3) will break at this point.. CmsSection now using the new waxtreerecordset which didnt exist previously. Checking this in because it really is needed on any site with large number of sections

--- lib/controller/CMSAdminSectionController.php
+++ lib/controller/CMSAdminSectionController.php
@@ -17,18 +17,23 @@ class CMSAdminSectionController extends CMSAdminComponent {
 	* create the tree structure used for the drop down section selection
 	**/
 	public function controller_global() {
-	  $this->tree_collection = array("None");
-	  $sections_as_collection = $this->model->sections_as_collection();
-		foreach($this->model->sections_as_collection() as $id => $section) $this->tree_collection[$id] = $section;
+		$this->tree_collection = array("None");
+		foreach($this->model->tree() as $section){
+			$tmp = str_pad("", $section->get_level(), "*", STR_PAD_LEFT);
+			$tmp = str_replace("*", "&nbsp;&nbsp;", $tmp);
+			$this->tree_collection[$section->id] = $tmp.$section->title;
+		}
 	}
+
 	/**
 	 * index page - list of all sections
 	 */	
 	public function index() {
-	  Session::set("list_refer", $_SERVER['REQUEST_URI']);
+		Session::set("list_refer", $_SERVER['REQUEST_URI']);
 		$this->set_order();
 		$this->display_action_name = 'List Items';
-		$this->all_rows = $this->model->all();
+		$this->all_rows = $this->model->tree();
+
 		if(!$this->all_rows) $this->all_rows = array();
 		$this->filter_block_partial = $this->render_partial("filter_block");
 		$this->list = $this->render_partial("list");
--- lib/model/CmsSection.php
+++ lib/model/CmsSection.php
@@ -5,21 +5,53 @@ class CmsSection extends WaxTreeModel {
 	public $order_field = "order";
 	public $order_direction = "ASC";
 	public $tree_array = false;
-	
+
 	public function setup(){
 		$this->define("title", "CharField", array('maxlength'=>255) );
 		$this->define("introduction", "TextField");
 		$this->define("order", "IntegerField", array('maxlength'=>5) );
 		$this->define("url", "CharField", array('maxlength'=>255) );
 	}
-	
+
 	public function before_save() {
-	  $this->introduction = CmsTextFilter::filter("before_save", $this->introduction);
 		$this->url = WXInflections::to_url($this->title);
+		if($this->type == "") $this->type = 0;
 	}	
-	
+
+	public function tree($nodes = false){
+		$model_class = get_class($this);
+		if($cache_tree = unserialize($this->session_cache_get("section_tree"))) {
+			return new RecursiveIteratorIterator(new WaxTreeRecordset($this, $cache_tree), RecursiveIteratorIterator::SELF_FIRST );
+		}else{
+			$new_tree = $this->build_tree($this->rows() );
+			$this->session_cache_set("section_tree", serialize($new_tree));
+			return new RecursiveIteratorIterator(new WaxTreeRecordset($this, $new_tree), RecursiveIteratorIterator::SELF_FIRST );
+		}
+	}
+
+	public function all() {
+		$res = $this->db->select($this);
+		return new WaxRecordset($this, $res);
+	}
+
+	public function build_tree($list) {
+		$lookup = array();
+		foreach( $list as $item ) {
+			$item['children'] = array();
+			$lookup[$item['id']] = $item;
+		}
+		$tree = array();
+		foreach( $lookup as $id => $foo ){
+			$item = &$lookup[$id];
+			if( $item['parent_id'] == 0 ) $tree[$id] = &$item;
+			elseif( isset( $lookup[$item['parent_id']] ) ) $lookup[$item['parent_id']]['children'][] = &$item;
+			else $tree['_orphans_'][$id] = &$item;
+		}
+		return array_values($tree);
+	}
+
 	public function sections_as_collection($input=false,$padding_char ="&nbsp;&nbsp;") {
-		if(!$input) $input = $this->all();
+		if(!$input) $input = new WaxRecordset(new CmsSection(), $this->tree());
 		$collection = array();
 		if(!$input) return $collection;
 		foreach($input as $item){
@@ -28,14 +60,29 @@ class CmsSection extends WaxTreeModel {
 		}
 		return $collection;
 	}
-	
+
 	public function permalink() {
-    $path = array_reverse($this->path_to_root());
-    foreach($path as $object)
-      $url .= "/".$object->url;
-    $url = str_replace("/home", "", $url);
-    return $url;
-  }
+		$path = array_reverse($this->path_to_root());
+		foreach($path as $object) $url .= "/".$object->url;
+		$url = str_replace("/home", "", $url);
+		return $url;
+	}
+
+	public function section_type(){
+		return $this->section_types[$this->type];
+	}
+
+	protected function session_cache_get($name, $expire="18000") {
+		if(Session::get($name)) {
+			if(Session::get($name."_cache") > time()-$expire) return Session::get($name);
+		}
+		return false;
+	}
+
+	protected function session_cache_set($name, $value) {
+		Session::set($name, $value);
+		Session::set($name."_cache", time());
+	}
 }
 
 ?>
