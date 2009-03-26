ac2827cf1bcddf8d140ec94da9c33c9bf98f4dc4
Sheldon <sheldon@sheldons-macbook-pro.local>
2009-03-26 13:15:57 +0000

    generic creates now redirect correctly for cancel and save & close

--- lib/controller/CMSAdminComponent.php
+++ lib/controller/CMSAdminComponent.php
@@ -118,12 +118,14 @@ class CMSAdminComponent extends WXControllerBase {
 	/**
 	* Create model item - has shared view cms/view/CONTROLLER/_form.html
 	*/
-	public function create($save=true) {
-		$this->display_action_name = 'Create';
-		$this->model = new $this->model_class;		
-		if($save) $this->save($this->model);
-		$this->form = $this->render_partial("form");
-	}
+  public function create($save=true) {
+  	$this->display_action_name = 'Create';
+  	$this->model = new $this->model_class;		
+  	$this->form = $this->render_partial("form");
+  	if($_POST['cancel']) $this->redirect_to(Session::get("list_refer"));
+  	if($_POST['save'] && $save) $this->save($this->model, "edit");
+  	elseif($save) $this->save($this->model, Session::get("list_refer"));
+  }
 	
 	/**
 	* Edit model item in lightbox interface - has shared view cms/view/CONTROLLER/_form.html
