6065923989c692e301e303f128cb1716b2fb9823
charles <charles@oneblackbear.com>
2009-04-16 09:07:57 +0100

    url is not longer required to be unique - as the section stacking works properly you can have the same url in more than one section

--- lib/model/CmsContent.php
+++ lib/model/CmsContent.php
@@ -14,7 +14,7 @@ class CmsContent extends WaxModel {
 		$this->define("date_created", "DateTimeField", array("editable"=>false));
 		$this->define("sort", "IntegerField", array('maxlength'=>3));
 		$this->define("pageviews", "IntegerField", array('maxlength'=>11));
-		$this->define("url", "CharField", array('maxlength'=>255,"unique"=>true));
+		$this->define("url", "CharField", array('maxlength'=>255));
 		//used in conversion
 		$this->define("oldid", "IntegerField");
 		//images
@@ -105,7 +105,7 @@ class CmsContent extends WaxModel {
 		$test_url = $original_url = $this->url;
 		$model = new CmsContent();
 		if($this->id) $model->filter('id <> '.$this->id);
-		while($model->filter(array('url'=>$test_url) )->first() ){
+		while($model->filter(array('url'=>$test_url, 'cms_section_id'=>$this->cms_section_id) )->first() ){
 			$test_url = $original_url . '-'.mt_rand(0,99);
 		} 
 		return $test_url;
