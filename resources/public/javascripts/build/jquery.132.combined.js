3894caa80716470d7fa5f8fbb30843fbea363161
Ross Riley <ross@oneblackbear.com>
2009-05-21 12:43:48 +0100

    Debug of descriptions in uploads

--- resources/public/javascripts/build/jquery.132.combined.js
+++ resources/public/javascripts/build/jquery.132.combined.js
@@ -2384,6 +2384,7 @@ function set_post_params(){
   } 
   swfu.addPostParam("wildfire_file_folder", fold);
   swfu.addPostParam("wildfire_file_description", jQuery("#wildfire_file_description").val());
+  alert("About to start upload description: "+jQuery("#wildfire_file_description").val());
   swfu.startUpload();
 }
 
