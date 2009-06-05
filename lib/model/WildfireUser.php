a8088d3e73bf6455eb4b0dd4fd8eedfb953269fa
Sheldon Els <sheldon.els@gmail.com>
2009-06-05 11:01:03 +0100

    added function to get a tree of the allowed sections. it's a bit heavy, would prefer to find a way to make it lighter.

--- lib/model/WildfireUser.php
+++ lib/model/WildfireUser.php
@@ -26,5 +26,18 @@ class WildfireUser extends WaxModel {
 	  $content = new CmsContent("published");
 	  return $content->filter(array("author_id"=>$this->id))->all();
 	}
+	/**
+	 * this function returns not just the associated sections, but also all sections under those
+	 *
+	 * @return Iterator with sections
+	 * @author Sheldon Els
+	 */
+	public function allowed_sections_tree(){
+	  $sections = new CmsSection();
+	  foreach($this->allowed_sections as $section)
+	    foreach($section->tree() as $sub_section)
+	      $allowed_section_ids[] = $sub_section->primval;
+	  return $sections->filter(array("id"=>$allowed_section_ids))->tree();
+	}
 	
 }
