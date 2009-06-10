50da29e460153f125918a83d0f3461182dc3a5cf
Sheldon Els <sheldon.els@gmail.com>
2009-06-10 17:52:48 +0100

    one day we'll write some code that can figure out where to save fields on a join to, target or join table. that code will have lots of bitches. it will have survived many drive-by's. in the meantime, here is julian the code. he hangs out in the library. shame.

--- lib/controller/CMSAdminContentController.php
+++ lib/controller/CMSAdminContentController.php
@@ -103,7 +103,19 @@ class CMSAdminContentController extends AdminComponent {
 		$this->join_name = "images";
 	  if(Request::post("id")) {
 		  $file = new WildfireFile(Request::post('id'));
-		  $this->page->images = $file;
+		  $all_current_images = $this->page->images;
+		  foreach($all_current_images as $current_image)
+		    if($current_image->primval == $file->primval)
+		      $dontadd = true;
+		  if(!$dontadd){
+  		  $images_join_col = $this->page->get_col("images");
+  		  $images_join = $images_join_col->join_model;
+  		  $images_join->{$this->page->table."_".$this->page->primary_key} = $this->page->primval;
+  		  $images_join->{$file->table."_".$file->primary_key} = $file->primval;
+  		  $images_join->join_order = count($all_current_images);
+  		  $images_join->save();
+        WaxModel::unset_cache($this->model_class, $this->join_name);
+	    }
 		  $this->image = $file;
 	  }
 	}
