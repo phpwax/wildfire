fad231fda545ab5f95265b5b9187a0b315919289
Sheldon Els <sheldon.els@gmail.com>
2009-06-26 13:24:01 +0100

    new permissions extend base ones in wax

--- lib/model/CmsPermission.php
+++ lib/model/CmsPermission.php
@@ -1,22 +1,11 @@
 <?php
-/**
- * permissions for modules -> users
- *
- */
-class CmsPermission extends WaxModel{
-	
-	public static $operations = array('CREATE', 'VIEW', 'EDIT', 'DELETE', 'ADMIN', 'SHOW IN MENU');
-	
+class CmsPermission extends WaxPermission{
+  
 	public function setup(){
-	  $this->define("module", "CharField", array('maxlength'=>255) );
-	  $this->define("operation", "IntegerField", array('choices'=>self::$operations, 'maxlength'=>3) );	  
-    $this->define("users", "ManyToManyField", array('target_model' => 'WildfireUser', 'join_model_class'=>"WaxModelPermissionsJoin", 'eager_loading'=>true));
+	  parent::setup();
+    $this->define("user", "ForeignKey", array('target_model' => 'WildfireUser'));
 	}
 		
-	public function operation(){
-	  return self::$operations[$this->operation];
-	}
-	
 }
 
 ?>
