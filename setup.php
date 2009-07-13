5aaa9e29f9ffd09e8e4992a3eac452fe37202482
Sheldon Els <sheldon.els@gmail.com>
2009-07-13 12:56:52 +0100

    put back old auth levels. need them for automated migration.

--- setup.php
+++ setup.php
@@ -5,12 +5,12 @@
 
 define("CMS_VERSION", "v3");
 define("CMS_DIR", dirname(__FILE__));
-CMSApplication::register_module("home", array("display_name"=>"Dashboard", "link"=>"/admin/home/"));
-CMSApplication::register_module("sections", array("display_name"=>"Site Sections", "link"=>"/admin/sections/"));
-CMSApplication::register_module("categories", array("display_name"=>"Categories", "link"=>"/admin/categories/"));
-CMSApplication::register_module("content", array("display_name"=>"Content", "link"=>"/admin/content/"));
-CMSApplication::register_module("comments", array("display_name"=>"Comments", "link"=>"/admin/comments/"));
-CMSApplication::register_module("files", array("display_name"=>"Files", "link"=>"/admin/files/"));
-CMSApplication::register_module("users", array("display_name"=>"CMS Users", "link"=>"/admin/users/"));
+CMSApplication::register_module("home", array("display_name"=>"Dashboard", "link"=>"/admin/home/", 'auth_level'=>0));
+CMSApplication::register_module("sections", array("display_name"=>"Site Sections", "link"=>"/admin/sections/",'auth_level'=>30));
+CMSApplication::register_module("categories", array("display_name"=>"Categories", "link"=>"/admin/categories/",'auth_level'=>20));
+CMSApplication::register_module("content", array("display_name"=>"Content", "link"=>"/admin/content/",'auth_level'=>10));
+CMSApplication::register_module("comments", array("display_name"=>"Comments", "link"=>"/admin/comments/",'auth_level'=>5));
+CMSApplication::register_module("files", array("display_name"=>"Files", "link"=>"/admin/files/", 'auth_level'=>0));
+CMSApplication::register_module("users", array("display_name"=>"CMS Users", "link"=>"/admin/users/",'auth_level'=>30));
 Autoloader::include_from_registry('CMSHelper');
 Autoloader::register_helpers();
