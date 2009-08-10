bd696daf5ef1a49582fa966ed94b537b191ca939
charlesmarshall <charles@oneblackbear.com>
2009-08-10 13:47:25 +0100

    updated a single vr in the js so now need to commit in the make updated files ... these should not be in the repo...

--- resources/public/javascripts/build/jquery.132.combined.js
+++ resources/public/javascripts/build/jquery.132.combined.js
@@ -459,7 +459,7 @@ var init_upload;
 var autosaver;
 wym_editors = [];
 if(typeof(file_browser_location) == "undefined") var file_browser_location = "/admin/files/browse_images";
-var file_mime_type = "images";
+var file_mime_type = "image";
 $(document).ready(function() {
     $("#container").tabs();
     
