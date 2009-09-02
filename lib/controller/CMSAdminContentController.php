05b45df172a577d96f68a859ae24804c40e01ed6
Ross <ross@macbook-pro-rr.home>
2009-09-02 12:03:14 +0100

    Debug of images problem

--- lib/controller/CMSAdminContentController.php
+++ lib/controller/CMSAdminContentController.php
@@ -248,7 +248,7 @@ class CMSAdminContentController extends AdminComponent {
 		//images
     if($this->model->images && $this->model->images->count()) $this->attached_images=$this->model->images;
     elseif($this->model->master && $this->model->master->primval && $this->model->master->images && $this->model->master->images->count()) $this->attached_images=$this->model->master->images;
-    else $attached_images = array();
+    else $this->attached_images = array();
     error_log(print_r($this->attached_images, 1));
     
 		//categories assocaited
