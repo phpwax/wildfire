2bcbcbb118e3feb1f2a5bde194ce039628b3833e
charlesmarshall <charles@oneblackbear.com>
2009-09-17 18:24:38 +0100

    run the after save hook

--- lib/controller/CMSAdminContentController.php
+++ lib/controller/CMSAdminContentController.php
@@ -196,8 +196,10 @@ class CMSAdminContentController extends AdminComponent {
       $preview->save();
   	  foreach($preview->columns as $col => $params) if($preview->$col) $copy_attributes[$col] = $preview->$col;
   	  $copy_attributes = array_diff_key($copy_attributes,array_flip(array($preview->primary_key,"master","status"))); //take out IDs and status
-	    return $master->update_attributes($copy_attributes);
-    }else return $master;
+	    $res = $master->update_attributes($copy_attributes);	    
+    }else $res = $master;
+    $this->after_save($res);
+    return $res;
 	}
 	/**
 	* the editing function... lets you change all the bits associated with the content record
