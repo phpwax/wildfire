f4b33e02563536df7f55ab02d6dff4aec51d2659
Ross Riley <ross@oneblackbear.com>
2010-01-28 11:09:13 +0000

    New video work

--- resources/public/javascripts/cms/wildfire_video.js
+++ resources/public/javascripts/cms/wildfire_video.js
@@ -5,10 +5,7 @@ jQuery(window).load(function(){
     var source_url = el.attr("href");
     var width = el.find("img").outerWidth();
     var height = el.find("img").outerHeight();
-    el.replaceWith('<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="' + width + '" height="' + height + '"><param name="movie" value="/images/wildfirevideoplayer.swf?source_url=' + source_url + '&vid_width=' + width + '&vid_height=' + height + '" /><param name="swliveconnect" value="true" /><param name="allowscriptaccess" value="always" /><param name="wmode" value="transparent" /><!--[if !IE]>--><object id="blahblah" type="application/x-shockwave-flash" data="/images/wildfirevideoplayer.swf?source_url=' + source_url + '&vid_width=' + width + '&vid_height=' + height + '" width="' + width + '" height="' + height + '"><param name="swliveconnect" value="true" /><param name="allowscriptaccess" value="always" /><param name="wmode" value="transparent" /><!--<![endif]--><p class="wildfire_video_alt_content"></p><!--[if !IE]>--></object><!--<![endif]--></object>').replaceAll(".wildfire_video_alt_content");
+    el.replaceWith('<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="' + width + '" height="' + height + '"><param name="movie" value="/images/player_flv.swf"/><param name="swliveconnect" value="true" /><param name="allowscriptaccess" value="always" /><param name="wmode" value="transparent" /><param name="flashvars" value="flv='+source_url+'" /><!--[if !IE]>--><object id="blahblah" type="application/x-shockwave-flash" data="/images/player_flv.swf" width="' + width + '" height="' + height + '"><param name="swliveconnect" value="true" /><param name="allowscriptaccess" value="always" /><param name="wmode" value="transparent" /><param name="flashvars" value="flv='+source_url+'" /><!--<![endif]--><p class="wildfire_video_alt_content"></p><!--[if !IE]>--></object><!--<![endif]--></object>').replaceAll(".wildfire_video_alt_content");
   });
-  jQuery("#blahblah").mouseleave(function(){
-    var flash_obj = jQuery(this)[0];
-    flash_obj.hidecontrols();
-  });
+  
 });
