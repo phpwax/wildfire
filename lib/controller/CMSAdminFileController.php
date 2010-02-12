3c3bf506427e4f0c01969983581cbf1c8142a275
Ross Riley <ross@oneblackbear.com>
2010-02-12 11:11:03 +0000

    Fix to filtering on images

--- lib/controller/CMSAdminFileController.php
+++ lib/controller/CMSAdminFileController.php
@@ -188,7 +188,7 @@ class CMSAdminFileController extends AdminComponent {
 		
 		if($mime_type = Request::param('mime_type')) $model->filter("type", "%$mime_type%", "LIKE");
 		
-		if($filter = Request::param('filter')) $model->filter('(id LIKE "%?%" OR filename LIKE "%?%" OR description LIKE "%?%")', array($filter,$filter,$filter));
+		if($filter = Request::param('filter')) $model->filter('(id LIKE ? OR filename LIKE ? OR description LIKE ?)', array("%".$filter."%","%".$filter."%","%".$filter."%"));
     
     if($folder = Request::post('filterfolder')) $model->filter("rpath", "$folder%", "LIKE");
     elseif(!$mime_type) $model->filter("rpath", "files");
