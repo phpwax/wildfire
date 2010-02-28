ab41abe23c83c8fd99fa39f08ab836876ba449c0
Ross Riley <ross@oneblackbear.com>
2010-02-28 20:41:37 +0000

    Set the default grab of cms listing to eager load. Reduces database overhead.

--- lib/controller/CMSApplicationController.php
+++ lib/controller/CMSApplicationController.php
@@ -146,7 +146,7 @@ class CMSApplicationController extends WaxController{
 		}else{
 			$filter = array('cms_section_id' => $this->cms_section->id);
 			if(!$this->this_page) $this->cms_content = $content->scope("published")->filter($filter)->all();
-			else $this->cms_content = $content->scope("published")->filter($filter)->page($this->this_page, $this->per_page);
+			else $this->cms_content = $content->scope("published")->filter($filter)->page($this->this_page, $this->per_page)->eager_load();
 		  
 		}
 		if(!$this->cms_section) throw new WXRoutingException('The page you are looking for is not available', "Page not found", '404');
