62b94e505d93af08975284fec4f5660024797140
Ross Riley <ross@oneblackbear.com>
2010-05-10 23:57:32 +0100

    debug on ajax image search

--- resources/public/javascripts/build/wildfire_cms.combined.js
+++ resources/public/javascripts/build/wildfire_cms.combined.js
@@ -398,7 +398,7 @@ function delayed_cat_filter(filter) {
 function delayed_image_filter(filter) {
   jQuery("#image_filter").css("background", "white url(/images/cms/indicator.gif) no-repeat right center");
   jQuery.ajax({type: "get", url: "/admin/files/browse_images", data: {"mime_type":file_mime_type, "filter":jQuery("#image_filter").val()},
-    complete: function(response){ 
+    success: function(response){ 
       jQuery("#image_list").html(response); 
       initialise_images();  
       if(typeof(t) != "undefined" ) clearTimeout(t); 
