4fee3db806a63009a493c610013c7e26a292e643
Sheldon <sheldon.els@gmail.com>
2010-05-17 10:16:17 +0100

    no testing. yeeehaw. logical awesome. too bad php doesn't have a final keyword.

--- lib/model/CmsSection.php
+++ lib/model/CmsSection.php
@@ -34,7 +34,7 @@ class CmsSection extends WaxTreeModel {
 	public function permalink() {
 		$path = $this->path_from_root();
 		foreach($path as $object) $url .= "/".$object->url;
-		$url = str_replace("/home", "", $url);
+		$url = str_replace("/home/", "/", $url);
 		return $url;
 	}
 
