ffda3df660dd6369bafbf0f19ed13eeb7378d877
charlesmarshall <cm56marshall@googlemail.com>
2010-06-21 16:54:36 +0100

    bug fix - only throw a 404 when you can't find content with a url passed in... so this means you can have a section without pages (ie one that has only sub sections and nothing assigned to itself)

--- lib/controller/CMSApplicationController.php
+++ lib/controller/CMSApplicationController.php
@@ -159,7 +159,7 @@ class CMSApplicationController extends WaxController{
 			else $this->cms_content = $content->scope($this->content_scope)->filter($filter)->page($this->this_page, $this->per_page)->eager_load();
 		}
 		if(!$this->cms_section) throw new WXRoutingException('The page you are looking for is not available', "Page not found", '404');
-	  if(!count($this->cms_content)) throw new WXRoutingException('The page you are looking for is not available', "Page not found", '404');
+	  if(!count($this->cms_content) && $url) throw new WXRoutingException('The page you are looking for is not available', "Page not found", '404');
 	}
 	
 	
