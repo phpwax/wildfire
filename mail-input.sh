14c9367117243bc683a330a6e164ffcfe7417547
Sheldon <sheldon.els@gmail.com>
2010-08-10 10:51:45 +0100

    only 1 line, since there can be many delivered-to's

--- mail-input.sh
+++ mail-input.sh
@@ -7,6 +7,6 @@ done
 
 # the last delivered to in the header is added by our server, so it'll have our domain attached.
 # make sure this domain points to the v-host of the site you want to accept mail
-domainname=`echo "$fullinput" | grep "^Delivered-To:" | sed "s/^Delivered-To: .*@\(.*\)/\1/g" | head`
+domainname=`echo "$fullinput" | grep "^Delivered-To:" | sed "s/^Delivered-To: .*@\(.*\)/\1/g" | head -n 1`
 
 exec curl -d "$fullinput" "http://$domainname/wildfire_email_new_content"
