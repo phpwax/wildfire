ea21a00270755ab3e113215e62dc1feb3f2dd1ae
Sheldon <sheldon.els@gmail.com>
2010-08-10 10:19:59 +0100

    using delivered to instead of just to, so that forwarded mail works too. added comments as well.

--- mail-input.sh
+++ mail-input.sh
@@ -5,5 +5,8 @@ do
 $inputline"
 done
 
-domainname=`echo "$fullinput" | grep "^To:" | sed "s/^To: .*@\(.*\)/\1/g"`
+# the last delivered to in the header is added by our server, so it'll have our domain attached.
+# make sure this domain points to the v-host of the site you want to accept mail
+domainname=`echo "$fullinput" | grep "^Delivered-To:" | sed "s/^Delivered-To: .*@\(.*\)/\1/g" | head`
+
 exec curl -d "$fullinput" "http://$domainname/wildfire_email_new_content"
