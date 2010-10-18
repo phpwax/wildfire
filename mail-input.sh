8f53081bbf0e3ddc205675244d7e58cdac714282
charlesmarshall <cm56marshall@googlemail.com>
2010-10-18 16:48:35 +0100

    remove the debugs

--- mail-input.sh
+++ mail-input.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-echo "triggered" > /tmp/incomingemailtrigger
-
 while read inputline
 do
   fullinput="$fullinput
@@ -10,8 +8,5 @@ done
 # the last delivered to in the header is added by our server, so it'll have our domain attached.
 # make sure this domain points to the v-host of the site you want to accept mail
 domainname=`echo "$fullinput" | grep "^Delivered-To:" | sed "s/^Delivered-To: .*@\(.*\)/\1/g" | head -n 1`
-
-echo "$domainname" > /tmp/incomingemtest
-
 
 exec curl -d -L "$fullinput" "http://$domainname/wildfire_email_new_content"
