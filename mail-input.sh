c58c87b741f2e111663c6c2008b12f79fd9ad080
charlesmarshall <cm56marshall@googlemail.com>
2010-11-25 12:29:39 +0000

    this way seems to work, save it to file in the local users tmp folder

--- mail-input.sh
+++ mail-input.sh
@@ -5,13 +5,11 @@ do
 $inputline"
 done
 
-
 # the last delivered to in the header is added by our server, so it'll have our domain attached.
 # make sure this domain points to the v-host of the site you want to accept mail
 domainname=`echo "$fullinput" | grep "^Delivered-To:" | sed "s/^Delivered-To: .*@\(.*\)/\1/g" | head -n 1`
 
-filename="/tmp/$domainname-"`eval date +%s`".email.log"
-
+filename=`echo "$HOME"`"/tmp/email/$domainname-"`eval date +%s`".log"
 echo "$fullinput" > $filename
 
 exec curl -L -d "$filename" "http://$domainname/wildfire_email_new_content?fname=$filename"
