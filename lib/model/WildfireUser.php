8dee47991a5612ffc13ca587760f6ae8fcda5e9c
charlesmarshall <cm56marshall@googlemail.com>
2011-02-07 22:57:23 +0000

    fix the join name

--- lib/model/WildfireUser.php
+++ lib/model/WildfireUser.php
@@ -1,24 +1,32 @@
 <?php
 
 class WildfireUser extends WaxModel {
-  
+
   public $identifier = "fullname";
   public $permissions_cache = false;
   public $enable_permissions = true;
-    
+
   public function setup() {
     $this->define("username", "CharField", array("required"=>true, "blank"=>false,"unique"=>true, 'default'=>'Enter Username Here'));
     $this->define("firstname", "CharField");
     $this->define("surname", "CharField");
     $this->define("email", "CharField");
     $this->define("password", "PasswordField");
-    
-    $this->define("permissions", "HasManyField", array('target_model' => 'WildfirePermission', 'join_order' => 'class', 'join_field' => 'wildfire_user_id', 'eager_loading' => true));
+
+    $this->define("permissions", "HasManyField", array('target_model' => 'WildfirePermissionBlacklist', 'eager_loading' => true));
   }
 
   public function before_save(){
     if(!$this->primval) $this->password = md5($this->password);
   }
-	
-	
+
+	public function allowed($classname=false,$action=false){
+	  $data = $this->permissions;
+	  if($classname) $data->filter("class", $classname);
+	  if($action) $data->filter("operation", $action);
+    if($data->all()->count()) return false;
+    else return true;
+	}
+
+
 }
