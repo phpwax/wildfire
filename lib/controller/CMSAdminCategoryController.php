c433fefbff83dca7d04c98284e57b5485c5de853
charlesmarshall <cm56marshall@googlemail.com>
2011-02-08 17:49:55 +0000

    start to tidy up the category controller

--- lib/controller/CMSAdminCategoryController.php
+++ lib/controller/CMSAdminCategoryController.php
@@ -6,32 +6,9 @@
 class CMSAdminCategoryController extends AdminComponent {
 
   public $module_name = "categories";												
-  public $model_class = 'CmsCategory';
-	public $model_name = "cms_category";													
+  public $model_class = 'WildfireCategory';
 	public $display_name = "Site Categories";
-	public $scaffold_columns = array(
-    "name"   =>array("link"=>"edit"),
-  );
-  public $filter_columns = array("name");
-	public static $permissions = array("create","edit","delete");
-	/**
-	 * set up the categories listing
-	 */	
-	public function controller_global() {
-	  $array = array(0 => 'No Parent');
-		$this->tree_collection = $this->model->categories_as_collection(null, $array);
-	}
-	/**
-	 * ajax filter function - takes the incoming string, matches against columns 
-	 * and outputs view of the matching data
-	 */	
-	public function filters() {
-	  $this->use_layout=false;
-	  $cat = new CmsCategory;
-	  if(strlen($fil = $_POST['filter'])<1) $this->all_categories = $cat->order("name ASC")->all();
-  	else $this->all_categories = $cat->filter("name LIKE '%$fil%'")->order("name ASC")->all();
-  	$this->use_view="_cat_list";
-	}
+
 
 }
 
