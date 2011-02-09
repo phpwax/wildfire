e24d93828a4c0f7afcbb841958fc348816af22a6
charlesmarshall <cm56marshall@googlemail.com>
2011-02-09 13:01:56 +0000

    moving around the columns, adding grouping on to them and order the setup to match the display for readability

--- lib/model/WildfireContent.php
+++ lib/model/WildfireContent.php
@@ -1,41 +1,43 @@
 <?php
 
 class WildfireContent extends WaxTreeModel {
-  
+
 
 	public function setup(){
-	  $this->define("status", "IntegerField", array('maxlength'=>2, "widget"=>"SelectInput", "choices"=>array(0=>"Draft/Revision",1=>"Live"), 'scaffold'=>true, 'group'=>'main details'));
-		$this->define("title", "CharField", array('maxlength'=>255, 'scaffold'=>true, 'group'=>"main details") );
-		$this->define("excerpt", "TextField");
-		$this->define("content", "TextField", array('group'=>'main details'));
+	  $this->define("status", "IntegerField", array('maxlength'=>2, "widget"=>"SelectInput", "choices"=>array(0=>"Draft/Revision",1=>"Live"), 'scaffold'=>true, 'editable'=>false));
 		
+		$this->define("title", "CharField", array('maxlength'=>255, 'scaffold'=>true) );
+		$this->define("content", "TextField");
+
 		$this->define("date_start", "DateTimeField", array('scaffold'=>true));
 		$this->define("date_end", "DateTimeField", array('scaffold'=>true));
-		
+
+		$this->define("files", "ManyToManyField", array('target_model'=>"WildfireFile", "eager_loading"=>true, "join_model_class"=>"WaxModelOrderedJoin", "join_order"=>"join_order", 'group'=>'files'));
+		$this->define("categories", "ManyToManyField", array('target_model'=>"WildfireCategory","eager_loading"=>true, "join_model_class"=>"WaxModelOrderedJoin", "join_order"=>"id", 'scaffold'=>true, 'group'=>'joins'));		
+
+		$this->define("excerpt", "TextField", array('group'=>'others'));
+		$this->define("meta_description", "TextField", array('group'=>'others'));
+		$this->define("meta_keywords", "TextField", array('group'=>'others'));
+
+		//hidden extras
+		$this->define("author", "ForeignKey", array('target_model'=>"WildfireUser", 'scaffold'=>true,'group'=>'others', 'editable'=>false));
+		$this->define("sort", "IntegerField", array('maxlength'=>3, "editable"=>false));
 		$this->define("date_modified", "DateTimeField", array("editable"=>false));
 		$this->define("date_created", "DateTimeField", array("editable"=>false));
-		
-		$this->define("sort", "IntegerField", array('maxlength'=>3, "editable"=>false));
-		
-		$this->define("meta_description", "TextField");
-		$this->define("meta_keywords", "TextField");
-				
-		$this->define("files", "ManyToManyField", array('target_model'=>"WildfireFile", 'editable'=>false, "eager_loading"=>true, "join_model_class"=>"WaxModelOrderedJoin", "join_order"=>"join_order"));
-		$this->define("categories", "ManyToManyField", array('target_model'=>"WildfireCategory",'editable'=>false, "eager_loading"=>true, "join_model_class"=>"WaxModelOrderedJoin", "join_order"=>"id", 'scaffold'=>true));
-		$this->define("author", "ForeignKey", array('target_model'=>"WildfireUser", 'scaffold'=>true));
+
 		//these are here just for simplicity so dont have to cross joins all the time
 		$this->define("language", "IntegerField", array("editable"=>false));
-		$this->define("permalink", "CharField");
+		$this->define("permalink", "CharField", array('editable'=>false));
 	}
-		
-		
+
+
 	public function format_content() {
     return CmsTextFilter::filter("before_output", $this->content);
   }
-	
-	
 
-  
+
+
+
   /***** Finders for dealing with the extra_content table ******/
 
   /* delete bits form join table -now handled by the field */
@@ -56,7 +58,7 @@ class WildfireContent extends WaxTreeModel {
     }
     return $ret;
 	}
-	
+
 	// extend delete to get rid of related items for that piece of content
 	public function delete(){
 	  $ret = parent::delete();
@@ -64,5 +66,5 @@ class WildfireContent extends WaxTreeModel {
 	  $rel->filter("(source_model = ? AND source_id = ?) OR (dest_model = ? AND dest_id = ?)", array(get_class($this), $this->primval(), get_class($this), $this->primval()))->delete();
 	  return $ret;
 	}
-	
+
 }
