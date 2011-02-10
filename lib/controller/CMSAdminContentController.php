6794e9575413a005f4e490db9021c5fa5597f808
charlesmarshall <cm56marshall@googlemail.com>
2011-02-10 09:52:20 +0000

    tidy this to match controller, ie move the events to the events function

--- lib/controller/CMSAdminContentController.php
+++ lib/controller/CMSAdminContentController.php
@@ -4,19 +4,27 @@
 * @package PHP-WAX CMS
 */
 class CMSAdminContentController extends AdminComponent {
-	public $module_name = "content";											
+	public $module_name = "content";
 	public $model_class = 'WildfireContent';
 	public $display_name = "Content";
-	
+
 	public $filter_fields=array(
                           'text' => array('columns'=>array('title'), 'partial'=>'_filters_text'),
                           'parent' => array('columns'=>array('parent_id'), 'partial'=>'_filters_parent')
 	                      );
-	
-	public function initialise(){
-	  parent::initialise();
-	  
-	  WaxEvent::add("cms.permissions.tree_creation", function() {
+
+	protected function events(){
+	  parent::events();
+	  //custom events
+	  WaxEvent::add("cms.index.all", function(){
+	    $obj = WaxEvent::$data;
+	    $obj->model = $obj->_handle_filters(new $obj->model_class($obj->model_scope), Request::param('filters'));
+	    $obj->cms_content = $obj->model->tree();
+      //foreach($obj->cms_content as $c) print_r($c);
+      //exit;
+    });
+
+    WaxEvent::add("cms.permissions.tree_creation", function() {
       $obj = WaxEvent::$data;
       $model = new $obj->model_class;
 	    foreach($model->tree() as $r){ //all sections
@@ -24,16 +32,19 @@ class CMSAdminContentController extends AdminComponent {
     	  $obj->possible_parents[$r->primval] = $tmp.$r->title;
       }
     });
-    WaxEvent::run("cms.permissions.tree_creation",$this);
-    
 	}
-	
-	
+
+	protected function initialise(){
+	  parent::initialise();
+    WaxEvent::run("cms.permissions.tree_creation",$this);
+	}
+
+
 	public function save_before(){
 	  //if the parameter is to create a revision by copying the joins over
 	  if(Request::param('revision')){
 	    $this->model = $this->model->copy();
-	    $this->form = new WaxForm($this->model);	    
+	    $this->form = new WaxForm($this->model);
     }
 	}
 	//use the after to check if this was published or not
@@ -44,6 +55,6 @@ class CMSAdminContentController extends AdminComponent {
 	  elseif(Request::param('revision')) $this->model->hide();
 	  $this->redirect_to("/".trim($this->controller,"/")."/edit/".$this->model->primval."/");
 	}
-	
+
 }
 ?>
