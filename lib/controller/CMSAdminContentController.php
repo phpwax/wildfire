47d9b582a34034194cf2875f0442ce036f6798ba
charlesmarshall <cm56marshall@googlemail.com>
2011-02-10 16:38:14 +0000

    these are now events

--- lib/controller/CMSAdminContentController.php
+++ lib/controller/CMSAdminContentController.php
@@ -50,29 +50,15 @@ class CMSAdminContentController extends AdminComponent {
 	    if(!isset($obj->model_filters['parent'])) $obj->cms_content = $obj->model->roots();
     });
 
+    WaxEvent::add("cms.model.tree", function(){});
+
 	}
 
 	protected function initialise(){
 	  parent::initialise();
-    WaxEvent::run("cms.permissions.tree_creation",$this);
+    WaxEvent::run("cms.model.tree",$this);
 	}
 
-
-	public function save_before(){
-	  //if the parameter is to create a revision by copying the joins over
-	  if(Request::param('revision')){
-	    $this->model = $this->model->copy();
-	    $this->form = new WaxForm($this->model);
-    }
-	}
-	//use the after to check if this was published or not
-	public function save_success(){
-	  //put this model live if its set to
-	  if(Request::param('live')) $this->model->show()->update_url_map(1);
-	  elseif(Request::param('hide')) $this->model->hide()->update_url_map(0);
-	  elseif(Request::param('revision')) $this->model->hide();
-	  $this->redirect_to("/".trim($this->controller,"/")."/edit/".$this->model->primval."/");
-	}
 
 }
 ?>
