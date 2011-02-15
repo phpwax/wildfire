e6dbcc67e7d7c380ea012c1b4a09d8a5b5ec4b7e
charlesmarshall <cm56marshall@googlemail.com>
2011-02-15 12:23:35 +0000

    similar functionality to the home page functions, fetch the children and insert to dom

---
+++ resources/public/javascripts/wildfire/content/edit/children-insert.js
@@ -0,0 +1,31 @@
+jQuery(document).ready(function(){
+  
+  jQuery('.content_parent .has-children a').live("click",function(){
+    var link = jQuery(this),
+        dest = link.attr("href"),
+        branch = jQuery(link.parents('li')[0])
+        ;
+    if(link.hasClass('open')){
+      link.removeClass("open");
+      branch.children("ul").slideUp("fast");
+      return false;
+    }else if(link.hasClass('fetched')){
+      link.addClass("open");
+      branch.children("ul").slideDown("fast");
+      return false;
+    }
+    jQuery.ajax({
+      url:dest,
+      type:"post",
+      success:function(res){
+        link.addClass("open").addClass("fetched");
+        branch.append(res);
+      },
+      error:function(){}
+    });
+    
+    return false;
+    
+  });
+  
+});
