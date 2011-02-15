a5a2180e5e3b60a882f8cdc478146ac1047c9bfb
charlesmarshall <cm56marshall@googlemail.com>
2011-02-15 12:05:13 +0000

    remove sorting js

--- resources/public/javascripts/wildfire/sortable.js
+++ resources/public/javascripts/wildfire/sortable.js
@@ -1,22 +1,3 @@
 jQuery(document).ready(function(){
-  jQuery(".tree").sortable({  		
-  			items: 'li',
-  			tolerance: 'pointer',
-  			toleranceElement: '> a',
-  			connectWith:'.tree'
-  });
-  jQuery(".tree").disableSelection();
-  
-  
-  jQuery(".tree" ).bind("sortstop", function(eve, ui) {
-    var moved = jQuery(ui.item), 
-        p_tree = moved.parents("ul.tree")[0], 
-        parent_id = jQuery(p_tree).attr('data-parent-value'), 
-        pos = moved.index(),
-        parent_ele = jQuery("#"+jQuery(p_tree).attr("data-parent-form-element")),
-        sort_ele = jQuery("#"+jQuery(p_tree).attr("data-sort-form-element"))
-        ;
-    parent_ele.val(parent_id);
-    sort_ele.val(pos);
-  });
+
 });
