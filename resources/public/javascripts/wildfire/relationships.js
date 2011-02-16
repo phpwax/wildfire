701e419bc46abe890595665d7e2ed29b75b13758
charlesmarshall <cm56marshall@googlemail.com>
2011-02-16 15:12:32 +0000

    remove the log

--- resources/public/javascripts/wildfire/relationships.js
+++ resources/public/javascripts/wildfire/relationships.js
@@ -2,7 +2,6 @@ jQuery(document).ready(function(){
   
   jQuery("#relationships .join-option input[type=checkbox]").live("click", function(){
     var ele = jQuery(this), checked = ele.attr('checked'), parent = ele.parents("li")
-    console.log(parent);
     if(checked) parent.removeClass('join-no').addClass('join-yes');
     else parent.addClass('join-no').removeClass('join-yes');
     
