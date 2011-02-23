2444f86d9daee1abc6fb35acab1ba68152a17dfd
charlesmarshall <cm56marshall@googlemail.com>
2011-02-23 16:54:31 +0000

    change that to show the first tab only

--- resources/public/javascripts/wildfire/content/edit/disabled.js
+++ resources/public/javascripts/wildfire/content/edit/disabled.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function(){
   jQuery('.edit_disabled .field, .edit_disabled div.form-submit').hide();
-  jQuery('.edit_disabled .tabs li:not(#tab-editing-content, #tab-all-versions)').hide();
+  jQuery('.edit_disabled .tabs li:not(:eq(0))').hide();
   
   jQuery('.make_editable').live("click", function(){
     jQuery('.edit_disabled .field, .edit_disabled div.form-submit').show();
