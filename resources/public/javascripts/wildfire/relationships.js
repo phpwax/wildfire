cc236c3c4d4ecccdf7f6359f442d83b442a311a7
charlesmarshall <cm56marshall@googlemail.com>
2011-02-23 23:45:33 +0000

    change the selector

--- resources/public/javascripts/wildfire/relationships.js
+++ resources/public/javascripts/wildfire/relationships.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function(){
   
-  jQuery("#relationships .join-option input[type=checkbox], #relationships .join-option input[type=radio]").live("click", function(){
+  jQuery(".relationships .join-option input[type=checkbox], .relationships .join-option input[type=radio]").live("click", function(){
     var ele = jQuery(this), checked = ele.attr('checked'), parent = ele.parents("li"), bool = (ele.attr('type') == "radio")?true:false;
     if(bool) parent.siblings("li").removeClass("join-yes").addClass('join-no');
     if(checked) parent.removeClass('join-no').addClass('join-yes');
