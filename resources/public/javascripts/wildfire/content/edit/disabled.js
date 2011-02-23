d10e03837fe768dadd482025a7b8165c10067f4a
charlesmarshall <cm56marshall@googlemail.com>
2011-02-23 20:02:26 +0000

    doh, when i updated the hide, forgot to update the show

--- resources/public/javascripts/wildfire/content/edit/disabled.js
+++ resources/public/javascripts/wildfire/content/edit/disabled.js
@@ -4,7 +4,7 @@ jQuery(document).ready(function(){
   
   jQuery('.make_editable').live("click", function(){
     jQuery('.edit_disabled .field, .edit_disabled div.form-submit').show();
-    jQuery('.edit_disabled .tabs li:not(#tab-editing-content, #tab-all-versions)').show();
+    jQuery('.edit_disabled .tabs li:not(:eq(0))').show();
     return false;
   });
   
