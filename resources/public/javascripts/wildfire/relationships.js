4a2f2cf077749340d4b297ac05a18a6d43956991
charlesmarshall <cm56marshall@googlemail.com>
2011-02-23 15:13:05 +0000

    working single select radio

--- resources/public/javascripts/wildfire/relationships.js
+++ resources/public/javascripts/wildfire/relationships.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function(){
   
-  jQuery("#relationships .join-option input[type=checkbox]").live("click", function(){
+  jQuery("#relationships .join-option input[type=checkbox], #relationships .join-option input[type=radio]").live("click", function(){
     var ele = jQuery(this), checked = ele.attr('checked'), parent = ele.parents("li")
     if(checked) parent.removeClass('join-no').addClass('join-yes');
     else parent.addClass('join-no').removeClass('join-yes');
