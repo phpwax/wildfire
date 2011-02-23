0c523f5e0372b8d7a1dad0afb9edbdc2f3643819
charlesmarshall <cm56marshall@googlemail.com>
2011-02-23 12:03:51 +0000

    fix the inserted string

--- resources/public/javascripts/wildfire/content/edit/file-joins.js
+++ resources/public/javascripts/wildfire/content/edit/file-joins.js
@@ -36,12 +36,13 @@ jQuery(document).ready(function(){
       bu.removeClass('add-button').addClass('remove-button').html('REMOVE');
       //if this was a info panel add, copy the item in to the exisiting files block
       if(!checkbox_ele.hasClass('joined_field')){
-        tag_option_str = "<div class='tag_options clearfix'>";
+        var tag_option_str = "<div class='tag_options clearfix'>";
         for(x in file_tags){
           tag_option_str += "<div class='clearfix'><input type='radio' name='tags["+fid+"][tag]' value='"+file_tags[x]+"' id='inserted_tag_"+fid+"_"+x+"' class='radio_field'"+(x==0?" checked='checked'": "")+"><label for='inserted_tag_"+fid+"_"+x+"'>"+file_tags[x]+"</label></div>";
         }
         tag_option_str += "</div>";
-        insert_str = '<div class="joined-file clearfix f'+fid+'"><input type="hidden" name="tags['+fid+'][order]" value="0" class="join-order-field"><img src="'+jQuery(imgs).attr('src')+'" alt="'+jQuery(imgs).attr('alt')+'">'+tag_option_str+'<a id="button-joined_file_'+fid+'" class="button js-added remove-button" data-input-link="join_file_'+fid+'" data-fileid="'+fid+'" href="#">REMOVE</a></div>';
+        var ord = jQuery('#exisiting-files .joined-file').length;
+        insert_str = '<div class="joined-file clearfix f'+fid+'"><input type="hidden" name="tags['+fid+'][order]" value="'+ord+'" class="join-order-field"><img src="'+jQuery(imgs).attr('src')+'" alt="'+jQuery(imgs).attr('alt')+'">'+tag_option_str+'<a id="button-joined_file_'+fid+'" class="button js-added remove-button" data-input-link="join_file_'+fid+'" data-fileid="'+fid+'" href="#">REMOVE</a></div>';
         jQuery('#exisiting-files').append(insert_str);
       }
       checkbox_ele.attr('checked', 'checked');
