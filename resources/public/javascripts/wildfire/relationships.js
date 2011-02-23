6a924162621e86ff86db2c0a0e7ba5b075e68d83
charlesmarshall <cm56marshall@googlemail.com>
2011-02-23 16:24:30 +0000

    on any radio boxes turn all the others off

--- resources/public/javascripts/wildfire/relationships.js
+++ resources/public/javascripts/wildfire/relationships.js
@@ -1,7 +1,8 @@
 jQuery(document).ready(function(){
   
   jQuery("#relationships .join-option input[type=checkbox], #relationships .join-option input[type=radio]").live("click", function(){
-    var ele = jQuery(this), checked = ele.attr('checked'), parent = ele.parents("li")
+    var ele = jQuery(this), checked = ele.attr('checked'), parent = ele.parents("li"), bool = (ele.attr('type') == "radio")?true:false;
+    if(bool) parent.siblings("li").removeClass("join-yes").addClass('join-no');
     if(checked) parent.removeClass('join-no').addClass('join-yes');
     else parent.addClass('join-no').removeClass('join-yes');
     
