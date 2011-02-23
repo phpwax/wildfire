f49c455bef83e362fb646774fde4dd1bc360a294
charlesmarshall <cm56marshall@googlemail.com>
2011-02-23 12:56:32 +0000

    put a live / hide check in as well

--- lib/controller/CMSAdminComponent.php
+++ lib/controller/CMSAdminComponent.php
@@ -159,6 +159,9 @@ class CMSAdminComponent extends CMSBaseComponent {
     WaxEvent::add("cms.save.after", function(){});
     WaxEvent::add("cms.save.success", function(){
       $obj = WaxEvent::data();
+      if(Request::param('live')) $obj->model->update_attributes(array('status'=>1));
+      elseif(Request::param('hide')) $obj->model->update_attributes(array('status'=>0));
+      
       WaxEvent::run('cms.joins.handle', $obj);
       WaxEvent::run('cms.file.tag', $obj);
     });
