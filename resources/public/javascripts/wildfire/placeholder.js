ff54986224c4a7ec6361c3c671d1af7fb4cb0340
charlesmarshall <cm56marshall@googlemail.com>
2011-03-15 13:39:36 +0000

    tweak the placeholder js

--- resources/public/javascripts/wildfire/placeholder.js
+++ resources/public/javascripts/wildfire/placeholder.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function(){
   var ua_placeholder = (jQuery.browser == "webkit")?true:false; 
   if(!ua_placeholder){
     jQuery('input[type="text"]').each(function(){
-      if(jQuery(this).attr('placeholder') && (jQuery(this).val() == jQuery(this).attr('placeholder') || jQuery(this).val() == '' )) jQuery(this).val(jQuery(this).attr('placeholder'));
+      if(jQuery(this).attr('placeholder') && (jQuery(this).val() == jQuery(this).attr('placeholder') || jQuery(this).val() == '' || jQuery(this).val() == 0 || jQuery(this).val() == 0.0 )) jQuery(this).val(jQuery(this).attr('placeholder'));
     }).live("focus", function(){          
       if(jQuery(this).attr('placeholder')  && jQuery(this).val() == jQuery(this).attr('placeholder')) jQuery(this).val('');
     }).live("blur", function(){           
