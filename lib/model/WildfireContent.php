6a91ac45fc0b2927421072a3ed1a89b16f359bfc
charlesmarshall <cm56marshall@googlemail.com>
2011-03-17 13:03:15 +0000

    default to the past

--- lib/model/WildfireContent.php
+++ lib/model/WildfireContent.php
@@ -10,7 +10,7 @@ class WildfireContent extends WaxTreeModel {
 		$this->define("content", "TextField", array('widget'=>"TinymceTextareaInput"));
 
 		$this->define("date_start", "DateTimeField", array('scaffold'=>true, 'default'=>date("Y-m-d h:i:s"), 'output_format'=>"j F Y H:i"));
-		$this->define("date_end", "DateTimeField", array('scaffold'=>true, 'default'=>date("Y-m-d h:i:s",mktime(0,0,0, date("m"), date("j"), date("y")+10 )), 'output_format'=>"j F Y H:i" ));
+		$this->define("date_end", "DateTimeField", array('scaffold'=>true, 'default'=>date("Y-m-d h:i:s",mktime(0,0,0, date("m"), date("j"), date("y")-10 )), 'output_format'=>"j F Y H:i" ));
 
 		$this->define("files", "ManyToManyField", array('target_model'=>"WildfireFile", "eager_loading"=>true, "join_model_class"=>"WildfireOrderedTagJoin", "join_order"=>"join_order", 'group'=>'files'));
 		$this->define("categories", "ManyToManyField", array('target_model'=>"WildfireCategory","eager_loading"=>true, "join_model_class"=>"WaxModelOrderedJoin", "join_order"=>"id", 'scaffold'=>true, 'group'=>'relationships'));
