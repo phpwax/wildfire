09a1d5032d9714aaaf4fe6c64890ca37d2b0014b
Sheldon <sheldon.els@gmail.com>
2011-05-31 09:39:11 +0100

    initial stab at image editing, dropped out cropping plugin for now, not sure about compatibility with draggable/zoomable etc.

--- resources/public/javascripts/wildfire/files/edit.js
+++ resources/public/javascripts/wildfire/files/edit.js
@@ -1,49 +1,30 @@
-$(document).ready(function(){
-	
-	$('#crop_container .croppable').Jcrop({
-		bgColor:     'black',
-		bgOpacity:   .4,
-		onChange: set_coords,
-		onSelect: set_coords
-	});
-	
-	$("#crop_save").click(function(){
-		$.ajax({
-		  url: $(this).attr("href"),
-		  type: "POST",
-		  data: $(".coords").serialize(),	
-		  complete: function() {window.location.reload();},
-		  error: function() {alert("The image could not be saved!");}
-		});
-		
-		
-		return false;		
-	});
-	
-	$(".operation_delete").live("click",function(){
-		var conf = confirm("Are you sure you want to permanently delete this?");
-		if(!conf) return false;
-		var file = $(this).attr("data-file");
-		$.ajax({
-		  url: $(this).attr("href"),
-		  type: "GET",
-		  data: {},	
-		  complete: function() {
-				$(".file-tree-container .node[rel='"+file+"']").remove();
-			},
-		  error: function() {alert("The file could not be deleted!");}
-		});
-		return false;
-	});
-	
+jQuery(function(){
+  if(jQuery(".zoomable_image img").length){
+    jQuery(".zoomable_image img").draggable();
+    jQuery(".image_zoom_select").change(function(){
+      var t = jQuery(jQuery(this).data("zoom-target"));
+      var v = jQuery(this).val();
+      if(v == "to_fit") t.width("100%");
+      else{
+        t.width(t.data("original-width") * v);
+        t.attr("data-image-zoom", v);
+      }
+    });
+  }
+  
+  jQuery(".operation_delete").live("click",function(){
+    var conf = confirm("Are you sure you want to permanently delete this?");
+    if(!conf) return false;
+    var file = jQuery(this).attr("data-file");
+    jQuery.ajax({
+      url: jQuery(this).attr("href"),
+      type: "GET",
+      data: {},  
+      complete: function() {
+        jQuery(".file-tree-container .node[rel='"+file+"']").remove();
+      },
+      error: function() {alert("The file could not be deleted!");}
+    });
+    return false;
+  });
 });
-
-
-function set_coords(c) {
-	$('#x1').val(c.x);
-	$('#y1').val(c.y);
-	$('#x2').val(c.x2);
-	$('#y2').val(c.y2);
-	$('#w').val(c.w);
-	$('#h').val(c.h);
-};
