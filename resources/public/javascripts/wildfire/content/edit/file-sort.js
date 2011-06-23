e8838aaf017df10c235be0b630f99dc6c7e4f257
Sheldon <sheldon.els@gmail.com>
2011-06-23 13:10:27 +0100

    much quicker sorting in js for files

--- resources/public/javascripts/wildfire/content/edit/file-sort.js
+++ resources/public/javascripts/wildfire/content/edit/file-sort.js
@@ -3,9 +3,8 @@ jQuery(document).ready(function(){
   jQuery("#existing-files").sortable({
     items:".joined-file",
     update:function(event, ui){
-      jQuery("#existing-files .joined-file").each(function(){
-        var i = jQuery(this).index("#existing-files .joined-file");
-        jQuery(this).find('.join-order-field').val(i);
+      jQuery("#existing-files .joined-file .join-order-field").each(function(i){
+        jQuery(this).val(i);
       });
     }
   });
