5d33adeaa5135f368de10b2dcfb464c2f4c3e51e
Sheldon <sheldon.els@gmail.com>
2011-06-29 14:37:15 +0100

    html5 ajax uploader fix to reset file input field

--- resources/public/javascripts/wildfire-plugins/html5-uploader/html5-uploader.js
+++ resources/public/javascripts/wildfire-plugins/html5-uploader/html5-uploader.js
@@ -56,6 +56,8 @@ var saf = jQuery.browser.safari, vn = parseInt(jQuery.browser.version); //seems 
 			progressBarContainer.className += " uploaded";
 			progressBar.innerHTML = "Uploaded!";
 			jQuery(li).fadeOut(15000);
+			jQuery("#files-upload").replaceWith(clear_file_upload);
+  	  clear_file_upload = jQuery("#files-upload").clone();
 		}, false);
 
 		xhr.open("post", jQuery("#upload-form").attr("data-html5-upload"), true);
@@ -99,8 +101,11 @@ var saf = jQuery.browser.safari, vn = parseInt(jQuery.browser.version); //seems 
 			fileList.innerHTML = "No support for the File API in this web browser";
 		}
 	}
+  var clear_file_upload;
 	if(filesUpload && (!saf || (saf && vn >= 533) ) ){
-	  filesUpload.addEventListener("change", function () {
+	  clear_file_upload = jQuery("#files-upload").clone();
+	  
+	  jQuery("#files-upload").live("change", function () {
 		  traverseFiles(this.files);
 	  }, false);
 
