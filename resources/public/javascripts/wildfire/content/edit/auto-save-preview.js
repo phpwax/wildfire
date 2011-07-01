ffa88f8d87844c2bdedc31de196865da86efa83c
Sheldon <sheldon.els@gmail.com>
2011-07-01 13:17:38 +0100

    new autosave style & animation. breaks if you try interact while it's animating, can't seem to figure out a nice way to fix that. otherwise looks good.

--- resources/public/javascripts/wildfire/content/edit/auto-save-preview.js
+++ resources/public/javascripts/wildfire/content/edit/auto-save-preview.js
@@ -1,4 +1,22 @@
 jQuery(document).ready(function(){
+  function span_hide(el, after){
+    el.delay(2000).clearQueue().animate({
+      "width": '1px',
+      "margin-right": '0'
+    }, 500, function(){
+      el.css({"display":"none", "width":""});
+      if(after) after();
+    });
+  }
+  
+  function span_show(el, after){
+    var target_width = el.width();
+    el.css({"display":"inline", "width":"1px"}).clearQueue().animate({
+      "width": target_width,
+      "margin-right": '5px'
+    }, 500, after);
+  }
+  
   function auto_save_form(auto_span, auto_saver, auto_image, preview){
     var endpoint = auto_span.attr('data-save-point'),
         action = auto_span.attr("data-controller")+"edit/";
@@ -6,8 +24,14 @@ jQuery(document).ready(function(){
         form_data = form_container.serialize();
     
     if(auto_save_signature != form_data){
-      var blink_image = function(){auto_image.fadeToggle("fast");},
-          blink_timer = setInterval(function(){blink_image();}, 10);
+      auto_image.attr("src", auto_image.attr("data-saving"));
+      span_show(auto_span, function(){
+        show_timeout = setTimeout(function(){
+          span_hide(auto_span, function(){
+            auto_image.attr("src", auto_image_src);
+          });
+        }, 2000);
+      });
       
       jQuery.ajax({
         url:endpoint,
@@ -16,12 +40,12 @@ jQuery(document).ready(function(){
         dataType:"json",
         async:preview?false:true,
         success:function(res){
-          if(res['id']) auto_span.attr('data-save-point', action+res['id']+".json");
-          clearInterval(blink_timer);
-          auto_image.fadeIn("fast");
           auto_save_signature = form_data;
-          preview.attr("href", res['permalink']+"?preview="+res['id']).removeClass('loading');
-          if(preview) window.open(preview.attr("href"));
+          if(res['id']) auto_span.attr('data-save-point', action+res['id']+".json");
+          if(preview){
+            preview.attr("href", res['permalink']+"?preview="+res['id']).removeClass('loading');
+            window.open(preview.attr("href"));
+          }
         },
         error:function(){clearInterval(blink_timer);}
       });
@@ -31,16 +55,38 @@ jQuery(document).ready(function(){
   var auto_saver = jQuery('#auto-save'),
       auto_span = auto_saver.find("span"),
       auto_image = auto_saver.find("img"),
-      auto_save_time = (typeof auto_save_time_override != "undefined")?auto_save_time_override : 30000,
+      auto_image_src = auto_image.attr("src"),
+      auto_save_time = (typeof auto_save_time_override != "undefined")?auto_save_time_override : 20000,
       auto_save_signature = auto_span.closest("form").serialize(),
+      auto_interval;
+  
+  setTimeout(function(){
+    span_hide(jQuery('#auto-save span'), function(){
+      jQuery('#auto-save span').text("Auto Saving");
       auto_interval = setInterval(function(){
-        if(auto_span.hasClass('enabled')) auto_save_form(auto_span,auto_saver,auto_image);
+        if(auto_saver.hasClass('enabled')) auto_save_form(auto_span, auto_saver, auto_image);
       }, auto_save_time);
+    });
+  }, 2000);
   
   jQuery('#auto-save').live("click", function(e){
-    auto_span.toggleClass('enabled');
-    if(auto_span.hasClass('enabled')) auto_image.attr('src', auto_image.attr('src').replace('-off', ''));
-    else auto_image.attr('src', auto_image.attr('src').replace('.png', '-off.png'));
+    var show_timeout;
+    clearTimeout(show_timeout);
+    auto_saver.toggleClass('enabled');
+    if(auto_saver.hasClass('enabled')){
+      auto_image.attr('src', auto_image_src);
+      auto_span.text("Auto Save Enabled");
+    }else{
+      auto_image.attr('src', auto_image.attr('data-off'));
+      auto_span.text("Auto Save Disabled");
+    }
+    span_show(auto_span, function(){
+      show_timeout = setTimeout(function(){
+        span_hide(auto_span, function(){
+          auto_span.text("Auto Saving");
+        });
+      }, 2000);
+    });
     e.preventDefault();
   });
   
