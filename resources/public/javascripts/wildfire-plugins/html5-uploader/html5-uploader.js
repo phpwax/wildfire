43db6c5cf4ae19caf3ae8bd0368ef2aeacf21b63
Mike <michal.noskovic@googlemail.com>
2011-08-08 13:03:56 +0100

    bad addEventListener good bind

--- resources/public/javascripts/wildfire-plugins/html5-uploader/html5-uploader.js
+++ resources/public/javascripts/wildfire-plugins/html5-uploader/html5-uploader.js
@@ -108,26 +108,26 @@ var saf = jQuery.browser.safari, vn = parseInt(jQuery.browser.version); //seems 
 	  jQuery("#files-upload").live("change", function () {
 		  traverseFiles(this.files);
 	  }, false);
-
-	  dropArea.addEventListener("dragleave", function (evt) {
+    
+	  jQuery(dropArea).bind("dragleave", function (evt) {
 		  var target = evt.target;
 		  if (target && target === dropArea) this.className = "";
 		  evt.preventDefault();
 		  evt.stopPropagation();
 	  }, false);
 
-	  dropArea.addEventListener("dragenter", function (evt) {
+	  jQuery(dropArea).bind("dragenter", function (evt) {
 		  this.className = "over";
 		  evt.preventDefault();
 		  evt.stopPropagation();
 	  }, false);
 
-	  dropArea.addEventListener("dragover", function (evt) {
+	  jQuery(dropArea).bind("dragover", function (evt) {
 		  evt.preventDefault();
 		  evt.stopPropagation();
 	  }, false);
 
-	  dropArea.addEventListener("drop", function (evt) {
+	  jQuery(dropArea).bind("drop", function (evt) {
 		  traverseFiles(evt.dataTransfer.files);
 		  this.className = "";
 		  evt.preventDefault();
