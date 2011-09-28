821afb91ebbd05b47af1c3dd49e60e04173e2b6f
charlesmarshall <cm56marshall@googlemail.com>
2011-09-28 10:56:58 +0100

    move tinymce config to vars so can be overwritten

--- resources/public/javascripts/wildfire/tinymce/_init.js
+++ resources/public/javascripts/wildfire/tinymce/_init.js
@@ -1,48 +1,53 @@
+var tinymce_config = {
+      // Location of TinyMCE script
+      script_url : '/tinymce/jscripts/tiny_mce/tiny_mce.js',
+      relative_urls: false,
+      theme: 'advanced',
+      skin:'o2k7',
+      skin_variant : "silver",
+      height:300,
+      //
+      plugins: 'contextmenu,directionality,jqueryinlinepopups,noneditable,paste,wflink,wfimage,wfhtml,wftable,wftemplate',
+      theme_advanced_blockformats: "p,h2,h3,h4,blockquote",
+      content_css: "/stylesheets/build/wildfire-templates_combined.css",
+      // Theme options
+    	theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,bullist,numlist,|,link,unlink,image,|,hr,|,code,|,table,|,row_before,row_after,delete_row,|,col_before,col_after,delete_col",
+    	theme_advanced_buttons2 : "formatselect,template",
+    	theme_advanced_buttons3 : "",
+    	theme_advanced_buttons4 : "",
+    	theme_advanced_toolbar_location : "top",
+    	theme_advanced_toolbar_align : "left",
+    	theme_advanced_statusbar_location : "bottom",
+    	theme_advanced_resizing : true
+    	//
+    },
+    simpletinymce_config = {
+      // Location of TinyMCE script
+      script_url : '/tinymce/jscripts/tiny_mce/tiny_mce.js',
+      relative_urls: false,
+      theme: 'advanced',
+      skin:'o2k7',
+      skin_variant : "silver",
+      //
+      plugins: 'contextmenu,directionality,jqueryinlinepopups,noneditable,paste,wflink',
+      // Theme options
+  		theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,link,unlink,|,hr",
+  		theme_advanced_buttons2 : "",
+  		theme_advanced_buttons3 : "",
+  		theme_advanced_buttons4 : "",
+  		theme_advanced_toolbar_location : "top",
+  		theme_advanced_toolbar_align : "left",
+  		theme_advanced_statusbar_location : "bottom",
+  		theme_advanced_resizing : true
+  		//
+    }
+    ;
+    
+
 jQuery(document).ready(function(){
-  var tinymce = jQuery('textarea.tinymce').tinymce({
-    // Location of TinyMCE script
-    script_url : '/tinymce/jscripts/tiny_mce/tiny_mce.js',
-    relative_urls: false,
-    theme: 'advanced',
-    skin:'o2k7',
-    skin_variant : "silver",
-    height:300,
-    //
-    plugins: 'contextmenu,directionality,jqueryinlinepopups,noneditable,paste,wflink,wfimage,wfhtml,wftable,wftemplate',
-    theme_advanced_blockformats: "p,h2,h3,h4,blockquote",
-    content_css: "/stylesheets/build/wildfire-templates_combined.css",
-    // Theme options
-		theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,bullist,numlist,|,link,unlink,image,|,hr,|,code,|,table,|,row_before,row_after,delete_row,|,col_before,col_after,delete_col",
-		theme_advanced_buttons2 : "formatselect,template",
-		theme_advanced_buttons3 : "",
-		theme_advanced_buttons4 : "",
-		theme_advanced_toolbar_location : "top",
-		theme_advanced_toolbar_align : "left",
-		theme_advanced_statusbar_location : "bottom",
-		theme_advanced_resizing : true
-		//
-  });
+  var tinymce = jQuery('textarea.tinymce').tinymce(tinymce_config);
   
-  var simpletinymce = jQuery('textarea.simpletinymce').tinymce({
-    // Location of TinyMCE script
-    script_url : '/tinymce/jscripts/tiny_mce/tiny_mce.js',
-    relative_urls: false,
-    theme: 'advanced',
-    skin:'o2k7',
-    skin_variant : "silver",
-    //
-    plugins: 'contextmenu,directionality,jqueryinlinepopups,noneditable,paste,wflink',
-    // Theme options
-		theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,link,unlink,|,hr",
-		theme_advanced_buttons2 : "",
-		theme_advanced_buttons3 : "",
-		theme_advanced_buttons4 : "",
-		theme_advanced_toolbar_location : "top",
-		theme_advanced_toolbar_align : "left",
-		theme_advanced_statusbar_location : "bottom",
-		theme_advanced_resizing : true
-		//
-  });
+  var simpletinymce = jQuery('textarea.simpletinymce').tinymce(simpletinymce_config);
 
   //hide the dialog boxes etc
   jQuery('#wildfire-link-dialog').hide();
