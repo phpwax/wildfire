e78ec84fb10cae1dd002e2573d16daf32458f77d
charlesmarshall <cm56marshall@googlemail.com>
2011-11-03 14:16:43 +0000

    js to send sort data

--- resources/public/javascripts/wildfire/sortable.js
+++ resources/public/javascripts/wildfire/sortable.js
@@ -1,3 +1,24 @@
 jQuery(document).ready(function(){
-
+  jQuery(".sortable-tree ul").sortable({
+    update:function(e,ui){
+      var obj = jQuery(this), 
+          parent = obj.parent(), 
+          ordering = {}, 
+          items = obj.find("li:not(.tree_headers)"),
+          dest = parent.attr("data-sort-action")
+          ;
+      items.each(function(){
+        ordering[jQuery(this).attr("data-row-id")] = items.index(jQuery(this));
+      });
+      jQuery.ajax({
+        url:dest,
+        type:"post",
+        data:{sort:ordering},
+        success:function(res){
+          
+        },
+        fail:function(){}
+      });
+    }
+  }).disableSelection();
 });
