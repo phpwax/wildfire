18f49e897e5250ba168fcb73139fbead9e695942
charlesmarshall <cm56marshall@googlemail.com>
2011-12-16 14:38:31 +0000

    remove the group

--- lib/model/WildfireContent.php
+++ lib/model/WildfireContent.php
@@ -49,7 +49,7 @@ class WildfireContent extends WaxTreeModel {
 	  if(!$this->parent_column) $this->parent_column = "parent";
     if(!$this->children_column) $this->children_column = "children";
     if(!$this->parent_join_field) $this->parent_join_field = $this->parent_column."_".$this->primary_key;
-	  $this->define($this->parent_column, "ForeignKey", array("col_name" => "parent_id", "target_model" => get_class($this), 'group'=>'parent', 'widget'=>'HiddenInput'));
+	  $this->define($this->parent_column, "ForeignKey", array("col_name" => "parent_id", "target_model" => get_class($this), 'widget'=>'HiddenInput'));
     $this->define($this->children_column, "HasManyField", array("target_model" => get_class($this), "join_field" => $this->parent_join_field, "eager_loading" => true, 'associations_block'=>true));
 	}
 
