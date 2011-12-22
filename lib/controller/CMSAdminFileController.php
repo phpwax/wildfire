d7359a702429c3bfaf188ce35c40e6d19f02fe46
charlesmarshall <cm56marshall@googlemail.com>
2011-12-22 12:28:40 +0000

    use updated attributes instead of assignments

--- lib/controller/CMSAdminFileController.php
+++ lib/controller/CMSAdminFileController.php
@@ -97,11 +97,9 @@ class CMSAdminFileController extends AdminComponent {
     $file = basename(Request::param("origin_file"));
     $model = new $this->model_class;
     $destination = Request::param("destination");
-    $f->rpath = $destination;
-    $f->path = $destination;
-    if($f->save()) {
       echo $f->id;
     $f = $model->filter("rpath", $origin)->filter("filename",$file)->first();
+    if($f->update_attributes(array('rpath'=>$destination, 'path'=>$destination)) ) {
 			exit;
     }
     
