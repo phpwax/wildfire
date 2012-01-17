7c051f65416495708723a78162bd0142a8f3f0e3
charlesmarshall <cm56marshall@googlemail.com>
2012-01-17 16:13:26 +0000

    starting the new media stuff ...
    this is the media 'account' model. from here you restrict what and where the media goes to. By default this will just be disk access; in the future will allow other classes, such as flickr / youtube api uploads and downloads based on the tokens used.

---
+++ lib/model/WildfireMediaAccount.php
@@ -0,0 +1,17 @@
+<?
+class WildfireMediaAccount extends WaxModel{
+  
+  public static $api_classes = array(''=>'-- Select --', 'WildfireDiskFile');
+
+  public function setup(){
+    $this->define("title", "CharField", array('required'=>true)); //visible name used
+    $this->define("token", "CharField"); //access token
+    $this->define("class", "CharField", array('widget'=>'SelectInput', 'choices'=>WildfireMediaAccount::$api_classes));
+  }
+
+  public function before_save(){
+    parent::before_save();
+    if(!$this->title) $this->title = "Media";
+  }
+}
+?>
