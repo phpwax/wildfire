a2ee3618d89f557fc9a7bdb7b23dac88c1e0938d
charlesmarshall <cm56marshall@googlemail.com>
2012-01-17 17:05:32 +0000

    bug fix

--- lib/model/Cron.php
+++ lib/model/Cron.php
@@ -2,21 +2,9 @@
 
 /* Media Processing Cron Jobs */
 
-class Cron {
-    
-  
-  public function process_video($width=false, $height=false, $owner = "apache") {
-  
-  }
-  
-  public function image_queue() {
-    $queu
-
-  }
-  
-  
-  
-  
+class Cron{
+  public function process_video($width=false, $height=false, $owner = "apache"){}
+  public function image_queue(){}
   
 }
 
