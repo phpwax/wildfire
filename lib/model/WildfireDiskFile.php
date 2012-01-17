a4f05aa889cce588d03963239dff70cdd6990f8d
charlesmarshall <cm56marshall@googlemail.com>
2012-01-17 21:14:10 +0000

    thats the set working

--- lib/model/WildfireDiskFile.php
+++ lib/model/WildfireDiskFile.php
@@ -1,17 +1,18 @@
 <?
 class WildfireDiskFile{
-  /**
-   * list of allowed formats for this interface
-   * - going to have to do file extensions :(
-   **/
-  public static $accepted_formats = array("jpg", "jpeg", "png", "gif", "doc", "xsl", "txt");
-
+  
   /**
    * this should handle the saving of the media to the disc
    **/
-  public function set($media_item){
-    //check its an allowed format
-    
+  public function set($media_item){  
+    if($media_item){
+      //set to 0, so unpublished
+      $media_item->update_attributes(array('status'=>0));
+      //the disk file has very little to do, just update with information about the file:
+      $file = PUBLIC_DIR.$media_item->uploaded_location;
+      if(is_readable($file)) return $media_item->update_attributes(array('status'=>1, 'source'=>$media_item->uploaded_location, 'media_class'=>get_class($this)));
+    }
+    return false;
   }
 
   public function get($media_item){
