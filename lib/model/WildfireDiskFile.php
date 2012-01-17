e5e2ca05ab1aa0c5feeac40b90ead6bc50639e9d
charlesmarshall <cm56marshall@googlemail.com>
2012-01-17 17:06:00 +0000

    start on the methods

--- lib/model/WildfireDiskFile.php
+++ lib/model/WildfireDiskFile.php
@@ -1,5 +1,27 @@
 <?
 class WildfireDiskFile{
-  
+  /**
+   * list of allowed formats for this interface
+   * - going to have to do file extensions :(
+   **/
+  public static $accepted_formats = array("jpg", "jpeg", "png", "gif", "doc", "xsl", "txt");
+
+  /**
+   * this should handle the saving of the media to the disc
+   **/
+  public function set($media_item){
+    //check its an allowed format
+    
+  }
+
+  public function get($media_item){
+    
+  }
+
+  public function show($media_item){
+    
+  }
+
+
 }
 ?>
