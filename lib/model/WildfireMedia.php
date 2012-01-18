ce1b2a8deee860f13dadb89cc9215b38799aa228
charlesmarshall <cm56marshall@googlemail.com>
2012-01-18 21:24:44 +0000

    add in the new file_meta stuff

--- lib/model/WildfireMedia.php
+++ lib/model/WildfireMedia.php
@@ -31,6 +31,35 @@ class WildfireMedia extends WaxModel{
     parent::setup();
   }
 
+  //this will need updating when the framework can handle manipulating join columns
+  public function file_meta_set($fileid, $tag, $order=0, $title=''){
+    $model = new WaxModel;
+    if($this->table < "wildfire_media") $model->table = $this->table."_wildfire_media";
+    else $model->table = "wildfire_media_".$this->table;
+    
+    $col = $this->table."_".$this->primary_key;
+    if(!$order) $order = 0;
+    if(($found = $model->filter($col, $this->primval)->filter("wildfire_media_id", $fileid)->all()) && $found->count()){
+      foreach($found as $r){
+        $sql = "UPDATE `".$model->table."` SET `join_order`=$order, `tag`='$tag', `title`='$title' WHERE `id`=$r->primval";
+        $model->query($sql);
+      }
+    }else{
+      $sql = "INSERT INTO `".$model->table."` (`wildfire_media_id`, `$col`, `join_order`, `tag`, `title`) VALUES ('$fileid', '$this->primval', '$order', '$tag', '$title')";
+      $model->query($sql);
+    }
+  }
+  public function file_meta_get($fileid=false, $tag=false){
+    $model = new WaxModel;
+    if($this->table < "wildfire_media") $model->table = $this->table."_wildfire_media";
+    else $model->table = "wildfire_media_".$this->table;
+    $col = $this->table."_".$this->primary_key;
+    if($fileid) return $model->filter($col, $this->primval)->filter("wildfire_media_id", $fileid)->order('join_order ASC')->first();
+    elseif($tag=="all") return $model->filter($col, $this->primval)->order('join_order ASC')->all();    
+    elseif($tag) return $model->filter($col, $this->primval)->filter("tag", $tag)->order('join_order ASC')->all();
+    else return false;
+  }
+
   public function preview(){
     return $this->render(40);
   }
