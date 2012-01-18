7ce0386c7853dac8ab1bbbc89462f2e645e71fc0
charlesmarshall <cm56marshall@googlemail.com>
2012-01-18 21:40:57 +0000

    doh, i added the file meta stuff to the wrong model!

--- lib/model/WildfireContent.php
+++ lib/model/WildfireContent.php
@@ -258,33 +258,33 @@ class WildfireContent extends WaxTreeModel {
     return $return;
   }
 
-  //this will need updating when the framework can handle manipulating join columns
+   //this will need updating when the framework can handle manipulating join columns
   public function file_meta_set($fileid, $tag, $order=0, $title=''){
-    // $model = new WaxModel;
-    // if($this->table < "wildfire_file") $model->table = $this->table."_wildfire_file";
-    // else $model->table = "wildfire_file_".$this->table;
+    $model = new WaxModel;
+    if($this->table < "wildfire_media") $model->table = $this->table."_wildfire_media";
+    else $model->table = "wildfire_media_".$this->table;
     
-    // $col = $this->table."_".$this->primary_key;
-    // if(!$order) $order = 0;
-    // if(($found = $model->filter($col, $this->primval)->filter("wildfire_file_id", $fileid)->all()) && $found->count()){
-    //   foreach($found as $r){
-    //     $sql = "UPDATE `".$model->table."` SET `join_order`=$order, `tag`='$tag', `title`='$title' WHERE `id`=$r->primval";
-    //     $model->query($sql);
-    //   }
-    // }else{
-    //   $sql = "INSERT INTO `".$model->table."` (`wildfire_file_id`, `$col`, `join_order`, `tag`, `title`) VALUES ('$fileid', '$this->primval', '$order', '$tag', '$title')";
-    //   $model->query($sql);
-    // }
+    $col = $this->table."_".$this->primary_key;
+    if(!$order) $order = 0;
+    if(($found = $model->filter($col, $this->primval)->filter("wildfire_media_id", $fileid)->all()) && $found->count()){
+      foreach($found as $r){
+        $sql = "UPDATE `".$model->table."` SET `join_order`=$order, `tag`='$tag', `title`='$title' WHERE `id`=$r->primval";
+        $model->query($sql);
+      }
+    }else{
+      $sql = "INSERT INTO `".$model->table."` (`wildfire_media_id`, `$col`, `join_order`, `tag`, `title`) VALUES ('$fileid', '$this->primval', '$order', '$tag', '$title')";
+      $model->query($sql);
+    }
   }
   public function file_meta_get($fileid=false, $tag=false){
-    // $model = new WaxModel;
-    // if($this->table < "wildfire_file") $model->table = $this->table."_wildfire_file";
-    // else $model->table = "wildfire_file_".$this->table;
-    // $col = $this->table."_".$this->primary_key;
-    // if($fileid) return $model->filter($col, $this->primval)->filter("wildfire_file_id", $fileid)->order('join_order ASC')->first();
-    // elseif($tag=="all") return $model->filter($col, $this->primval)->order('join_order ASC')->all();    
-    // elseif($tag) return $model->filter($col, $this->primval)->filter("tag", $tag)->order('join_order ASC')->all();
-    // else return false;
+    $model = new WaxModel;
+    if($this->table < "wildfire_media") $model->table = $this->table."_wildfire_media";
+    else $model->table = "wildfire_media_".$this->table;
+    $col = $this->table."_".$this->primary_key;
+    if($fileid) return $model->filter($col, $this->primval)->filter("wildfire_media_id", $fileid)->order('join_order ASC')->first();
+    elseif($tag=="all") return $model->filter($col, $this->primval)->order('join_order ASC')->all();    
+    elseif($tag) return $model->filter($col, $this->primval)->filter("tag", $tag)->order('join_order ASC')->all();
+    else return false;
   }
 
 	public function format_content() {
