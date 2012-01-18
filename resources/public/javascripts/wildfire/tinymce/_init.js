8d701ed7189666974cad067cb2b60748aab70298
charlesmarshall <cm56marshall@googlemail.com>
2012-01-18 22:11:30 +0000

    need to rewrite the inline image filter

--- resources/public/javascripts/wildfire/tinymce/_init.js
+++ resources/public/javascripts/wildfire/tinymce/_init.js
@@ -54,15 +54,14 @@ jQuery(document).ready(function(){
   //source code view
   jQuery('#wildfire-source-code').hide();
   //inline image picker
-  var cloned_files = jQuery("#files .file-listing").clone();
-  cloned_files.find("#root_node").attr("id", "inline_root_node");
-
+  var cloned_files = jQuery("#media .file-listing").clone();
+  /** 
+   * NEED TO FIX THIS!
+   */
   jQuery("#wildfire-image-dialog").html(jQuery(cloned_files).html()).hide().ajaxSuccess(function(){
-    if(!jQuery("#wildfire-image-dialog").find(".image-info").length && jQuery("#wildfire-image-dialog").find(".file-info img").length){
-      jQuery("#wildfire-image-dialog").find("input,label").remove();
-      jQuery("#wildfire-image-dialog").find(".file-info").append("<div class='image-info'><label for='wf_img_size'>Size</label><input type='text' name='wf_img_size' value='120' id='wf_img_size'><label for='wf_img_pos'>Align</label><select name='wf_img_pos' id='wf_img_pos'><option value='inline_image align_left'>Left</option><option value='inline_image align_right'>Right</option><option value='inline_image align_center'>Center</option></select><label for='wf_img_cap'>Description</label><input type='text' name='wf_img_cap' value='' id='wf_img_cap'></div>");
-    }
-  }).find(".upload-destination, #files-upload, #drop-area, #file-list").remove();
+    
+  });
+
   //table insert
   jQuery("#wildfire-table-insert").hide();
   //template insert
