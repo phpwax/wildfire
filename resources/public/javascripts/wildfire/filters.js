b932be02a1bd9cbeccf3b342f4badb2db928a980
Sheldon <sheldon.els@gmail.com>
2012-02-21 17:09:14 +0000

    reworked filter js a bit to allow for multiple inline filters on different types of objects on the same page.

--- resources/public/javascripts/wildfire/filters.js
+++ resources/public/javascripts/wildfire/filters.js
@@ -1,12 +1,12 @@
-var filter_listener = false, inline_filter_listener = false;
-
-function filter_list(form, replace){
-  var form = jQuery(form),
-      data={},
-      dest = form.find("fieldset.filters_container").attr('data-action'),
-      r = form.find("fieldset.filters_container").attr('data-replace')
+function filter_list(trigger_element, replace){
+  var form = trigger_element.closest("form"),
+      data = {},
+      fieldset = trigger_element.closest("fieldset.filters_container"),
+      dest = fieldset.attr('data-action'),
+      r = fieldset.attr('data-replace')
       ;
-  form.addClass('loading').find("fieldset.filters_container input[type='text'], fieldset.filters_container select").each(function(){
+  form.addClass('loading');
+  fieldset.find("input[type='text'], select").each(function(){
     var field = jQuery(this), nm = field.attr('name'), pl = field.attr('placeholder'), val = field.val();
     if(val != pl) data[nm] = val;
     else data[nm]='';
@@ -18,7 +18,7 @@ function filter_list(form, replace){
     success:function(res){
       form.removeClass("loading");
       if(typeof replace != "undefined") jQuery(replace).replaceWith(res);
-      else form.find(".filter-block-and-listing").find(r).replaceWith(res);
+      else fieldset.closest(".filter-block-and-listing").find(r).replaceWith(res);
       jQuery(window).trigger("filter.trigger");
       jQuery(window).trigger("join.files.highlight");
     },
@@ -58,16 +58,20 @@ function inline_filter(form_input){
 }
 
 jQuery(document).ready(function(){
+  
   jQuery(window).bind("filter.bind", function(e, obj, parent_form, replace){
-    obj.unbind("change keyup").bind("change keyup", function(){ clearTimeout(filter_listener); filter_listener = setTimeout(function(){filter_list(parent_form, replace);}, 500);});
+    var filter_listener = false;
+    obj.unbind("change keyup").bind("change keyup", function(){ clearTimeout(filter_listener); filter_listener = setTimeout(function(){filter_list(obj, replace);}, 500);});
   });
+
   jQuery('form fieldset.filters_container').find("input[type='text'],select").each(function(){
-    var obj = jQuery(this), parent_form = obj.parents("form");
+    var obj = jQuery(this), parent_form = obj.closest("form");
     if(parent_form.find(".data_table").length) jQuery(window).trigger("filter.bind", [obj, parent_form ]);
     else jQuery(window).trigger("filter.bind", [obj, parent_form, "#data-listing .data_table" ]);
   });
   jQuery(".inline-filter").each(function(){
     var obj = jQuery(this);
+    var inline_filter_listener = false;
     obj.unbind("change keyup").bind("change keyup", function(){clearTimeout(inline_filter_listener); inline_filter_listener = setTimeout(function(){inline_filter(obj);}, 500);});
   });
   if(jQuery("form .media-listing") && jQuery("form .media-listing").length){
