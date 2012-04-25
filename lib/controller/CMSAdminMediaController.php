74f693f7b10872cda8f50c0859a4666d47b7be77
charlesmarshall <cm56marshall@googlemail.com>
2012-04-25 19:48:28 +0100

    move to a public controller

--- lib/controller/CMSAdminMediaController.php
+++ lib/controller/CMSAdminMediaController.php
@@ -70,26 +70,7 @@ class CMSAdminMediaController extends AdminComponent{
     WaxEvent::run("cms.sync.location", $this);
     WaxEvent::run("cms.sync.run", $this);
   }
-  //to loop over all media records and run a render on them, 1 every 5 seconds on the sizes set in config
-  public function pre_render(){
-    set_time_limit(0);
-    ini_set('memory_limit','512M');
-    $sizes = array_merge(array(40,200), (array) Config::get("media_sizes"));
-    $model = new $this->model_class;
-    foreach($model->filter("file_type LIKE '%image%'")->filter("pre_rendered",0)->limit(1)->order("date_created ASC")->all() as $media){
-      if(!$media->ext) $media->update_attributes(array('ext'=>end(explode('.', $media->source))));
-      foreach($sizes as $size){
 
-        $file = CACHE_DIR."images/".$media->hash."/".$size.".".$media->ext;
-        if(is_readable($file)) unlink($file);
-        echo "rendering $media->title @ $size ($file)<br>\r\n";
-        echo $media->render($size) ."<hr>\r\n";
-        sleep(5);
-      }
-      $media->update_attributes(array('pre_rendered'=>1));
-    }
-    exit;
-  }
 
 }
 ?>
