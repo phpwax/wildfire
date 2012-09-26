bd6a6f75051478ff6efcbb3d95dacb8b3d457e43
Mike <michal.noskovic@googlemail.com>
2012-09-26 16:51:36 +0100

    this doesnt seem to be used anywhere

--- lib/controller/CMSAdminHomeController.php
+++ lib/controller/CMSAdminHomeController.php
@@ -39,7 +39,7 @@ class CMSAdminHomeController extends AdminComponent {
 	  $analytics = Config::get("analytics");
 	  $api = new GoogleAnalytics();
 	  if($this->use_format == "ajax" && ($login = $api->login($analytics['email'], $analytics['password']))){
-	    $api->load_accounts();
+	    //$api->load_accounts();
     	$this->visit_data = $api->data($analytics['id'], 'ga:day,ga:date', 'ga:visits', "-ga:date",false,false,($analytics['days'])?$analytics['days']:7);
     	$this->sources_data = $api->data($analytics['id'], 'ga:source,ga:referralPath', 'ga:visits');
     	$this->search_data = $api->data($analytics['id'], 'ga:keyword', 'ga:visits');
