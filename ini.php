e4063a01014c922084339a5b9bf4d7f4dc5385f6
Ross Riley <riley.ross@gmail.com>
2013-01-18 19:15:48 +0000

    register resources controllers

--- ini.php
+++ ini.php
@@ -7,3 +7,4 @@ AutoLoader::add_asset_type("help", "help");
 AutoLoader::add_asset_type('tinymce', 'tinymce');
 AutoLoader::register_view_path("plugin", __DIR__."/view/");
 AutoLoader::register_controller_path("plugin", __DIR__."/lib/controller/");
+AutoLoader::register_controller_path("plugin", __DIR__."/resources/lib/controller/");
