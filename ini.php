59270e7163eeacd3870abab6306e757c465bab1f
Ross Riley <riley.ross@gmail.com>
2013-01-21 15:14:43 +0000

    wrong symlink

--- ini.php
+++ ini.php
@@ -10,5 +10,5 @@ AutoLoader::register_controller_path("plugin", __DIR__."/lib/controller/");
 AutoLoader::register_controller_path("plugin", __DIR__."/resources/app/controller/");
 Autoloader::register_assets("wildfire",__DIR__."/resources/public/stylesheets/wildfire/", "stylesheets");
 Autoloader::register_assets("wildfire",__DIR__."/resources/public/javascripts/wildfire/", "javascripts");
-Autoloader::register_assets("wildfire",__DIR__."/resources/public/javascripts/wildfire/", "images");
+Autoloader::register_assets("wildfire",__DIR__."/resources/public/images/wildfire/", "images");
 Autoloader::$plugin_array[] = array("name"=>"wildfire","dir"=>__DIR__);
