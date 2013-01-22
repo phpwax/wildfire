1febf1efa922a0caf886605a29c6b3039a6ccd2f
Ross Riley <riley.ross@gmail.com>
2013-01-22 21:08:20 +0000

    no need for asset type now

--- ini.php
+++ ini.php
@@ -3,9 +3,6 @@
  * The first file loaded in for each plugin, should do very little and only manipulate simple values.
  * DO NOT DO ANYTHING COMPLICATED HERE!
  */
-
- AutoLoader::add_asset_type("help", "help");
- AutoLoader::add_asset_type('tinymce', 'tinymce');
  
  AutoLoader::register_view_path("plugin", __DIR__."/view/");
  AutoLoader::register_controller_path("plugin", __DIR__."/lib/controller/");
