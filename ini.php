de0641e924b26223bbfeeae5bd93622dbd16049f
Ross Riley <riley.ross@gmail.com>
2013-01-22 19:54:39 +0000

    new simpler declaration syntax for asset registration

--- ini.php
+++ ini.php
@@ -10,9 +10,9 @@
  AutoLoader::register_view_path("plugin", __DIR__."/view/");
  AutoLoader::register_controller_path("plugin", __DIR__."/lib/controller/");
  AutoLoader::register_controller_path("plugin", __DIR__."/resources/app/controller/");
- AutoLoader::register_assets("wildfire",__DIR__."/resources/public/stylesheets/wildfire/", "stylesheets");
- AutoLoader::register_assets("wildfire",__DIR__."/resources/public/javascripts/wildfire/", "javascripts");
- AutoLoader::register_assets("wildfire",__DIR__."/resources/public/images/wildfire/", "images");
- AutoLoader::$plugin_array[] = array("name"=>"wildfire","dir"=>__DIR__);
+ AutoLoader::register_assets("stylesheets/wildfire",__DIR__."/resources/public/stylesheets/wildfire/", "/*.css");
+ AutoLoader::register_assets("javascripts/wildfire",__DIR__."/resources/public/javascripts/wildfire/", "/*.js");
+ AutoLoader::register_assets("images/wildfire",__DIR__."/resources/public/images/wildfire/");
  AutoLoader::register_assets("tinymce",__DIR__."/resources/public/tinymce/");
+ AutoLoader::$plugin_array[] = array("name"=>"wildfire","dir"=>__DIR__);
  
