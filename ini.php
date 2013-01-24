c71491e9f4790d838e86cef3ad851eb0c915a8a7
Ross Riley <riley.ross@gmail.com>
2013-01-24 09:04:27 +0000

    try reducing to a single include file

--- ini.php
+++ ini.php
@@ -1,15 +1,7 @@
 <?
 /**
  * The first file loaded in for each plugin, should do very little and only manipulate simple values.
- * DO NOT DO ANYTHING COMPLICATED HERE!
+ *  
  */
  
- AutoLoader::register_view_path("plugin", __DIR__."/view/");
- AutoLoader::register_controller_path("plugin", __DIR__."/lib/controller/");
- AutoLoader::register_controller_path("plugin", __DIR__."/resources/app/controller/");
- AutoLoader::register_assets("stylesheets/wildfire",__DIR__."/resources/public/stylesheets/wildfire/", "/*.css");
- AutoLoader::register_assets("javascripts/wildfire",__DIR__."/resources/public/javascripts/wildfire/", "/*.js");
- AutoLoader::register_assets("images/wildfire",__DIR__."/resources/public/images/wildfire/");
- AutoLoader::register_assets("tinymce",__DIR__."/resources/public/tinymce/");
- AutoLoader::$plugin_array[] = array("name"=>"wildfire","dir"=>__DIR__);
  
--- setup.php
+++ setup.php
@@ -13,7 +13,15 @@ CMSApplication::register_module("media", array("display_name"=>"Media", "link"=>
 CMSApplication::register_module("users", array("display_name"=>"CMS Users", "link"=>"/admin/users/", 'split'=>true));
 CMSApplication::register_module("redirect", array("display_name"=>"Redirects", "link"=>"/admin/redirect/", 'split'=>true));
 
-
+AutoLoader::register_view_path("plugin", __DIR__."/view/");
+AutoLoader::register_controller_path("plugin", __DIR__."/lib/controller/");
+AutoLoader::register_controller_path("plugin", __DIR__."/resources/app/controller/");
+AutoLoader::register_assets("stylesheets/wildfire",__DIR__."/resources/public/stylesheets/wildfire/", "/*.css");
+AutoLoader::register_assets("javascripts/wildfire",__DIR__."/resources/public/javascripts/wildfire/", "/*.js");
+AutoLoader::register_assets("images/wildfire",__DIR__."/resources/public/images/wildfire/");
+AutoLoader::register_assets("tinymce",__DIR__."/resources/public/tinymce/");
+AutoLoader::$plugin_array[] = array("name"=>"wildfire","dir"=>__DIR__);
+ 
 
 AutoLoader::include_from_registry('CMSHelper');
 AutoLoader::register_helpers(array('CMSHelper'));
