9ba990ebf1593c88d09c74a5fd36d168c13401d4
Ross Riley <riley.ross@gmail.com>
2013-01-25 13:08:26 +0000

    fix

--- ini.php
+++ ini.php
@@ -3,4 +3,4 @@
 define("CMS_VERSION", "v7");
 define("CMS_DIR", dirname(__FILE__));
 
-AutoLoader::add_plugin_setup_scripts(__DIR__."/setup.php");
+AutoLoader::add_plugin_setup_script(__DIR__."/setup.php");
