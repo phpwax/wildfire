307dfaff64ff8340c5ca5ee699a617f2a33dcd20
Charles Marshall <charles@oneblackbear.com>
2013-02-04 21:13:27 +0000

    removing all non core files

--- lib/model/WildfireOrderedTagJoin.php
+++
@@ -1,9 +0,0 @@
-<?
-class WildfireOrderedTagJoin extends WaxModelOrderedJoin {
-  public function setup() {
-    parent::setup();
-    $this->define("tag", "CharField");
-    $this->define("title", "CharField");
-  }
-}
-?>
