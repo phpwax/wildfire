b8aa0d4f508ffc0a964a3367a7096ea9b5b7463e
Charles Marshall <charles@oneblackbear.com>
2013-02-04 21:43:45 +0000

    remove all join references

--- lib/model/WildfireContent.php
+++ lib/model/WildfireContent.php
@@ -11,12 +11,10 @@ class WildfireContent extends WaxTreeModel {
     $this->define("title", "CharField", array('export'=>true, 'maxlength'=>255, 'scaffold'=>true, 'default'=>"enter title here", 'info_preview'=>1) );
     $this->define("content", "TextField", array('widget'=>"TinymceTextareaInput"));
 
-    $this->define("media", "ManyToManyField", array('target_model'=>"WildfireMedia", "eager_loading"=>true, "join_model_class"=>"WildfireOrderedTagJoin", "join_order"=>"join_order", 'group'=>'media', 'module'=>'media'));
 
     $this->define("date_start", "DateTimeField", array('export'=>true, 'default'=>"now", 'output_format'=>"j F Y H:i",'input_format'=> 'j F Y H:i', 'info_preview'=>1));
     $this->define("date_end", "DateTimeField", array('export'=>true, 'default'=>date("Y-m-d",mktime(0,0,0, date("m"), date("j"), date("y")-10 )), 'output_format'=>"j F Y H:i", 'input_format'=> 'j F Y H:i','info_preview'=>1));
 
-    $this->define("categories", "ManyToManyField", array('target_model'=>"WildfireCategory","eager_loading"=>true, "join_model_class"=>"WaxModelOrderedJoin", "join_order"=>"join_order", 'scaffold'=>false, 'group'=>'relationships', 'info_preview'=>1));
 
     $langs = array();
     foreach(CMSApplication::$languages as $i=>$l) $langs[$i] = $l['name'];
@@ -31,7 +29,6 @@ class WildfireContent extends WaxTreeModel {
     $this->define("meta_keywords", "TextField", array('group'=>'others', 'editable'=>false));
 
     //hidden extras
-    $this->define("author", "ForeignKey", array('export'=>true, 'target_model'=>"WildfireUser", 'scaffold'=>true, 'widget'=>'HiddenInput', 'info_preview'=>1));
     $this->define("sort", "IntegerField", array('maxlength'=>3, 'default'=>0, 'widget'=>"HiddenInput", 'group'=>'parent'));
     $this->define("date_modified", "DateTimeField", array('export'=>true, 'scaffold'=>true, "editable"=>false));
     $this->define("date_created", "DateTimeField", array('export'=>true, "editable"=>false));
@@ -288,31 +285,31 @@ class WildfireContent extends WaxTreeModel {
 
    //this will need updating when the framework can handle manipulating join columns
   public function file_meta_set($fileid, $tag, $order=0, $title=''){
-    $model = new WaxModel;
-    if($this->table < "wildfire_media") $model->table = $this->table."_wildfire_media";
-    else $model->table = "wildfire_media_".$this->table;
+    // $model = new WaxModel;
+    // if($this->table < "wildfire_media") $model->table = $this->table."_wildfire_media";
+    // else $model->table = "wildfire_media_".$this->table;
 
-    $col = $this->table."_".$this->primary_key;
-    if(!$order) $order = 0;
-    if(($found = $model->filter($col, $this->primval)->filter("wildfire_media_id", $fileid)->all()) && $found->count()){
-      foreach($found as $r){
-        $sql = "UPDATE `".$model->table."` SET `join_order`=$order, `tag`='$tag', `title`='$title' WHERE `id`=$r->primval";
-        $model->query($sql);
-      }
-    }else{
-      $sql = "INSERT INTO `".$model->table."` (`wildfire_media_id`, `$col`, `join_order`, `tag`, `title`) VALUES ('$fileid', '$this->primval', '$order', '$tag', '$title')";
-      $model->query($sql);
-    }
+    // $col = $this->table."_".$this->primary_key;
+    // if(!$order) $order = 0;
+    // if(($found = $model->filter($col, $this->primval)->filter("wildfire_media_id", $fileid)->all()) && $found->count()){
+    //   foreach($found as $r){
+    //     $sql = "UPDATE `".$model->table."` SET `join_order`=$order, `tag`='$tag', `title`='$title' WHERE `id`=$r->primval";
+    //     $model->query($sql);
+    //   }
+    // }else{
+    //   $sql = "INSERT INTO `".$model->table."` (`wildfire_media_id`, `$col`, `join_order`, `tag`, `title`) VALUES ('$fileid', '$this->primval', '$order', '$tag', '$title')";
+    //   $model->query($sql);
+    // }
   }
   public function file_meta_get($fileid=false, $tag=false){
-    $model = new WaxModel;
-    if($this->table < "wildfire_media") $model->table = $this->table."_wildfire_media";
-    else $model->table = "wildfire_media_".$this->table;
-    $col = $this->table."_".$this->primary_key;
-    if($fileid) return $model->filter($col, $this->primval)->filter("wildfire_media_id", $fileid)->order('join_order ASC')->first();
-    elseif($tag=="all") return $model->filter($col, $this->primval)->order('join_order ASC')->all();
-    elseif($tag) return $model->filter($col, $this->primval)->filter("tag", $tag)->order('join_order ASC')->all();
-    else return false;
+    // $model = new WaxModel;
+    // if($this->table < "wildfire_media") $model->table = $this->table."_wildfire_media";
+    // else $model->table = "wildfire_media_".$this->table;
+    // $col = $this->table."_".$this->primary_key;
+    // if($fileid) return $model->filter($col, $this->primval)->filter("wildfire_media_id", $fileid)->order('join_order ASC')->first();
+    // elseif($tag=="all") return $model->filter($col, $this->primval)->order('join_order ASC')->all();
+    // elseif($tag) return $model->filter($col, $this->primval)->filter("tag", $tag)->order('join_order ASC')->all();
+    // else return false;
   }
 
   public function format_content() {
