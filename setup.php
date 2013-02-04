6b2e92f9fc879418cb0d703b996411c402f9122a
Charles Marshall <charles@oneblackbear.com>
2013-02-04 21:44:01 +0000

    remove references to all the other modules

--- setup.php
+++ setup.php
@@ -6,32 +6,13 @@
 
 
 CMSApplication::register_module("home", array("display_name"=>"Dashboard", "link"=>"/admin/home/", 'plugin_name'=>'wildfire', 'assets_for_cms'=>true));
-CMSApplication::register_module("categories", array("display_name"=>"Categories", "link"=>"/admin/categories/"));
 CMSApplication::register_module("content", array("display_name"=>"Content", "link"=>"/admin/content/"));
-CMSApplication::register_module("media", array("display_name"=>"Media", "link"=>"/admin/media/"));
-CMSApplication::register_module("users", array("display_name"=>"CMS Users", "link"=>"/admin/users/", 'split'=>true));
-CMSApplication::register_module("redirect", array("display_name"=>"Redirects", "link"=>"/admin/redirect/", 'split'=>true));
 
 AutoLoader::register_view_path("plugin", __DIR__."/view/");
 AutoLoader::register_controller_path("plugin", __DIR__."/lib/controller/");
 AutoLoader::register_controller_path("plugin", __DIR__."/resources/app/controller/");
 AutoLoader::$plugin_array[] = array("name"=>"wildfire","dir"=>__DIR__);
- 
+
 
 AutoLoader::register_helpers(array('CMSHelper'));
 
-WildfireMedia::$classes[] = 'WildfireDiskFile';
-//set the default media types for uploads
-WildfireMedia::$allowed  = array(
-                            'jpg'=>'WildfireDiskFile',
-                            'jpeg'=>'WildfireDiskFile',
-                            'JPG'=>'WildfireDiskFile',
-                            'png'=>'WildfireDiskFile',
-                            'gif'=>'WildfireDiskFile',
-                            'txt'=>'WildfireDiskFile',
-                            'doc'=>'WildfireDiskFile',
-                            'xls'=>'WildfireDiskFile',
-                            'zip'=>'WildfireDiskFile',
-                            'pdf'=>'WildfireDiskFile',
-			                      'webp'=>'WildfireDiskFile'
-                          );
