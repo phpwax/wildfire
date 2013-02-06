33ec7f880f1f8a3ded8ecea0ac09b9648728e108
Charles Marshall <charles@oneblackbear.com>
2013-02-06 18:10:51 +0000

    remove the other layout / format types

--- lib/controller/CMSBaseComponent.php
+++ lib/controller/CMSBaseComponent.php
@@ -98,22 +98,7 @@ class CMSBaseComponent extends WaxController {
     WaxEvent::add("cms.format.set", function(){
       $obj = WaxEvent::data();
       $obj->use_format = "json";
-      if($obj->use_format == "ajax" || $obj->use_format == "json") $obj->use_layout = false;
-      elseif($obj->use_format == "nochrome"){
-        $obj->use_format = "html";
-        $obj->use_layout = "nochrome";
-      }elseif($obj->use_format == "nolayout"){
-        $obj->use_format = "html";
-        $obj->use_layout = "nolayout";
-      }elseif($obj->use_format == "csv"){
-        $name = str_replace("/", "-", $obj->controller). "-".date("Ymdh").".csv";
-        header("Content-type: application/csv");
-        header("Content-Disposition: attachment; filename=".$name);
-        header("Pragma: no-cache");
-        header("Expires: 0");
-      }elseif($obj->use_format == "zip"){
-        $obj->use_view = $obj->use_layout = false;
-      }
+
     });
     WaxEvent::add("cms.layout.sublinks", function(){});
     WaxEvent::add('cms.search.'.$this->module_name, function(){});
