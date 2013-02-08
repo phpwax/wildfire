042209d7aa60ddad32fb46a825687df63caa0c67
Charles Marshall <charles@oneblackbear.com>
2013-02-08 16:19:50 +0000

    move autoloader bits to ini

--- ini.php
+++ ini.php
@@ -4,4 +4,9 @@ define("CMS_VERSION", "v7");
 define("CMS_DIR", dirname(__FILE__));
 
 
+AutoLoader::register_view_path("plugin", __DIR__."/view/");
+AutoLoader::register_controller_path("plugin", __DIR__."/lib/controller/");
+AutoLoader::register_controller_path("plugin", __DIR__."/resources/app/controller/");
+AutoLoader::$plugin_array[] = array("name"=>"wildfire","dir"=>__DIR__);
+
 AutoLoader::add_plugin_setup_script(__DIR__."/setup.php");
