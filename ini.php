e5b1a027ef7a38297c4b679b70da9978da7231a0
Ross Riley <riley.ross@gmail.com>
2013-11-29 08:13:38 +0000

    this code not available in this version

--- ini.php
+++ ini.php
@@ -11,14 +11,12 @@ AutoLoader::register_assets("javascripts/wildfire-plugins",__DIR__."/resources/p
 AutoLoader::register_assets("images/wildfire",__DIR__."/resources/public/images/wildfire/");
 AutoLoader::register_assets("fonts/wildfire",__DIR__."/resources/public/fonts/wildfire/","/*");
 AutoLoader::register_assets("tinymce",__DIR__."/resources/public/tinymce/");
-CMSApplication::register_asset("wildfire", "js", "wildfire");
-CMSApplication::register_asset("wildfire", "css", "wildfire");
 
 
 AutoLoader::register_view_path("plugin", __DIR__."/view/");
 AutoLoader::register_controller_path("plugin", __DIR__."/lib/controller/");
 AutoLoader::register_controller_path("plugin", __DIR__."/resources/app/controller/");
-AutoLoader::$plugin_array[] = array("name"=>"wildfire","dir"=>__DIR__);
+AutoLoader::$plugin_array[] = array("name"=>"wildfire","dir"=>__DIR__,"assets_for_cms"=>true);
 
 AutoLoader::add_plugin_setup_script(__DIR__."/setup.php");
 
