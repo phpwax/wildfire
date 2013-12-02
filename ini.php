711e2b4a054eb95e2774a8c1fb42ea11de4cf036
Ross Riley <riley.ross@gmail.com>
2013-12-02 10:21:05 +0000

    add loader for plugins

--- ini.php
+++ ini.php
@@ -17,6 +17,7 @@ AutoLoader::register_view_path("plugin", __DIR__."/view/");
 AutoLoader::register_controller_path("plugin", __DIR__."/lib/controller/");
 AutoLoader::register_controller_path("plugin", __DIR__."/resources/app/controller/");
 AutoLoader::$plugin_array[] = array("name"=>"wildfire","dir"=>__DIR__,"assets_for_cms"=>true);
+AutoLoader::$plugin_array[] = array("name"=>"wildfire-plugins","dir"=>__DIR__);
 
 AutoLoader::add_plugin_setup_script(__DIR__."/setup.php");
 
