d6ffc6f94963f6cf7bed5cde346a4a431507d737
mikenosko <michal@oneblackbear.com>
2014-05-30 15:16:10 +0100

    fixing js errors

--- resources/public/javascripts/wildfire/hover-info.js
+++ resources/public/javascripts/wildfire/hover-info.js
@@ -7,19 +7,22 @@ jQuery(function(){
     jQuery(update.attr("data-hover-selector")).live('mouseover', function(){
       if(!jQuery(this).data('init')){
         jQuery(this).data('init', true);
-        jQuery(this).hoverIntent({over:function(){
-          var hover_target = jQuery(this);
-          jQuery(update.attr("data-hover-selector")).removeClass("active");
-          hover_target.addClass("active");
-          
-          els.each(function(){
-            var el = jQuery(this);
-            var data_hover_fetch = el.attr("data-hover-fetch");
-            if(data_hover_fetch) el.text(hover_target.attr(data_hover_fetch));
-            var data_hover_fetch_href = el.attr("data-hover-fetch-href");
-            if(data_hover_fetch_href) el.attr("href", hover_target.attr(data_hover_fetch_href));
-          });
-        }, timeout:400});
+        jQuery(this).hoverIntent({
+          over:function(){
+            var hover_target = jQuery(this);
+            jQuery(update.attr("data-hover-selector")).removeClass("active");
+            hover_target.addClass("active");
+            
+            els.each(function(){
+              var el = jQuery(this);
+              var data_hover_fetch = el.attr("data-hover-fetch");
+              if(data_hover_fetch) el.text(hover_target.attr(data_hover_fetch));
+              var data_hover_fetch_href = el.attr("data-hover-fetch-href");
+              if(data_hover_fetch_href) el.attr("href", hover_target.attr(data_hover_fetch_href));
+            });
+          }, timeout:400,
+          out:function(){}
+        });
         jQuery(this).trigger('mouseover');
       }
     });
