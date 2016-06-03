53d291cc1209727658a4bf7ee1b9d6e373e7c08d
Ross Riley <riley.ross@gmail.com>
2016-06-03 17:50:43 +0100

    and add asset path registration

--- ini.php
+++ ini.php
@@ -4,6 +4,14 @@
  * DO NOT DO ANYTHING COMPLICATED HERE!
  */
 Autoloader::add_asset_type("help", "help");
+
+AutoLoader::register_assets("stylesheets/wildfire",__DIR__."/resources/public/stylesheets/wildfire/", "/*.css");
+AutoLoader::register_assets("javascripts/wildfire",__DIR__."/resources/public/javascripts/wildfire/", "/*.js");
+AutoLoader::register_assets("javascripts/wildfire-plugins",__DIR__."/resources/public/javascripts/wildfire-plugins/", "/*.js");
+AutoLoader::register_assets("images/wildfire",__DIR__."/resources/public/images/wildfire/");
+AutoLoader::register_assets("fonts/wildfire",__DIR__."/resources/public/fonts/wildfire/","/*");
+AutoLoader::register_assets("tinymce",__DIR__."/resources/public/tinymce/");
+
 AutoLoader::register_view_path("plugin", __DIR__."/view/");
 AutoLoader::register_controller_path("plugin", __DIR__."/lib/controller/");
 AutoLoader::register_controller_path("plugin", __DIR__."/resources/app/controller/");
