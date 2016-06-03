0b509f33f62945bfc18e0737656c828a6cecfffd
Ross Riley <riley.ross@gmail.com>
2016-06-03 17:37:20 +0100

    for composer compatibility add an explicit register call to the view and controller paths

--- ini.php
+++ ini.php
@@ -1,7 +1,9 @@
-<?
+<?php
 /**
  * The first file loaded in for each plugin, should do very little and only manipulate simple values.
  * DO NOT DO ANYTHING COMPLICATED HERE!
  */
 Autoloader::add_asset_type("help", "help");
-?>
+AutoLoader::register_view_path("plugin", __DIR__."/view/");
+AutoLoader::register_controller_path("plugin", __DIR__."/lib/controller/");
+AutoLoader::register_controller_path("plugin", __DIR__."/resources/app/controller/");
