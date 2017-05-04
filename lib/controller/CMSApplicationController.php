6b9c1b019a115f082091cdfa49cb135c399d2c1e
mikenosko <michal@oneblackbear.com>
2017-05-04 19:22:54 +0100

    add protocol

--- lib/controller/CMSApplicationController.php
+++ lib/controller/CMSApplicationController.php
@@ -236,7 +236,7 @@ class CMSApplicationController extends WaxController{
 	 * split out what to do for a map object
 	 */
 	public function map_to_content($map){
-		if($map->destination_url && !$map->track_url) $this->redirect_to($map->destination_url, $map->header_status);
+		if($map->destination_url && !$map->track_url) $this->redirect_to($map->destination_url, "http://", $map->header_status);
 	  elseif($map->destination_url) $this->redirect_to($map->destination_url."?utm_source=".$map->origin_url."&utm_campaign=".$map->utm_campaign."&utm_medium=".(($map->utm_medium)? $map->utm_medium : "Web Redirect") . $map->hash, "http://", $map->header_status);
 	  elseif(($model = $map->destination_model) && ($model_id = $map->destination_id) ) return new $model($model_id);
 	}
